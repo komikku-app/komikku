65bd181fee9d78a2c79d2d0fef33b10d8e232571
jobobby04 <jobobby04@users.noreply.github.com>
2020-04-01 12:34:03 -0400

    Notification conflict fix

--- app/src/main/java/eu/kanade/tachiyomi/data/notification/Notifications.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/notification/Notifications.kt
@@ -49,7 +49,7 @@ object Notifications {
      * Notification channel and ids used by the library updater.
      */
     const val CHANNEL_UPDATES_TO_EXTS = "updates_ext_channel"
-    const val ID_UPDATES_TO_EXTS = -401
+    const val ID_UPDATES_TO_EXTS = -501
 
     /**
      * Creates the notification channels introduced in Android Oreo.
