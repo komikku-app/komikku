a5ea09426b50b21f4f7f6155c75f84a95d0d8d67
jobobby04 <jobobby04@users.noreply.github.com>
2020-04-04 00:26:32 -0400

    Minor formatting
    (cherry picked from commit f1cca207fcc3d2e8895da26c47085fd93f6ee787)

--- app/src/main/java/eu/kanade/tachiyomi/ui/catalogue/filter/SeparatorItem.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/catalogue/filter/SeparatorItem.kt
@@ -1,8 +1,9 @@
 package eu.kanade.tachiyomi.ui.catalogue.filter
 
 import android.annotation.SuppressLint
-import android.view.View
 import com.google.android.material.R
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.items.AbstractHeaderItem
 import eu.davidea.flexibleadapter.items.IFlexible
@@ -16,12 +17,11 @@ class SeparatorItem(val filter: Filter.Separator) : AbstractHeaderItem<Separator
         return R.layout.design_navigation_item_separator
     }
 
-    override fun createViewHolder(view: View, adapter: FlexibleAdapter<IFlexible<androidx.recyclerview.widget.RecyclerView.ViewHolder>>): Holder {
+    override fun createViewHolder(view: View, adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>): Holder {
         return Holder(view, adapter)
     }
 
-    override fun bindViewHolder(adapter: FlexibleAdapter<IFlexible<androidx.recyclerview.widget.RecyclerView.ViewHolder>>, holder: Holder, position: Int, payloads: List<Any?>?) {
-
+    override fun bindViewHolder(adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>, holder: Holder, position: Int, payloads: List<Any?>?) {
     }
 
     override fun equals(other: Any?): Boolean {
