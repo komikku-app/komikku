8cba8c4b4498d235f30e46e2ca46669bdf20353e
jobobby04 <jobobby04@users.noreply.github.com>
2020-04-04 16:45:11 -0400

    More preference grouping
    (cherry picked from commit 3a62acc54d5a2643a7151a92deeec6814e0f4996)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsGeneralController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsGeneralController.kt
@@ -27,103 +27,13 @@ class SettingsGeneralController : SettingsController() {
     override fun setupPreferenceScreen(screen: PreferenceScreen) = with(screen) {
         titleRes = R.string.pref_category_general
 
-        listPreference {
-            key = Keys.lang
-            titleRes = R.string.pref_language
-            entryValues = arrayOf("", "ar", "bg", "bn", "ca", "cs", "de", "el", "en-US", "en-GB",
-                    "es", "fr", "hi", "hu", "in", "it", "ja", "ko", "lv", "ms", "nb-rNO", "nl", "pl", "pt",
-                    "pt-BR", "ro", "ru", "sc", "sr", "sv", "th", "tl", "tr", "uk", "vi", "zh-rCN")
-            entries = entryValues.map { value ->
-                val locale = LocaleHelper.getLocaleFromString(value.toString())
-                locale?.getDisplayName(locale)?.capitalize()
-                        ?: context.getString(R.string.system_default)
-            }.toTypedArray()
-            defaultValue = ""
-            summary = "%s"
-
-            onChange { newValue ->
-                val activity = activity ?: return@onChange false
-                val app = activity.application
-                LocaleHelper.changeLocale(newValue.toString())
-                LocaleHelper.updateConfiguration(app, app.resources.configuration)
-                activity.recreate()
-                true
-            }
-        }
-        listPreference {
-            key = Keys.dateFormat
-            titleRes = R.string.pref_date_format
-            entryValues = arrayOf("", "MM/dd/yy", "dd/MM/yy", "yyyy-MM-dd")
-            entries = entryValues.map { value ->
-                if (value == "") {
-                    context.getString(R.string.system_default)
-                } else {
-                    value
-                }
-            }.toTypedArray()
-            defaultValue = ""
-            summary = "%s"
-        }
-        listPreference {
-            key = Keys.themeMode
-            titleRes = R.string.pref_theme_mode
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                entriesRes = arrayOf(
-                        R.string.theme_system,
-                        R.string.theme_light,
-                        R.string.theme_dark)
-                entryValues = arrayOf(
-                        Values.THEME_MODE_SYSTEM,
-                        Values.THEME_MODE_LIGHT,
-                        Values.THEME_MODE_DARK)
-                defaultValue = Values.THEME_MODE_SYSTEM
-            } else {
-                entriesRes = arrayOf(
-                        R.string.theme_light,
-                        R.string.theme_dark)
-                entryValues = arrayOf(
-                        Values.THEME_MODE_LIGHT,
-                        Values.THEME_MODE_DARK)
-                defaultValue = Values.THEME_MODE_LIGHT
-            }
-
-            summary = "%s"
-
-            onChange {
-                activity?.recreate()
-                true
-            }
-        }
-        listPreference {
-            key = Keys.themeDark
-            titleRes = R.string.pref_theme_dark
-            entriesRes = arrayOf(
-                    R.string.theme_dark_default,
-                    R.string.theme_dark_blue,
-                    R.string.theme_dark_amoled)
-            entryValues = arrayOf(
-                    Values.THEME_DARK_DEFAULT,
-                    Values.THEME_DARK_BLUE,
-                    Values.THEME_DARK_AMOLED)
-            defaultValue = Values.THEME_DARK_DEFAULT
-            summary = "%s"
-
-            preferences.themeMode().asObservable()
-                    .subscribeUntilDestroy { isVisible = it != Values.THEME_MODE_LIGHT }
-
-            onChange {
-                if (preferences.themeMode().getOrDefault() != Values.THEME_MODE_LIGHT) {
-                    activity?.recreate()
-                }
-                true
-            }
-        }
         intListPreference {
             key = Keys.startScreen
             titleRes = R.string.pref_start_screen
-            entriesRes = arrayOf(R.string.label_library, R.string.label_recent_manga,
-                    R.string.label_recent_updates)
+            entriesRes = arrayOf(
+                R.string.label_library, R.string.label_recent_manga,
+                R.string.label_recent_updates
+            )
             entryValues = arrayOf("1", "2", "3")
             defaultValue = "1"
             summary = "%s"
@@ -140,6 +50,144 @@ class SettingsGeneralController : SettingsController() {
             }
         }
 
+        preferenceCategory {
+            titleRes = R.string.pref_category_display
+            
+            listPreference {
+                key = Keys.lang
+                titleRes = R.string.pref_language
+                entryValues = arrayOf(
+                    "",
+                    "ar",
+                    "bg",
+                    "bn",
+                    "ca",
+                    "cs",
+                    "de",
+                    "el",
+                    "en-US",
+                    "en-GB",
+                    "es",
+                    "fr",
+                    "hi",
+                    "hu",
+                    "in",
+                    "it",
+                    "ja",
+                    "ko",
+                    "lv",
+                    "ms",
+                    "nb-rNO",
+                    "nl",
+                    "pl",
+                    "pt",
+                    "pt-BR",
+                    "ro",
+                    "ru",
+                    "sc",
+                    "sr",
+                    "sv",
+                    "th",
+                    "tl",
+                    "tr",
+                    "uk",
+                    "vi",
+                    "zh-rCN"
+                )
+                entries = entryValues.map { value ->
+                    val locale = LocaleHelper.getLocaleFromString(value.toString())
+                    locale?.getDisplayName(locale)?.capitalize()
+                        ?: context.getString(R.string.system_default)
+                }.toTypedArray()
+                defaultValue = ""
+                summary = "%s"
+
+                onChange { newValue ->
+                    val activity = activity ?: return@onChange false
+                    val app = activity.application
+                    LocaleHelper.changeLocale(newValue.toString())
+                    LocaleHelper.updateConfiguration(app, app.resources.configuration)
+                    activity.recreate()
+                    true
+                }
+            }
+            listPreference {
+                key = Keys.dateFormat
+                titleRes = R.string.pref_date_format
+                entryValues = arrayOf("", "MM/dd/yy", "dd/MM/yy", "yyyy-MM-dd")
+                entries = entryValues.map { value ->
+                    if (value == "") {
+                        context.getString(R.string.system_default)
+                    } else {
+                        value
+                    }
+                }.toTypedArray()
+                defaultValue = ""
+                summary = "%s"
+            }
+            listPreference {
+                key = Keys.themeMode
+                titleRes = R.string.pref_theme_mode
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    entriesRes = arrayOf(
+                        R.string.theme_system,
+                        R.string.theme_light,
+                        R.string.theme_dark
+                    )
+                    entryValues = arrayOf(
+                        Values.THEME_MODE_SYSTEM,
+                        Values.THEME_MODE_LIGHT,
+                        Values.THEME_MODE_DARK
+                    )
+                    defaultValue = Values.THEME_MODE_SYSTEM
+                } else {
+                    entriesRes = arrayOf(
+                        R.string.theme_light,
+                        R.string.theme_dark
+                    )
+                    entryValues = arrayOf(
+                        Values.THEME_MODE_LIGHT,
+                        Values.THEME_MODE_DARK
+                    )
+                    defaultValue = Values.THEME_MODE_LIGHT
+                }
+
+                summary = "%s"
+
+                onChange {
+                    activity?.recreate()
+                    true
+                }
+            }
+            listPreference {
+                key = Keys.themeDark
+                titleRes = R.string.pref_theme_dark
+                entriesRes = arrayOf(
+                    R.string.theme_dark_default,
+                    R.string.theme_dark_blue,
+                    R.string.theme_dark_amoled
+                )
+                entryValues = arrayOf(
+                    Values.THEME_DARK_DEFAULT,
+                    Values.THEME_DARK_BLUE,
+                    Values.THEME_DARK_AMOLED
+                )
+                defaultValue = Values.THEME_DARK_DEFAULT
+                summary = "%s"
+
+                preferences.themeMode().asObservable()
+                    .subscribeUntilDestroy { isVisible = it != Values.THEME_MODE_LIGHT }
+
+                onChange {
+                    if (preferences.themeMode().getOrDefault() != Values.THEME_MODE_LIGHT) {
+                        activity?.recreate()
+                    }
+                    true
+                }
+            }
+        }
+
         // --> EXH
         switchPreference {
             key = Keys.eh_expandFilters
@@ -150,7 +198,8 @@ class SettingsGeneralController : SettingsController() {
         switchPreference {
             key = Keys.eh_autoSolveCaptchas
             title = "Automatically solve captcha"
-            summary = "Use HIGHLY EXPERIMENTAL automatic ReCAPTCHA solver. Will be grayed out if unsupported by your device."
+            summary =
+                "Use HIGHLY EXPERIMENTAL automatic ReCAPTCHA solver. Will be grayed out if unsupported by your device."
             defaultValue = false
             shouldDisableView = Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP
         }
@@ -179,7 +228,8 @@ class SettingsGeneralController : SettingsController() {
                 key = Keys.eh_lock_manually
 
                 title = "Lock manually only"
-                summary = "Disable automatic app locking. The app can still be locked manually by long-pressing the three-lines/back button in the top left corner."
+                summary =
+                    "Disable automatic app locking. The app can still be locked manually by long-pressing the three-lines/back button in the top left corner."
                 defaultValue = false
             }
             switchPreference {
