7a959b1dcb4c535d3308f56053bea897cacc9fd1
jobobby04 <jobobby04@users.noreply.github.com>
2020-04-04 22:41:31 -0400

    Hide download/delete actions when sensible, make bookmark/read toggle actions
    (cherry picked from commit 090a7794b5a3d93ac6e62afe04c750596a6b4e8d)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/chapter/ChaptersController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/chapter/ChaptersController.kt
@@ -390,6 +390,17 @@ class ChaptersController : NucleusController<ChaptersPresenter>(),
             destroyActionModeIfNeeded()
         } else {
             mode.title = count.toString()
+
+            val chapters = getSelectedChapters()
+            menu.findItem(R.id.action_download)?.isVisible = chapters.any { !it.isDownloaded }
+            menu.findItem(R.id.action_delete)?.isVisible = chapters.any { it.isDownloaded }
+            menu.findItem(R.id.action_bookmark)?.isVisible = chapters.any { !it.chapter.bookmark }
+            menu.findItem(R.id.action_remove_bookmark)?.isVisible = chapters.all { it.chapter.bookmark }
+            menu.findItem(R.id.action_mark_as_read)?.isVisible = chapters.any { !it.chapter.read }
+            menu.findItem(R.id.action_mark_as_unread)?.isVisible = chapters.all { it.chapter.read }
+
+            // Hide FAB to avoid interfering with the bottom action toolbar
+            fab.hide()
         }
         return false
     }
