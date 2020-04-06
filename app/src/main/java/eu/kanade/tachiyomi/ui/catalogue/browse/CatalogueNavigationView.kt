18bef8345dacc3e2ad8a59cb54b485b1a5ea3e17
jobobby04 <jobobby04@users.noreply.github.com>
2020-04-06 18:17:35 -0400

    Minor edits
    (cherry picked from commit 441e2a69d8797084b5024743484a6f48debe0238)

--- app/src/main/java/eu/kanade/tachiyomi/ui/catalogue/browse/CatalogueNavigationView.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/catalogue/browse/CatalogueNavigationView.kt
@@ -21,7 +21,6 @@ import kotlinx.android.synthetic.main.catalogue_drawer_content.view.reset_btn
 import kotlinx.android.synthetic.main.catalogue_drawer_content.view.save_search_btn
 import kotlinx.android.synthetic.main.catalogue_drawer_content.view.saved_searches
 import kotlinx.android.synthetic.main.catalogue_drawer_content.view.search_btn
-import kotlinx.android.synthetic.main.catalogue_drawer_content.view.title
 
 class CatalogueNavigationView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
     SimpleNavigationView(context, attrs) {
@@ -52,7 +51,6 @@ class CatalogueNavigationView @JvmOverloads constructor(context: Context, attrs:
         val view = inflate(R.layout.catalogue_drawer_content)
         ((view as ViewGroup).getChildAt(1) as ViewGroup).addView(recycler)
         addView(view)
-        title.text = context.getString(R.string.source_search_options)
         save_search_btn.setOnClickListener { onSaveClicked() }
         search_btn.setOnClickListener { onSearchClicked() }
         reset_btn.setOnClickListener { onResetClicked() }
