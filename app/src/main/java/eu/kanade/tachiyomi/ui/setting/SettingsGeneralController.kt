a2c25a392bdc4facad8ab78ac8e73f0dad4835da
jobobby04 <jobobby04@users.noreply.github.com>
2020-04-17 01:07:09 -0400

    Migrations now shows how many manual migrations were made after skipping the rest
    (cherry picked from commit bac01c98073dbee6c09144b6ebc55b6a0d56d0da)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsGeneralController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsGeneralController.kt
@@ -16,7 +16,6 @@ import eu.kanade.tachiyomi.util.preference.onChange
 import eu.kanade.tachiyomi.util.preference.onClick
 import eu.kanade.tachiyomi.util.preference.preference
 import eu.kanade.tachiyomi.util.preference.preferenceCategory
-import eu.kanade.tachiyomi.util.preference.summaryRes
 import eu.kanade.tachiyomi.util.preference.switchPreference
 import eu.kanade.tachiyomi.util.preference.titleRes
 import eu.kanade.tachiyomi.util.system.LocaleHelper
@@ -184,16 +183,6 @@ class SettingsGeneralController : SettingsController() {
             }
         }
 
-        if (preferences.skipPreMigration().getOrDefault() || preferences.migrationSources()
-                .getOrDefault().isNotEmpty()) {
-            switchPreference {
-                key = Keys.skipPreMigration
-                titleRes = R.string.pref_skip_pre_migration
-                summaryRes = R.string.pref_skip_pre_migration_summary
-                defaultValue = false
-            }
-        }
-
         // --> EXH
         switchPreference {
             key = Keys.eh_expandFilters
@@ -207,7 +196,6 @@ class SettingsGeneralController : SettingsController() {
             summary =
                 "Use HIGHLY EXPERIMENTAL automatic ReCAPTCHA solver. Will be grayed out if unsupported by your device."
             defaultValue = false
-            shouldDisableView = Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP
         }
 
         preferenceCategory {
