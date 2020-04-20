8b6caa40058989249ecce703c5c750a23d8e03d0
Jobobby04 <jobobby04@users.noreply.github.com>
2020-04-20 19:29:42 -0400

    Revert attempted WebView process fix
    (cherry picked from commit f115691365b65e2f33c266c71a629283dc103f31)

--- app/src/main/java/eu/kanade/tachiyomi/App.kt
+++ app/src/main/java/eu/kanade/tachiyomi/App.kt
@@ -6,7 +6,6 @@ import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Build
 import android.os.Environment
-import android.webkit.WebView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
@@ -70,14 +69,6 @@ open class App : Application(), LifecycleObserver {
         LocaleHelper.updateConfiguration(this, resources.configuration)
 
         ProcessLifecycleOwner.get().lifecycle.addObserver(this)
-
-        // Avoid sharing WebView instance across multiple processes
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            val processName = getProcessName()
-            if (packageName != processName) {
-                WebView.setDataDirectorySuffix(processName)
-            }
-        }
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
