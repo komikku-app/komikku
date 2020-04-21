2b91531fc4df3676e5dc1bf76aa29588bf116e99
Jobobby04 <jobobby04@users.noreply.github.com>
2020-04-21 16:18:20 -0400

    Complete RxBindings to FlowBinding migration
    (cherry picked from commit 3d10dad780d7d9c1a1b9c5fa634efe710183845b)

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryController.kt
@@ -23,7 +23,6 @@ import com.bluelinelabs.conductor.ControllerChangeHandler
 import com.bluelinelabs.conductor.ControllerChangeType
 import com.f2prateek.rx.preferences.Preference
 import com.google.android.material.tabs.TabLayout
-import com.jakewharton.rxbinding.support.v4.view.pageSelections
 import com.jakewharton.rxrelay.BehaviorRelay
 import com.jakewharton.rxrelay.PublishRelay
 import eu.kanade.tachiyomi.R
@@ -56,6 +55,7 @@ import kotlinx.android.synthetic.main.main_activity.tabs
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.onEach
 import reactivecircus.flowbinding.appcompat.queryTextChanges
+import reactivecircus.flowbinding.viewpager.pageSelections
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 import timber.log.Timber
@@ -189,10 +189,12 @@ class LibraryController(
 
         adapter = LibraryAdapter(this)
         binding.libraryPager.adapter = adapter
-        binding.libraryPager.pageSelections().skip(1).subscribeUntilDestroy {
-            preferences.lastUsedCategory().set(it)
-            activeCategory = it
-        }
+        binding.libraryPager.pageSelections()
+            .onEach {
+                preferences.lastUsedCategory().set(it)
+                activeCategory = it
+            }
+            .launchInUI()
 
         getColumnsPreferenceForCurrentOrientation().asObservable()
                 .doOnNext { mangaPerRow = it }
