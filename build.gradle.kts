b47ab357fecf0c13fd0bba6cb68e5dc605cd62f6
Jobobby04 <jobobby04@users.noreply.github.com>
2020-05-02 00:46:06 -0400

    Update top level project build scripts
    (cherry picked from commit 81b2dfd9d039d62f169be60151e3dd3db6ea1b13)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,9 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.github.ben-manes.versions") version "0.28.0"
-}
-
-buildscript {
-    repositories {
-        google()
-        jcenter()
-        maven { setUrl("https://maven.fabric.io/public") }
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:3.6.2")
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.28.0")
-        classpath("com.github.zellius:android-shortcut-gradle-plugin:0.1.2")
-        classpath("org.jmailen.gradle:kotlinter-gradle:2.3.2")
-        classpath("com.google.gms:google-services:4.3.3")
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.2")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-
-        // Realm (EH)
-        classpath("io.realm:realm-gradle-plugin:5.13.1")
-
-        // Firebase (EH)
-        classpath("io.fabric.tools:gradle:1.31.0")
-    }
+    id("com.android.application") version BuildPluginsVersion.AGP apply false
+    id("com.android.library") version BuildPluginsVersion.AGP apply false
+    kotlin("android") version BuildPluginsVersion.KOTLIN apply false
+    id("org.jlleitschuh.gradle.ktlint") version BuildPluginsVersion.KTLINT
+    id("com.github.ben-manes.versions") version BuildPluginsVersion.VERSIONS_PLUGIN
 }
 
 allprojects {
@@ -34,6 +13,44 @@ allprojects {
         maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
         maven { setUrl("https://dl.bintray.com/ibm-cloud-sdks/ibm-cloud-sdk-repo") }
         maven { setUrl("https://plugins.gradle.org/m2/") }
+        jcenter()
+    }
+}
+
+subprojects {
+    apply {
+        plugin("org.jlleitschuh.gradle.ktlint")
+    }
+
+    ktlint {
+        debug.set(false)
+        version.set(Versions.KTLINT)
+        verbose.set(true)
+        android.set(false)
+        outputToConsole.set(true)
+        ignoreFailures.set(false)
+        enableExperimentalRules.set(true)
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
+    }
+}
+
+buildscript {
+    dependencies {
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.28.0")
+        classpath("com.github.zellius:android-shortcut-gradle-plugin:0.1.2")
+        classpath("com.google.gms:google-services:4.3.3")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.2")
+        // Realm (EH)
+        classpath("io.realm:realm-gradle-plugin:5.13.1")
+
+        // Firebase (EH)
+        classpath("io.fabric.tools:gradle:1.31.0")
+    }
+    repositories {
+        google()
         jcenter()
     }
 }
