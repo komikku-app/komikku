8fed4191be1954d9e91f6f036486679e239ff9a8
arkon <eugcheung94@gmail.com>
2020-05-10 11:42:46 -0400

    Convert rotation to FlowPreference, remove some unused subscriptions code

--- app/src/main/java/eu/kanade/tachiyomi/ui/base/controller/RxController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/base/controller/RxController.kt
@@ -10,11 +10,21 @@ import rx.subscriptions.CompositeSubscription
 
 abstract class RxController<VB : ViewBinding>(bundle: Bundle? = null) : BaseController<VB>(bundle) {
 
-    var untilDetachSubscriptions = CompositeSubscription()
-        private set
+    private var untilDestroySubscriptions = CompositeSubscription()
+    private var untilDetachSubscriptions = CompositeSubscription()
 
-    var untilDestroySubscriptions = CompositeSubscription()
-        private set
+    @CallSuper
+    override fun onViewCreated(view: View) {
+        if (untilDestroySubscriptions.isUnsubscribed) {
+            untilDestroySubscriptions = CompositeSubscription()
+        }
+    }
+
+    @CallSuper
+    override fun onDestroyView(view: View) {
+        super.onDestroyView(view)
+        untilDestroySubscriptions.unsubscribe()
+    }
 
     @CallSuper
     override fun onAttach(view: View) {
@@ -30,62 +40,11 @@ abstract class RxController<VB : ViewBinding>(bundle: Bundle? = null) : BaseCont
         untilDetachSubscriptions.unsubscribe()
     }
 
-    @CallSuper
-    override fun onViewCreated(view: View) {
-        if (untilDestroySubscriptions.isUnsubscribed) {
-            untilDestroySubscriptions = CompositeSubscription()
-        }
-    }
-
-    @CallSuper
-    override fun onDestroyView(view: View) {
-        super.onDestroyView(view)
-        untilDestroySubscriptions.unsubscribe()
-    }
-
-    fun <T> Observable<T>.subscribeUntilDetach(): Subscription {
-        return subscribe().also { untilDetachSubscriptions.add(it) }
-    }
-
-    fun <T> Observable<T>.subscribeUntilDetach(onNext: (T) -> Unit): Subscription {
-        return subscribe(onNext).also { untilDetachSubscriptions.add(it) }
-    }
-
-    fun <T> Observable<T>.subscribeUntilDetach(
-        onNext: (T) -> Unit,
-        onError: (Throwable) -> Unit
-    ): Subscription {
-        return subscribe(onNext, onError).also { untilDetachSubscriptions.add(it) }
-    }
-
-    fun <T> Observable<T>.subscribeUntilDetach(
-        onNext: (T) -> Unit,
-        onError: (Throwable) -> Unit,
-        onCompleted: () -> Unit
-    ): Subscription {
-        return subscribe(onNext, onError, onCompleted).also { untilDetachSubscriptions.add(it) }
-    }
-
-    fun <T> Observable<T>.subscribeUntilDestroy(): Subscription {
-        return subscribe().also { untilDestroySubscriptions.add(it) }
-    }
-
     fun <T> Observable<T>.subscribeUntilDestroy(onNext: (T) -> Unit): Subscription {
         return subscribe(onNext).also { untilDestroySubscriptions.add(it) }
     }
 
-    fun <T> Observable<T>.subscribeUntilDestroy(
-        onNext: (T) -> Unit,
-        onError: (Throwable) -> Unit
-    ): Subscription {
-        return subscribe(onNext, onError).also { untilDestroySubscriptions.add(it) }
-    }
-
-    fun <T> Observable<T>.subscribeUntilDestroy(
-        onNext: (T) -> Unit,
-        onError: (Throwable) -> Unit,
-        onCompleted: () -> Unit
-    ): Subscription {
-        return subscribe(onNext, onError, onCompleted).also { untilDestroySubscriptions.add(it) }
+    fun <T> Observable<T>.subscribeUntilDetach(onNext: (T) -> Unit): Subscription {
+        return subscribe(onNext).also { untilDetachSubscriptions.add(it) }
     }
 }
