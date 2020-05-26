75bddd5105cd9f9d1d214e97f486215c58f6a138
Jobobby04 <jobobby04@users.noreply.github.com>
2020-05-26 14:07:57 -0400

    Make global search use full height manga cards with respect to the source display setting

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/globalsearch/GlobalSearchCardItem.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/globalsearch/GlobalSearchCardItem.kt
@@ -7,11 +7,18 @@ import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
 import eu.davidea.flexibleadapter.items.IFlexible
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.database.models.Manga
+import eu.kanade.tachiyomi.data.preference.PreferenceValues
+import eu.kanade.tachiyomi.data.preference.PreferencesHelper
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 class GlobalSearchCardItem(val manga: Manga) : AbstractFlexibleItem<GlobalSearchCardHolder>() {
 
     override fun getLayoutRes(): Int {
-        return R.layout.global_search_controller_card_item
+        return when (Injekt.get<PreferencesHelper>().catalogueDisplayMode().get()) {
+            PreferenceValues.DisplayMode.COMPACT_GRID -> R.layout.global_search_controller_compact_card_item
+            else -> R.layout.global_search_controller_comfortable_card_item
+        }
     }
 
     override fun createViewHolder(view: View, adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>): GlobalSearchCardHolder {
