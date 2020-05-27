dfe7057cd0bb0555c94099827a96e59356cb5504
az4521 <az4521@users.noreply.github.com>
2020-05-27 15:10:18 -0400

    whoops

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/info/MangaInfoController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/info/MangaInfoController.kt
@@ -23,6 +23,7 @@ import eu.kanade.tachiyomi.data.database.models.Manga
 import eu.kanade.tachiyomi.data.glide.GlideApp
 import eu.kanade.tachiyomi.data.glide.toMangaThumbnail
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
+import eu.kanade.tachiyomi.data.preference.getOrDefault
 import eu.kanade.tachiyomi.databinding.MangaInfoControllerBinding
 import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.SourceManager
