7bd7725887c3bce91830854b47538d89e4666fe7
az4521 <az4521@users.noreply.github.com>
2020-06-10 17:20:41 -0400

    i'm an idiot

--- app/src/main/java/eu/kanade/tachiyomi/ui/source/browse/SourceComfortableGridHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/source/browse/SourceComfortableGridHolder.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.ui.browse.source.browse
+package eu.kanade.tachiyomi.ui.source.browse
 
 import android.view.View
 import com.bumptech.glide.load.engine.DiskCacheStrategy
