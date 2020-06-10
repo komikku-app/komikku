e770c76d4082107a45f5dfd03a164cc78b0aa545
az4521 <az4521@users.noreply.github.com>
2020-06-10 05:34:18 -0400

    IT JUST KEEPS HAPPENING

--- app/src/main/java/eu/kanade/tachiyomi/ui/extension/ExtensionPreferencesController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/extension/ExtensionPreferencesController.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.ui.browse.extension
+package eu.kanade.tachiyomi.ui.extension
 
 import android.annotation.SuppressLint
 import android.content.Context
--- app/src/main/java/eu/kanade/tachiyomi/ui/extension/ExtensionPreferencesPresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/extension/ExtensionPreferencesPresenter.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.ui.browse.extension
+package eu.kanade.tachiyomi.ui.extension
 
 import eu.kanade.tachiyomi.extension.ExtensionManager
 import eu.kanade.tachiyomi.ui.base.presenter.BasePresenter
