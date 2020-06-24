3e9e42271eae7bdd38714fda4eaf10fd339128f0
arkon <eugcheung94@gmail.com>
2020-06-23 22:42:53 -0400

    Add advanced option for DNS over HTTPS via Cloudflare (closes #3377)
    (cherry picked from commit 3645d19135683cf8d076f560619c1b47c7b21d00)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
    #	app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsAdvancedController.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
@@ -240,6 +240,8 @@ class PreferencesHelper(val context: Context) {
 
     fun trustedSignatures() = flowPrefs.getStringSet("trusted_signatures", emptySet())
 
+    fun enableDoh() = prefs.getBoolean(Keys.enableDoh, false)
+
     // --> AZ J2K CHERRYPICKING
 
     fun defaultMangaOrder() = flowPrefs.getString("default_manga_order", "")
