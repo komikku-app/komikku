af0c037845ca5cd2969d744ea82e99725a271afe
Jobobby04 <jobobby04@users.noreply.github.com>
2020-06-27 19:22:37 -0400

    More mangadex work (search is still not working)

--- app/src/main/java/eu/kanade/tachiyomi/source/online/all/MangaDex.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/online/all/MangaDex.kt
@@ -3,16 +3,25 @@ package eu.kanade.tachiyomi.source.online.all
 import android.net.Uri
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.source.ConfigurableSource
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.source.online.UrlImportableSource
 import exh.source.DelegatedHttpSource
+import exh.util.urlImportFetchSearchManga
+import rx.Observable
 
 class MangaDex(delegate: HttpSource) :
     DelegatedHttpSource(delegate),
     ConfigurableSource,
     UrlImportableSource {
 
-    override val matchingHosts: List<String> = listOf("mangadex.org")
+    override val matchingHosts: List<String> = listOf("mangadex.org", "www.mangadex.org")
+
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> =
+        urlImportFetchSearchManga(query) {
+            super.fetchSearchManga(page, query, filters)
+        }
 
     override fun mapUrlToMangaUrl(uri: Uri): String? {
         val lcFirstPathSegment = uri.pathSegments.firstOrNull()?.toLowerCase() ?: return null
