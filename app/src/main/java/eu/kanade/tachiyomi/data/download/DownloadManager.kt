589464d723f132a659764c14c925e280c9df02ee
Jobobby04 <jobobby04@users.noreply.github.com>
2020-07-24 23:11:36 -0400

    More rename downloaded chapter tweaks

--- app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadManager.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadManager.kt
@@ -251,20 +251,17 @@ class DownloadManager(/* SY private */ val context: Context) {
      * @param newChapter the target chapter with the new name.
      */
     fun renameChapter(source: Source, manga: Manga, oldChapter: Chapter, newChapter: Chapter) {
-        val oldName = provider.getValidChapterDirNames(oldChapter)
+        val oldNames = provider.getValidChapterDirNames(oldChapter)
         val newName = provider.getChapterDirName(newChapter)
         val mangaDir = provider.getMangaDir(manga, source)
 
-        var oldFolder: UniFile? = null
-        val oldFolderName = oldName.find {
-            oldFolder = mangaDir.findFile(it)
-            oldFolder != null
-        }
+        // There should only be one folder with the chapter
+        val oldFolder = oldNames.mapNotNull { mangaDir.findFile(it) }.firstOrNull()
         if (oldFolder?.renameTo(newName) == true) {
             cache.removeChapter(oldChapter, manga)
             cache.addChapter(newName, mangaDir, manga)
         } else {
-            Timber.e("Could not rename downloaded chapter: %s.", oldFolderName)
+            Timber.e("Could not rename downloaded chapter: %s.", oldNames.joinToString())
         }
     }
 }
