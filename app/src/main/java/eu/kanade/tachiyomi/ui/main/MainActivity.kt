5b5895da65bccd79dd47b5cc9a458df92c71a2f9
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2020-08-31 03:24:55 +0100

    Add Black-Red theme (#25)
    * Appveyor
    
    * Add red to dark variants
    
    * Add red theme to strings.xml
    
    * Add red to PreferenceValues
    
    * Add red theme to BaseActivity
    
    * Add red theme
    
    * Correct red theme
    
    * Fix typo
    
    * Add more stuff to red theme
    
    * Update
    
    * Update 2: More Red
    
    * Update 3: More Red
    
    * Update 4: More Red
    
    * Update 5: More red
    
    * Fix typo
    
    * Add ()
    
    * Nav Icon Attempt 2
    
    * final reddening
    
    * Decrease red, remove appveyor

--- app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
@@ -142,7 +142,14 @@ class MainActivity : BaseActivity<MainActivityBinding>() {
         setSupportActionBar(binding.toolbar)
 
         drawerArrow = DrawerArrowDrawable(this)
-        drawerArrow?.color = if ((preferences.themeMode().get() == PreferenceValues.ThemeMode.light || (preferences.themeMode().get() == PreferenceValues.ThemeMode.system && resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK != Configuration.UI_MODE_NIGHT_YES)) && preferences.themeLight().get() == PreferenceValues.LightThemeVariant.default) Color.BLACK else Color.WHITE
+        var navIconColor = if ((preferences.themeMode().get() == PreferenceValues.ThemeMode.light || (preferences.themeMode().get() == PreferenceValues.ThemeMode.system && resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK != Configuration.UI_MODE_NIGHT_YES)) && preferences.themeLight().get() == PreferenceValues.LightThemeVariant.default) Color.BLACK else Color.WHITE
+
+        if (navIconColor == Color.WHITE && preferences.themeDark().get() == PreferenceValues.DarkThemeVariant.red) {
+            navIconColor = Color.RED
+        }
+
+        drawerArrow?.color = navIconColor
+
         binding.toolbar.navigationIcon = drawerArrow
 
         tabAnimator = TabsAnimator(binding.tabs)
