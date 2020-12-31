67b919423aa915b09a677d2367493a781a528201
arkon <eugcheung94@gmail.com>
2020-12-31 16:41:09 -0500

    Remove some unused variables
    (cherry picked from commit dcb6bfb18dca482e0ae8aa4a8d3aea87fb66c7fe)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/SourceAdapter.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/SourceAdapter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/SourceAdapter.kt
@@ -2,18 +2,14 @@ package eu.kanade.tachiyomi.ui.browse.migration.sources
 
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.items.IFlexible
-import eu.kanade.tachiyomi.R
-import eu.kanade.tachiyomi.util.system.getResourceColor
 
 /**
  * Adapter that holds the catalogue cards.
  *
  * @param controller instance of [MigrationSourcesController].
  */
-class SourceAdapter(val controller: MigrationSourcesController) :
+class SourceAdapter(controller: MigrationSourcesController) :
     FlexibleAdapter<IFlexible<*>>(null, controller, true) {
-
-    val cardBackground = controller.activity!!.getResourceColor(R.attr.colorSurface)
 
     init {
         setDisplayHeadersAtStartUp(true)
