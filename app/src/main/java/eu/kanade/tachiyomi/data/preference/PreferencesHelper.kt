Ramsesdb/gexu

Add external repo support

app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
@@ -445,4 +445,6 @@ class PreferencesHelper(val context: Context) {
     fun sortTagsForLibrary() = flowPrefs.getStringSet(Keys.sortTagsForLibrary, mutableSetOf())
 
     fun dontDeleteFromCategories() = flowPrefs.getStringSet(Keys.dontDeleteFromCategories, emptySet())
+
+    fun extensionRepos() = flowPrefs.getStringSet(Keys.dontDeleteFromCategories, emptySet())
 }

app/src/main/java/eu/kanade/tachiyomi/ui/category/repos/RepoAdapter.kt
@@ -1,0 +1,30 @@
+package eu.kanade.tachiyomi.ui.category.repos
+
+import eu.davidea.flexibleadapter.FlexibleAdapter
+
+/**
+ * Custom adapter for repos.
+ *
+ * @param controller The containing controller.
+ */
+class RepoAdapter(controller: RepoController) :
+    FlexibleAdapter<RepoItem>(null, controller, true) {
+
+    /**
+     * Clears the active selections from the list and the model.
+     */
+    override fun clearSelection() {
+        super.clearSelection()
+        (0 until itemCount).forEach { getItem(it)?.isSelected = false }
+    }
+
+    /**
+     * Toggles the selection of the given position.
+     *
+     * @param position The position to toggle.
+     */
+    override fun toggleSelection(position: Int) {
+        super.toggleSelection(position)
+        getItem(position)?.isSelected = isSelected(position)
+    }
+}

app/src/main/java/eu/kanade/tachiyomi/ui/category/repos/RepoHolder.kt
@@ -1,0 +1,28 @@
+package eu.kanade.tachiyomi.ui.category.repos
+
+import android.view.View
+import androidx.core.view.isVisible
+import eu.davidea.viewholders.FlexibleViewHolder
+import eu.kanade.tachiyomi.databinding.CategoriesItemBinding
+
+/**
+ * Holder used to display repo items.
+ *
+ * @param view The view used by repo items.
+ * @param adapter The adapter containing this holder.
+ */
+class RepoHolder(view: View, val adapter: RepoAdapter) : FlexibleViewHolder(view, adapter) {
+
+    private val binding = CategoriesItemBinding.bind(view)
+
+    /**
+     * Binds this holder with the given category.
+     *
+     * @param category The category to bind.
+     */
+    fun bind(category: String) {
+        // Set capitalized title.
+        binding.title.text = category
+        binding.reorder.isVisible = false
+    }
+}

app/src/main/java/eu/kanade/tachiyomi/ui/category/repos/RepoItem.kt
@@ -1,0 +1,62 @@
+package eu.kanade.tachiyomi.ui.category.repos
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
+import eu.davidea.flexibleadapter.items.IFlexible
+import eu.kanade.tachiyomi.R
+
+/**
+ * Repo item for a recycler view.
+ */
+class RepoItem(val repo: String) : AbstractFlexibleItem<RepoHolder>() {
+
+    /**
+     * Whether this item is currently selected.
+     */
+    var isSelected = false
+
+    /**
+     * Returns the layout resource for this item.
+     */
+    override fun getLayoutRes(): Int {
+        return R.layout.categories_item
+    }
+
+    /**
+     * Returns a new view holder for this item.
+     *
+     * @param view The view of this item.
+     * @param adapter The adapter of this item.
+     */
+    override fun createViewHolder(view: View, adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>): RepoHolder {
+        return RepoHolder(view, adapter as RepoAdapter)
+    }
+
+    /**
+     * Binds the given view holder with this item.
+     *
+     * @param adapter The adapter of this item.
+     * @param holder The holder to bind.
+     * @param position The position of this item in the adapter.
+     * @param payloads List of partial changes.
+     */
+    override fun bindViewHolder(
+        adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>,
+        holder: RepoHolder,
+        position: Int,
+        payloads: List<Any?>?
+    ) {
+        holder.bind(repo)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return repo.hashCode()
+    }
+}

app/src/main/java/eu/kanade/tachiyomi/ui/category/repos/RepoPresenter.kt
@@ -1,0 +1,89 @@
+package eu.kanade.tachiyomi.ui.category.repos
+
+import android.os.Bundle
+import eu.kanade.tachiyomi.data.preference.PreferencesHelper
+import eu.kanade.tachiyomi.ui.base.presenter.BasePresenter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+
+/**
+ * Presenter of [RepoController]. Used to manage the repos for the extensions.
+ */
+class RepoPresenter(
+    private val preferences: PreferencesHelper = Injekt.get()
+) : BasePresenter<RepoController>() {
+    val scope = CoroutineScope(Job() + Dispatchers.Main)
+
+    /**
+     * List containing repos.
+     */
+    private var repos: List<String> = emptyList()
+
+    /**
+     * Called when the presenter is created.
+     *
+     * @param savedState The saved state of this presenter.
+     */
+    override fun onCreate(savedState: Bundle?) {
+        super.onCreate(savedState)
+
+        preferences.extensionRepos().asFlow().onEach { repos ->
+            this.repos = repos.toList().sortedBy { it.toLowerCase() }
+
+            Observable.just(this.repos)
+                .map { it.map(::RepoItem) }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeLatestCache(RepoController::setRepos)
+        }.launchIn(scope)
+    }
+
+    /**
+     * Creates and adds a new repo to the database.
+     *
+     * @param name The name of the repo to create.
+     */
+    fun createRepo(name: String) {
+        // Do not allow duplicate repos.
+        if (repoExists(name)) {
+            Observable.just(Unit).subscribeFirst({ view, _ -> view.onRepoExistsError() })
+            return
+        }
+
+        // Do not allow invalid formats
+        if (!name.matches(repoRegex)) {
+            Observable.just(Unit).subscribeFirst({ view, _ -> view.onRepoInvalidNameError() })
+            return
+        }
+
+        preferences.extensionRepos().set((repos + name).toSet())
+    }
+
+    /**
+     * Deletes the given repos from the database.
+     *
+     * @param repos The list of repos to delete.
+     */
+    fun deleteRepos(repos: List<String>) {
+        preferences.extensionRepos().set(
+            this.repos.filterNot { it in repos }.toSet()
+        )
+    }
+
+    /**
+     * Returns true if a repo with the given name already exists.
+     */
+    private fun repoExists(name: String): Boolean {
+        return repos.any { it.equals(name, true) }
+    }
+
+    companion object {
+        val repoRegex = """^[a-zA-Z-_.]*?\/[a-zA-Z-_.]*?$""".toRegex()
+    }
+}

komikku-app/komikku

app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
@@ -366,4 +366,6 @@ class PreferencesHelper(val context: Context) {
     fun startReadingButton() = flowPrefs.getBoolean(Keys.startReadingButton, true)
 
     fun createLegacyBackup() = flowPrefs.getBoolean(Keys.createLegacyBackup, false)
+
+    fun extensionRepos() = flowPrefs.getStringSet(Keys.extensionRepos, emptySet())
 }

app/src/main/java/eu/kanade/tachiyomi/ui/extension/repos/RepoAdapter.kt
@@ -1,0 +1,30 @@
+package eu.kanade.tachiyomi.ui.extension.repos
+
+import eu.davidea.flexibleadapter.FlexibleAdapter
+
+/**
+ * Custom adapter for repos.
+ *
+ * @param controller The containing controller.
+ */
+class RepoAdapter(controller: RepoController) :
+    FlexibleAdapter<RepoItem>(null, controller, true) {
+
+    /**
+     * Clears the active selections from the list and the model.
+     */
+    override fun clearSelection() {
+        super.clearSelection()
+        (0 until itemCount).forEach { getItem(it)?.isSelected = false }
+    }
+
+    /**
+     * Toggles the selection of the given position.
+     *
+     * @param position The position to toggle.
+     */
+    override fun toggleSelection(position: Int) {
+        super.toggleSelection(position)
+        getItem(position)?.isSelected = isSelected(position)
+    }
+}

app/src/main/java/eu/kanade/tachiyomi/ui/extension/repos/RepoHolder.kt
@@ -1,0 +1,28 @@
+package eu.kanade.tachiyomi.ui.extension.repos
+
+import android.view.View
+import androidx.core.view.isVisible
+import eu.davidea.viewholders.FlexibleViewHolder
+import eu.kanade.tachiyomi.databinding.CategoriesItemBinding
+
+/**
+ * Holder used to display repo items.
+ *
+ * @param view The view used by repo items.
+ * @param adapter The adapter containing this holder.
+ */
+class RepoHolder(view: View, val adapter: RepoAdapter) : FlexibleViewHolder(view, adapter) {
+
+    private val binding = CategoriesItemBinding.bind(view)
+
+    /**
+     * Binds this holder with the given category.
+     *
+     * @param category The category to bind.
+     */
+    fun bind(category: String) {
+        // Set capitalized title.
+        binding.title.text = category
+        binding.reorder.isVisible = false
+    }
+}

app/src/main/java/eu/kanade/tachiyomi/ui/extension/repos/RepoItem.kt
@@ -1,0 +1,62 @@
+package eu.kanade.tachiyomi.ui.extension.repos
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
+import eu.davidea.flexibleadapter.items.IFlexible
+import eu.kanade.tachiyomi.R
+
+/**
+ * Repo item for a recycler view.
+ */
+class RepoItem(val repo: String) : AbstractFlexibleItem<RepoHolder>() {
+
+    /**
+     * Whether this item is currently selected.
+     */
+    var isSelected = false
+
+    /**
+     * Returns the layout resource for this item.
+     */
+    override fun getLayoutRes(): Int {
+        return R.layout.categories_item
+    }
+
+    /**
+     * Returns a new view holder for this item.
+     *
+     * @param view The view of this item.
+     * @param adapter The adapter of this item.
+     */
+    override fun createViewHolder(view: View, adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>): RepoHolder {
+        return RepoHolder(view, adapter as RepoAdapter)
+    }
+
+    /**
+     * Binds the given view holder with this item.
+     *
+     * @param adapter The adapter of this item.
+     * @param holder The holder to bind.
+     * @param position The position of this item in the adapter.
+     * @param payloads List of partial changes.
+     */
+    override fun bindViewHolder(
+        adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>,
+        holder: RepoHolder,
+        position: Int,
+        payloads: List<Any?>?
+    ) {
+        holder.bind(repo)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return repo.hashCode()
+    }
+}

app/src/main/java/eu/kanade/tachiyomi/ui/extension/repos/RepoPresenter.kt
@@ -1,0 +1,90 @@
+package eu.kanade.tachiyomi.ui.extension.repos
+
+import android.os.Bundle
+import eu.kanade.tachiyomi.data.preference.PreferencesHelper
+import eu.kanade.tachiyomi.ui.base.presenter.BasePresenter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+
+/**
+ * Presenter of [RepoController]. Used to manage the repos for the extensions.
+ */
+class RepoPresenter(
+    private val preferences: PreferencesHelper = Injekt.get()
+) : BasePresenter<RepoController>() {
+    val scope = CoroutineScope(Job() + Dispatchers.Main)
+
+    /**
+     * List containing repos.
+     */
+    private var repos: List<String> = emptyList()
+
+    /**
+     * Called when the presenter is created.
+     *
+     * @param savedState The saved state of this presenter.
+     */
+    override fun onCreate(savedState: Bundle?) {
+        super.onCreate(savedState)
+
+        preferences.extensionRepos().asFlow().onEach { repos ->
+            this.repos = repos.toList().sortedBy { it.toLowerCase() }
+
+            Observable.just(this.repos)
+                .map { it.map(::RepoItem) }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeLatestCache(RepoController::setRepos)
+        }.launchIn(scope)
+    }
+
+    /**
+     * Creates and adds a new repo to the database.
+     *
+     * @param name The name of the repo to create.
+     */
+    fun createRepo(name: String) {
+        // Do not allow duplicate repos.
+        if (repoExists(name)) {
+            Observable.just(Unit).subscribeFirst({ view, _ -> view.onRepoExistsError() })
+            return
+        }
+
+        // Do not allow invalid formats
+        if (!name.matches(repoRegex)) {
+            Observable.just(Unit).subscribeFirst({ view, _ -> view.onRepoInvalidNameError() })
+            return
+        }
+
+        preferences.extensionRepos().set((repos + name).toSet())
+    }
+
+    /**
+     * Deletes the given repos from the database.
+     *
+     * @param repos The list of repos to delete.
+     */
+    fun deleteRepos(repos: List<String>) {
+        preferences.extensionRepos().set(
+            this.repos.filterNot { it in repos }.toSet()
+        )
+    }
+
+    /**
+     * Returns true if a repo with the given name already exists.
+     */
+    private fun repoExists(name: String): Boolean {
+        return repos.any { it.equals(name, true) }
+    }
+
+    companion object {
+        val repoRegex =
+            """^[a-zA-Z-_.]*?\/[a-zA-Z-_.]*?$""".toRegex()
+    }
+}