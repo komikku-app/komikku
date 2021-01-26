a088e1ffc2e49aad60b53f310dad6f16f239fb72
Jobobby04 <jobobby04@users.noreply.github.com>
2021-01-25 21:33:49 -0500

    Cleanup removeArticals extension function

--- app/src/main/java/exh/util/StringUtil.kt
+++ app/src/main/java/exh/util/StringUtil.kt
@@ -4,8 +4,10 @@ fun List<String>.trimAll() = map { it.trim() }
 fun List<String>.dropBlank() = filter { it.isNotBlank() }
 fun List<String>.dropEmpty() = filter { it.isNotEmpty() }
 
+private val articleRegex by lazy { "^(an|a|the) ".toRegex(RegexOption.IGNORE_CASE) }
+
 fun String.removeArticles(): String {
-    return this.replace(Regex("^(an|a|the) ", RegexOption.IGNORE_CASE), "")
+    return this.replace(articleRegex, "")
 }
 
 fun String.trimOrNull(): String? {
