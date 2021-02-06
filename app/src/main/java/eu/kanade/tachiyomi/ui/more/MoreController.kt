aadfa2aa8cd891e81580f8c4daca530fdf498c0a
arkon <eugcheung94@gmail.com>
2021-02-06 13:09:56 -0500

    Show help action when source fails to load
    (cherry picked from commit d84b5e8b461bb8c9c0e6c684dc717e27ba40f6c5)

--- app/src/main/java/eu/kanade/tachiyomi/ui/more/MoreController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/more/MoreController.kt
@@ -164,6 +164,6 @@ class MoreController :
     }
 
     companion object {
-        private const val URL_HELP = "https://tachiyomi.org/help/"
+        const val URL_HELP = "https://tachiyomi.org/help/"
     }
 }
