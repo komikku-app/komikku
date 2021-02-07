e25991544e02b8d9b90e29fc564a3cec5466cafb
arkon <eugcheung94@gmail.com>
2021-02-07 16:55:44 -0500

    Add Esperanto locale
    (cherry picked from commit b85cd86b24a69cd94c7c0bd9e3740c8d9c7cdd0c)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsGeneralController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsGeneralController.kt
@@ -183,6 +183,7 @@ class SettingsGeneralController : SettingsController() {
                     "cv",
                     "de",
                     "el",
+                    "eo",
                     "es",
                     "es-419",
                     "en-US",
