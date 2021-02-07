d4de925ec7348d86039c076b2ca7a7d1bbedaa8a
Andreas <andreas.everos@gmail.com>
2021-02-07 05:26:56 +0100

    Add Right and Left navigation (#4392)
    and remove default navigation classes in favor of the navigation classes
    
    (cherry picked from commit b63d24ac1ab1253a62b6a2886f11953539f13340)

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/webtoon/WebtoonConfig.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/webtoon/WebtoonConfig.kt
@@ -6,6 +6,7 @@ import eu.kanade.tachiyomi.ui.reader.viewer.ViewerNavigation
 import eu.kanade.tachiyomi.ui.reader.viewer.navigation.EdgeNavigation
 import eu.kanade.tachiyomi.ui.reader.viewer.navigation.KindlishNavigation
 import eu.kanade.tachiyomi.ui.reader.viewer.navigation.LNavigation
+import eu.kanade.tachiyomi.ui.reader.viewer.navigation.RightAndLeftNavigation
 import kotlinx.coroutines.CoroutineScope
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
@@ -62,7 +63,7 @@ class WebtoonConfig(
         }
 
     override fun defaultNavigation(): ViewerNavigation {
-        return WebtoonDefaultNavigation()
+        return LNavigation()
     }
 
     override fun updateNavigation(navigationMode: Int) {
@@ -71,6 +72,7 @@ class WebtoonConfig(
             1 -> LNavigation()
             2 -> KindlishNavigation()
             3 -> EdgeNavigation()
+            4 -> RightAndLeftNavigation()
             else -> defaultNavigation()
         }
     }
