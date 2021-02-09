37ce2140f339dad33735ff2117e4b7fa1bb7bd23
vance <32921531+rawfh@users.noreply.github.com>
2021-02-10 06:54:44 +0800

    Added dual page split setting (#4252)
    * Add DualPageSplit option
    
    * remove extra line
    
    * Split double-page into two pages
    
    * Remove !isAnimated check and add (ALPHA) to the label
    
    * Fix missing insert pages
    
    * Pager cleanup
    
    * Add dual split to Webtoon and fix Vertical
    
    * Fix L2R/R2L
    
    * Add comments and refactor code in ImageUtil
    
    * Use a simpler split solution in webtoon mode
    
    Co-authored-by: weng <>
    Co-authored-by: Andreas E <andreas.everos@gmail.com>
    (cherry picked from commit b5017eebbf0740189f254d5db25df3c54a282e1f)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/pager/PagerPageHolder.kt
    #	app/src/main/java/eu/kanade/tachiyomi/util/system/ImageUtil.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/model/ReaderPage.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/model/ReaderPage.kt
@@ -4,7 +4,7 @@ import android.graphics.drawable.Drawable
 import eu.kanade.tachiyomi.source.model.Page
 import java.io.InputStream
 
-class ReaderPage(
+open class ReaderPage(
     index: Int,
     url: String = "",
     imageUrl: String? = null,
@@ -16,5 +16,5 @@ class ReaderPage(
 
 ) : Page(index, url, imageUrl, null) {
 
-    lateinit var chapter: ReaderChapter
+    open lateinit var chapter: ReaderChapter
 }
