a207b4b7292ccaccad25d4ff98b955a5229894e0
Jobobby04 <jobobby04@users.noreply.github.com>
2021-02-11 19:12:50 -0500

    Fix build after AboutLibraries update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -305,6 +305,8 @@ dependencies {
     implementation("com.squareup.moshi:moshi:1.11.0")
 
     implementation("androidx.gridlayout:gridlayout:1.0.0")
+
+    implementation("com.mikepenz:fastadapter:5.3.4")
     // SY -->
 }
 
