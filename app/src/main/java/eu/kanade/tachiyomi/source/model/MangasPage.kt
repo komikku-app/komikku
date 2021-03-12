c600d45e84d6966d8b806ac500943a9556bd5d6d
Jobobby04 <jobobby04@users.noreply.github.com>
2021-03-11 20:29:21 -0500

    Maybe fix EHentai dupes in browse issue

--- app/src/main/java/eu/kanade/tachiyomi/source/model/MangasPage.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/model/MangasPage.kt
@@ -2,8 +2,26 @@ package eu.kanade.tachiyomi.source.model
 
 import exh.metadata.metadata.base.RaisedSearchMetadata
 
-/* SY --> */ open /* SY <-- */ class MangasPage(val mangas: List<SManga>, val hasNextPage: Boolean)
+/* SY --> */ open /* SY <-- */ class MangasPage(open val mangas: List<SManga>, open val hasNextPage: Boolean) {
+    // SY -->
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is MangasPage) return false
+
+        if (mangas != other.mangas) return false
+        if (hasNextPage != other.hasNextPage) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = mangas.hashCode()
+        result = 31 * result + hasNextPage.hashCode()
+        return result
+    }
+    // SY <--
+}
 
 // SY -->
-class MetadataMangasPage(mangas: List<SManga>, hasNextPage: Boolean, val mangasMetadata: List<RaisedSearchMetadata>) : MangasPage(mangas, hasNextPage)
+data class MetadataMangasPage(override val mangas: List<SManga>, override val hasNextPage: Boolean, val mangasMetadata: List<RaisedSearchMetadata>) : MangasPage(mangas, hasNextPage)
 // SY <--
