6fa67c9a5fdfcfb8da8b180083e4ead0a6e2c528
Jobobby04 <jobobby04@users.noreply.github.com>
2021-04-11 21:51:55 -0400

    Release 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         targetSdkVersion(AndroidConfig.targetSdk)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         versionCode = 14
-        versionName = "1.5.0"
+        versionName = "1.6.0"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
