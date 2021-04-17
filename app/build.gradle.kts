fb5872ef51189b57c72890cbada1d5a5bf98af68
arkon <eugcheung94@gmail.com>
2021-04-16 22:27:00 -0400

    Case insensitive source directory search
    (cherry picked from commit a59e134862be796f5409d7fb535a2dd5225a8ead)
    
    # Conflicts:
    #	app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -181,7 +181,7 @@ dependencies {
 
     // Disk
     implementation("com.jakewharton:disklrucache:2.0.2")
-    implementation("com.github.inorichi:unifile:e9ee588")
+    implementation("com.github.tachiyomiorg:unifile:17bec43")
     implementation("com.github.junrar:junrar:7.4.0")
 
     // HTML parser
