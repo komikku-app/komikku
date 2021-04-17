08f0e515d567d0c36097c7983bf5150009d84454
arkon <eugcheung94@gmail.com>
2021-04-17 11:06:30 -0400

    Use DSL for creating chapter description spanned string
    (cherry picked from commit 6dbc0a6fd50f0b97a0798792d482aa6657a7ea11)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/manga/chapter/ChapterHolder.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/chapter/ChapterHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/chapter/ChapterHolder.kt
@@ -1,9 +1,9 @@
 package eu.kanade.tachiyomi.ui.manga.chapter
 
-import android.text.SpannableString
 import android.text.SpannableStringBuilder
-import android.text.style.ForegroundColorSpan
 import android.view.View
+import androidx.core.text.buildSpannedString
+import androidx.core.text.color
 import androidx.core.view.isVisible
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.database.models.Manga
@@ -64,8 +64,10 @@ class ChapterHolder(
             } else /* SY <-- */ descriptions.add(adapter.dateFormat.format(Date(chapter.date_upload)))
         }
         if ((!chapter.read || (adapter.preserveReadingPosition && manga.isEhBasedManga())) && chapter.last_page_read > 0) {
-            val lastPageRead = SpannableString(itemView.context.getString(R.string.chapter_progress, chapter.last_page_read + 1)).apply {
-                setSpan(ForegroundColorSpan(adapter.readColor), 0, length, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+            val lastPageRead = buildSpannedString {
+                color(adapter.readColor) {
+                    append(itemView.context.getString(R.string.chapter_progress, chapter.last_page_read + 1))
+                }
             }
             descriptions.add(lastPageRead)
         }
