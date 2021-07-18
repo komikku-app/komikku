02586404d7dbd18ec3cc75a9697947a0fa72c0af
arkon <eugcheung94@gmail.com>
2021-07-18 15:00:07 -0400

    Remove autoAddTrack preference
    (cherry picked from commit dce30494467429a0c578386508f1c253fc6d7799)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaController.kt
@@ -712,7 +712,7 @@ class MangaController :
             }
         }
 
-        if (source != null && preferences.autoAddTrack()) {
+        if (source != null) {
             presenter.trackList
                 .map { it.service }
                 .filterIsInstance<EnhancedTrackService>()
