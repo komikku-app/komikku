e422993c2ed266547d850c3ffd781dd2aa79ddce
arkon <eugcheung94@gmail.com>
2021-07-24 11:54:53 -0400

    Remove unnecessary MultiDex library
    (cherry picked from commit f2fca0f13d67ca1800b59ca807280316d700d55d)
    
    # Conflicts:
    #	app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,6 @@ android {
         buildConfigField("String", "BUILD_TIME", "\"${getBuildTime()}\"")
         buildConfigField("boolean", "INCLUDE_UPDATER", "false")
 
-        multiDexEnabled = true
-
         ndk {
             abiFilters += setOf("armeabi-v7a", "arm64-v8a", "x86")
         }
@@ -139,7 +137,6 @@ dependencies {
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.1.0")
     implementation("androidx.core:core-ktx:1.7.0-alpha01")
     implementation("androidx.core:core-splashscreen:1.0.0-alpha01")
-    implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.preference:preference-ktx:1.1.1")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
@@ -260,7 +257,6 @@ dependencies {
 
     val robolectricVersion = "3.1.4"
     testImplementation("org.robolectric:robolectric:$robolectricVersion")
-    testImplementation("org.robolectric:shadows-multidex:$robolectricVersion")
     testImplementation("org.robolectric:shadows-play-services:$robolectricVersion")
 
     // For detecting memory leaks; see https://square.github.io/leakcanary/
