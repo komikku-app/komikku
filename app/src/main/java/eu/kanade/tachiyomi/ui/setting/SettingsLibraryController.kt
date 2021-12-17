cb00bf66cf409a790a15fbaeb89ed0232d55a035
arkon <eugcheung94@gmail.com>
2021-12-17 14:14:48 -0500

    Adjust global update preference wording and visibility
    (cherry picked from commit afc80d6a7c5e0e8cdeff713616e126d9313afa7c)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsLibraryController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsLibraryController.kt
@@ -228,8 +228,6 @@ class SettingsLibraryController : SettingsController() {
                 entryValues = arrayOf(MANGA_FULLY_READ, MANGA_ONGOING)
                 defaultValue = preferences.libraryUpdateMangaRestriction().defaultValue
 
-                visibleIfGlobalUpdateEnabled()
-
                 fun updateSummary() {
                     val restrictions = preferences.libraryUpdateMangaRestriction().get()
                         .sorted()
@@ -256,8 +254,6 @@ class SettingsLibraryController : SettingsController() {
             preference {
                 key = Keys.libraryUpdateCategories
                 titleRes = R.string.categories
-
-                visibleIfGlobalUpdateEnabled()
 
                 onClick {
                     LibraryGlobalUpdateCategoriesDialog().showDialog(router)
@@ -319,7 +315,6 @@ class SettingsLibraryController : SettingsController() {
                 titleRes = R.string.pref_library_update_refresh_metadata
                 summaryRes = R.string.pref_library_update_refresh_metadata_summary
                 defaultValue = false
-                visibleIfGlobalUpdateEnabled()
             }
             if (trackManager.hasLoggedServices()) {
                 switchPreference {
@@ -327,7 +322,6 @@ class SettingsLibraryController : SettingsController() {
                     titleRes = R.string.pref_library_update_refresh_trackers
                     summaryRes = R.string.pref_library_update_refresh_trackers_summary
                     defaultValue = false
-                    visibleIfGlobalUpdateEnabled()
                 }
             }
         }
