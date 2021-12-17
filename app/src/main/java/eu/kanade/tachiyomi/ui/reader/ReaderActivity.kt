22a6c8d7729a0373a6abf073760c804f961145a6
Ivan Iskandar <12537387+ivaniskandar@users.noreply.github.com>
2021-12-17 10:57:31 +0700

    Use elevated overlay on reader menu (#6347)
    (cherry picked from commit 6b52fc1e2d0c23ec1224e654ee36de032661f6e2)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt
@@ -14,7 +14,7 @@ import android.graphics.Color
 import android.graphics.ColorMatrix
 import android.graphics.ColorMatrixColorFilter
 import android.graphics.Paint
-import android.graphics.PorterDuff
+import android.graphics.drawable.RippleDrawable
 import android.os.Build
 import android.os.Bundle
 import android.view.Gravity
@@ -91,6 +91,7 @@ import eu.kanade.tachiyomi.util.system.isLTR
 import eu.kanade.tachiyomi.util.system.isNightMode
 import eu.kanade.tachiyomi.util.system.logcat
 import eu.kanade.tachiyomi.util.system.toast
+import eu.kanade.tachiyomi.util.view.copy
 import eu.kanade.tachiyomi.util.view.popupMenu
 import eu.kanade.tachiyomi.util.view.setTooltip
 import eu.kanade.tachiyomi.widget.listener.SimpleAnimationListener
@@ -509,25 +510,34 @@ class ReaderActivity : BaseRxActivity<ReaderActivityBinding, ReaderPresenter>() 
         updateBottomButtons()
 
         initDropdownMenu()
-        // <-- EH
 
-        val alpha = if (isNightMode()) 230 else 242 // 90% dark 95% light
-        val toolbarColor = ColorUtils.setAlphaComponent(getThemeColor(R.attr.colorToolbar), alpha)
-        listOf(
-            binding.toolbarBottom,
-            binding.leftChapter,
-            binding.readerSeekbar,
-            binding.rightChapter,
-            // SY -->
-            binding.readerSeekbarVert,
-            binding.aboveChapter,
-            binding.belowChapter
-            // SY <--
-        ).forEach {
-            it.backgroundTintMode = PorterDuff.Mode.DST_IN
-            it.backgroundTintList = ColorStateList.valueOf(toolbarColor)
+        val toolbarBackground = (binding.toolbar.background as MaterialShapeDrawable).apply {
+            elevation = resources.getDimension(R.dimen.m3_sys_elevation_level2)
+            alpha = if (isNightMode()) 230 else 242 // 90% dark 95% light
+        }
+        binding.toolbarBottom.background = toolbarBackground.copy(this@ReaderActivity)
+
+        binding.readerSeekbar.background = toolbarBackground.copy(this@ReaderActivity)?.apply {
+            setCornerSize(999F)
+        }
+        // SY -->
+        binding.readerSeekbarVert.background = toolbarBackground.copy(this@ReaderActivity)?.apply {
+            setCornerSize(999F)
+        }
+        // SY <--
+        listOf(binding.leftChapter, binding.rightChapter /* SY --> */, binding.belowChapter, binding.aboveChapter /* SY <-- */).forEach {
+            it.background = binding.readerSeekbar.background.copy(this)
+            it.foreground = RippleDrawable(
+                ColorStateList.valueOf(getThemeColor(android.R.attr.colorControlHighlight)),
+                null,
+                it.background
+            )
         }
 
+        val toolbarColor = ColorUtils.setAlphaComponent(
+            toolbarBackground.resolvedTintColor,
+            toolbarBackground.alpha
+        )
         window.statusBarColor = toolbarColor
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             window.navigationBarColor = toolbarColor
