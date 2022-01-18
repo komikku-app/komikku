324d74f6c30b2a249cce1c12890eb17c2e8f77cf
arkon <eugcheung94@gmail.com>
2022-01-18 17:51:59 -0500

    Remove some dead code
    (cherry picked from commit edd213343b9b524d0cbb05165969b559c24e401c)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt

--- app/src/main/java/eu/kanade/tachiyomi/source/online/HttpSource.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/online/HttpSource.kt
@@ -59,13 +59,6 @@ abstract class HttpSource : CatalogueSource {
     }
     // SY <--
 
-//    /**
-//     * Preferences that a source may need.
-//     */
-//    val preferences: SharedPreferences by lazy {
-//        Injekt.get<Application>().getSharedPreferences(source.getPreferenceKey(), Context.MODE_PRIVATE)
-//    }
-
     /**
      * Base url of the website without the trailing slash, like: http://mysite.com
      */
