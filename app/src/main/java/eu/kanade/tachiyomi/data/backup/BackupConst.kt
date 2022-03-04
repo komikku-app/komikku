Ramsesdb/gexu-legacy-fork

Use existing worker for manual backup creation (#6718)
* Use existing worker for manual backup creation

This will show the "creating backup" notification when auto backup is
running. Complete or error notification will continue to be shown only on
manual job.

* Make sure disabling auto backup don't cancel running manual backup job

app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupConst.kt
@@ -11,4 +11,15 @@ object BackupConst {
 
     const val BACKUP_TYPE_LEGACY = 0
     const val BACKUP_TYPE_FULL = 1
+
+    // Filter options
+    internal const val BACKUP_CATEGORY = 0x1
+    internal const val BACKUP_CATEGORY_MASK = 0x1
+    internal const val BACKUP_CHAPTER = 0x2
+    internal const val BACKUP_CHAPTER_MASK = 0x2
+    internal const val BACKUP_HISTORY = 0x4
+    internal const val BACKUP_HISTORY_MASK = 0x4
+    internal const val BACKUP_TRACK = 0x8
+    internal const val BACKUP_TRACK_MASK = 0x8
+    internal const val BACKUP_ALL = 0xF
 }

komikku-app/komikku

(cherry picked from commit d53bb4c337da6df807b43295d2991e8af9015098)
# Conflicts:
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/AbstractBackupManager.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupCreateService.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/full/FullBackupManager.kt
#	app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsBackupController.kt

app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupConst.kt
@@ -11,4 +11,22 @@ object BackupConst {
 
     const val BACKUP_TYPE_LEGACY = 0
     const val BACKUP_TYPE_FULL = 1
+
+    // Filter options
+    internal const val BACKUP_CATEGORY = 0x1
+    internal const val BACKUP_CATEGORY_MASK = 0x1
+    internal const val BACKUP_CHAPTER = 0x2
+    internal const val BACKUP_CHAPTER_MASK = 0x2
+    internal const val BACKUP_HISTORY = 0x4
+    internal const val BACKUP_HISTORY_MASK = 0x4
+    internal const val BACKUP_TRACK = 0x8
+    internal const val BACKUP_TRACK_MASK = 0x8
+
+    // SY -->
+    internal const val BACKUP_CUSTOM_INFO = 0x10
+    internal const val BACKUP_CUSTOM_INFO_MASK = 0x10
+    internal const val BACKUP_READ_MANGA = 0x20
+    internal const val BACKUP_READ_MANGA_MASK = 0x20
+    internal const val BACKUP_ALL = 0x3F
+    // SY <--
 }