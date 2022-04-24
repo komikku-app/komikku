8b11357effdac0b7f2e6194ff8baa5c974ec4da6
Jobobby04 <jobobby04@users.noreply.github.com>
2022-04-23 20:43:02 -0400

    Separate EHTags into multiple files
    (cherry picked from commit c51e8c7ab4ee57ab78e86a4c3021ff637490f647)

---
+++ app/src/main/java/exh/eh/tags/TagList.kt
@@ -0,0 +1,18 @@
+package exh.eh.tags
+
+interface TagList {
+    fun getTags1(): List<String>
+
+    fun getTags2(): List<String> = emptyList()
+
+    fun getTags3(): List<String> = emptyList()
+
+    fun getTags4(): List<String> = emptyList()
+
+    fun getTags() = listOf(
+        getTags1(),
+        getTags2(),
+        getTags3(),
+        getTags4()
+    )
+}
