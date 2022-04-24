c51e8c7ab4ee57ab78e86a4c3021ff637490f647
Jobobby04 <jobobby04@users.noreply.github.com>
2022-04-23 20:43:02 -0400

    Separate EHTags into multiple files

---
+++ app/src/main/java/exh/eh/tags/Mixed.kt
@@ -0,0 +1,24 @@
+package exh.eh.tags
+
+object Mixed : TagList {
+
+    override fun getTags1() = listOf(
+        "mixed:animal on animal",
+        "mixed:body swap",
+        "mixed:ffm threesome",
+        "mixed:frottage",
+        "mixed:group",
+        "mixed:incest",
+        "mixed:mmf threesome",
+        "mixed:mmt threesome",
+        "mixed:mtf threesome",
+        "mixed:multimouth blowjob",
+        "mixed:multiple assjob",
+        "mixed:multiple footjob",
+        "mixed:multiple handjob",
+        "mixed:oyakodon",
+        "mixed:shimaidon",
+        "mixed:ttm threesome",
+        "mixed:twins",
+    )
+}
---
+++ app/src/main/java/exh/eh/tags/ReClass.kt
@@ -0,0 +1,17 @@
+package exh.eh.tags
+
+object ReClass : TagList {
+
+    override fun getTags1() = listOf(
+        "reclass:artistcg",
+        "reclass:asianporn",
+        "reclass:cosplay",
+        "reclass:doujinshi",
+        "reclass:gamecg",
+        "reclass:imageset",
+        "reclass:manga",
+        "reclass:misc",
+        "reclass:non-h",
+        "reclass:western",
+    )
+}
