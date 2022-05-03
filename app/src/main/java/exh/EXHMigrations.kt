ea20b44865122c29057ef40a5b13410ea61f6ec6
FourTOne5 <59261191+FourTOne5@users.noreply.github.com>
2022-05-04 04:39:04 +0600

    Reword and Fix library sort (#7068)
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/library/setting/SortModeSetting.kt

--- app/src/main/java/exh/EXHMigrations.kt
+++ app/src/main/java/exh/EXHMigrations.kt
@@ -314,11 +314,11 @@ object EXHMigrations {
                     val newSortingMode = when (oldSortingMode) {
                         LibrarySort.ALPHA -> SortModeSetting.ALPHABETICAL
                         LibrarySort.LAST_READ -> SortModeSetting.LAST_READ
-                        LibrarySort.LAST_CHECKED -> SortModeSetting.LAST_CHECKED
-                        LibrarySort.UNREAD -> SortModeSetting.UNREAD
+                        LibrarySort.LAST_CHECKED -> SortModeSetting.LAST_MANGA_UPDATE
+                        LibrarySort.UNREAD -> SortModeSetting.UNREAD_COUNT
                         LibrarySort.TOTAL -> SortModeSetting.TOTAL_CHAPTERS
                         LibrarySort.LATEST_CHAPTER -> SortModeSetting.LATEST_CHAPTER
-                        LibrarySort.CHAPTER_FETCH_DATE -> SortModeSetting.DATE_FETCHED
+                        LibrarySort.CHAPTER_FETCH_DATE -> SortModeSetting.CHAPTER_FETCH_DATE
                         LibrarySort.DATE_ADDED -> SortModeSetting.DATE_ADDED
                         LibrarySort.DRAG_AND_DROP -> SortModeSetting.DRAG_AND_DROP
                         LibrarySort.TAG_LIST -> SortModeSetting.TAG_LIST
