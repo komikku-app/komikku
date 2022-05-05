7b470640b6c6514d0f10b9c9e5b6ce7684ad5b13
Jobobby04 <jobobby04@users.noreply.github.com>
2022-05-04 23:15:16 -0400

    Hide hidden sources from sources filter

--- app/src/main/java/eu/kanade/domain/source/interactor/GetLanguagesWithSources.kt
+++ app/src/main/java/eu/kanade/domain/source/interactor/GetLanguagesWithSources.kt
@@ -4,6 +4,7 @@ import eu.kanade.domain.source.model.Source
 import eu.kanade.domain.source.repository.SourceRepository
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
 import eu.kanade.tachiyomi.util.system.LocaleHelper
+import exh.source.BlacklistedSources
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 
@@ -18,7 +19,7 @@ class GetLanguagesWithSources(
             preferences.disabledSources().asFlow(),
             repository.getOnlineSources()
         ) { enabledLanguage, disabledSource, onlineSources ->
-            val sortedSources = onlineSources.sortedWith(
+            val sortedSources = onlineSources.filterNot { it.id in BlacklistedSources.HIDDEN_SOURCES }.sortedWith(
                 compareBy<Source> { it.id.toString() in disabledSource }
                     .thenBy(String.CASE_INSENSITIVE_ORDER) { it.name }
             )
