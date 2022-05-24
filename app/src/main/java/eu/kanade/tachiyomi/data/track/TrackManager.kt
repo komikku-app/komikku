d7a93e254f1b7d658b33c814340f7cb0f1c50e58
Andreas <andreas.everos@gmail.com>
2022-05-25 00:00:33 +0200

    Add MangaUpdates as a tracker (#7170)
    * Add MangaUpdates as a tracker
    
    - jobobby04 co-authored for suggestion in BackupTracking.kt
    
    Co-authored-by: jobobby04 <jobobby04@users.noreply.github.com>
    
    * Changes from code review
    
    Co-authored-by: arkon <arkon@users.noreply.github.com>
    
    Co-authored-by: jobobby04 <jobobby04@users.noreply.github.com>
    Co-authored-by: arkon <arkon@users.noreply.github.com>
    (cherry picked from commit 0c631a499025aee66354caec690b68cea4dedfdc)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/data/track/TrackManager.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/track/TrackManager.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/TrackManager.kt
@@ -6,6 +6,7 @@ import eu.kanade.tachiyomi.data.track.anilist.Anilist
 import eu.kanade.tachiyomi.data.track.bangumi.Bangumi
 import eu.kanade.tachiyomi.data.track.kitsu.Kitsu
 import eu.kanade.tachiyomi.data.track.komga.Komga
+import eu.kanade.tachiyomi.data.track.mangaupdates.MangaUpdates
 import eu.kanade.tachiyomi.data.track.mdlist.MdList
 import eu.kanade.tachiyomi.data.track.myanimelist.MyAnimeList
 import eu.kanade.tachiyomi.data.track.shikimori.Shikimori
@@ -19,6 +20,7 @@ class TrackManager(context: Context) {
         const val SHIKIMORI = 4
         const val BANGUMI = 5
         const val KOMGA = 6
+        const val MANGA_UPDATES = 7
 
         // SY --> Mangadex from Neko
         const val MDLIST = 60
@@ -49,7 +51,9 @@ class TrackManager(context: Context) {
 
     val komga = Komga(context, KOMGA)
 
-    val services = listOf(mdList, myAnimeList, aniList, kitsu, shikimori, bangumi, komga)
+    val mangaUpdates = MangaUpdates(context, MANGA_UPDATES)
+
+    val services = listOf(mdList, myAnimeList, aniList, kitsu, shikimori, bangumi, komga, mangaUpdates)
 
     fun getService(id: Int) = services.find { it.id == id }
 
