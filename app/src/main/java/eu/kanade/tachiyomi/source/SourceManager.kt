70aeaccbee90d1794b489edd913605ddb3f3b9e3
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2022-07-15 03:17:31 +0600

    Little cleanup for stub source (#7519)
    * Little cleanup for stub source
    
    Fixes instances where name shows up blank for stub sources
    
    * Review Changes
    
    Co-authored-by: Andreas <andreas.everos@gmail.com>
    
    Co-authored-by: Andreas <andreas.everos@gmail.com>
    (cherry picked from commit 902bb35ba7f56f06660985dda0f7f8abf41ee669)

--- app/src/main/java/eu/kanade/tachiyomi/source/SourceManager.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/SourceManager.kt
@@ -224,11 +224,11 @@ class SourceManager(private val context: Context) {
     @Suppress("OverridingDeprecatedMember")
     open inner class StubSource(val sourceData: SourceData) : Source {
 
-        override val name: String = sourceData.name
+        override val id: Long = sourceData.id
+
+        override val name: String = sourceData.name.ifBlank { id.toString() }
 
         override val lang: String = sourceData.lang
-
-        override val id: Long = sourceData.id
 
         override suspend fun getMangaDetails(manga: MangaInfo): MangaInfo {
             throw getSourceNotInstalledException()
@@ -255,10 +255,7 @@ class SourceManager(private val context: Context) {
         }
 
         override fun toString(): String {
-            if (name.isNotBlank() && lang.isNotBlank()) {
-                return "$name (${lang.uppercase()})"
-            }
-            return id.toString()
+            return if (sourceData.isMissingInfo.not()) "$name (${lang.uppercase()})" else id.toString()
         }
 
         fun getSourceNotInstalledException(): SourceNotInstalledException {
