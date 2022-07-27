ffc24b9d140f5451abdafeab7658b7b7b0c6b5e2
Andreas <andreas.everos@gmail.com>
2022-07-27 15:00:09 +0200

    Wait for library to load before creating Pager (#7623)
    (cherry picked from commit f8d8cf9f6a27c536ad3126de8d2d5de373248e04)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/presentation/library/LibraryScreen.kt

--- app/src/main/java/eu/kanade/presentation/library/LibraryScreen.kt
+++ app/src/main/java/eu/kanade/presentation/library/LibraryScreen.kt
@@ -1,11 +1,13 @@
 package eu.kanade.presentation.library
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import eu.kanade.domain.category.model.Category
 import eu.kanade.presentation.components.LibraryBottomActionMenu
+import eu.kanade.presentation.components.LoadingScreen
 import eu.kanade.presentation.components.Scaffold
 import eu.kanade.presentation.library.components.LibraryContent
 import eu.kanade.presentation.library.components.LibraryToolbar
@@ -36,60 +38,65 @@ fun LibraryScreen(
     onOpenReader: (LibraryManga) -> Unit,
     // SY <--
 ) {
-    Scaffold(
-        modifier = Modifier.safeDrawingPadding(),
-        topBar = {
-            val title by presenter.getToolbarTitle()
-            LibraryToolbar(
-                state = presenter,
-                title = title,
-                onClickUnselectAll = onClickUnselectAll,
-                onClickSelectAll = onClickSelectAll,
-                onClickInvertSelection = onClickInvertSelection,
-                onClickFilter = onClickFilter,
-                onClickRefresh = { onClickRefresh(null) },
-                // SY -->
-                onClickSyncExh = onClickSyncExh,
-                // SY <--
-            )
-        },
-        bottomBar = {
-            LibraryBottomActionMenu(
-                visible = presenter.selectionMode,
-                onChangeCategoryClicked = onChangeCategoryClicked,
-                onMarkAsReadClicked = onMarkAsReadClicked,
-                onMarkAsUnreadClicked = onMarkAsUnreadClicked,
-                onDownloadClicked = onDownloadClicked,
-                onDeleteClicked = onDeleteClicked.takeIf { presenter.selection.none { it.source == LocalSource.ID } },
-                // SY -->
-                onClickCleanTitles = onClickCleanTitles.takeIf { presenter.showCleanTitles },
-                onClickMigrate = onClickMigrate,
-                onClickAddToMangaDex = onClickAddToMangaDex.takeIf { presenter.showAddToMangadex },
-                // SY <--
-            )
-        },
-    ) { paddingValues ->
-        LibraryContent(
-            state = presenter,
-            contentPadding = paddingValues,
-            currentPage = presenter.activeCategory,
-            isLibraryEmpty = presenter.loadedManga.isEmpty(),
-            showPageTabs = presenter.tabVisibility,
-            showMangaCount = presenter.mangaCountVisibility,
-            onChangeCurrentPage = { presenter.activeCategory = it },
-            onMangaClicked = onMangaClicked,
-            onToggleSelection = { presenter.toggleSelection(it) },
-            onRefresh = onClickRefresh,
-            onGlobalSearchClicked = onGlobalSearchClicked,
-            getNumberOfMangaForCategory = { presenter.getMangaCountForCategory(it) },
-            getDisplayModeForPage = { presenter.getDisplayMode(index = it) },
-            getColumnsForOrientation = { presenter.getColumnsPreferenceForCurrentOrientation(it) },
-            getLibraryForPage = { presenter.getMangaForCategory(page = it) },
-            isIncognitoMode = presenter.isIncognitoMode,
-            isDownloadOnly = presenter.isDownloadOnly,
-            // SY -->
-            onOpenReader = onOpenReader,
-            // SY <--
-        )
+    Crossfade(targetState = presenter.isLoading) { state ->
+        when (state) {
+            true -> LoadingScreen()
+            false -> Scaffold(
+                modifier = Modifier.safeDrawingPadding(),
+                topBar = {
+                    val title by presenter.getToolbarTitle()
+                    LibraryToolbar(
+                        state = presenter,
+                        title = title,
+                        onClickUnselectAll = onClickUnselectAll,
+                        onClickSelectAll = onClickSelectAll,
+                        onClickInvertSelection = onClickInvertSelection,
+                        onClickFilter = onClickFilter,
+                        onClickRefresh = { onClickRefresh(null) },
+                        // SY -->
+                        onClickSyncExh = onClickSyncExh,
+                        // SY <--
+                    )
+                },
+                bottomBar = {
+                    LibraryBottomActionMenu(
+                        visible = presenter.selectionMode,
+                        onChangeCategoryClicked = onChangeCategoryClicked,
+                        onMarkAsReadClicked = onMarkAsReadClicked,
+                        onMarkAsUnreadClicked = onMarkAsUnreadClicked,
+                        onDownloadClicked = onDownloadClicked,
+                        onDeleteClicked = onDeleteClicked.takeIf { presenter.selection.none { it.source == LocalSource.ID } },
+                        // SY -->
+                        onClickCleanTitles = onClickCleanTitles.takeIf { presenter.showCleanTitles },
+                        onClickMigrate = onClickMigrate,
+                        onClickAddToMangaDex = onClickAddToMangaDex.takeIf { presenter.showAddToMangadex },
+                        // SY <--
+                    )
+                },
+            ) { paddingValues ->
+                LibraryContent(
+                    state = presenter,
+                    contentPadding = paddingValues,
+                    currentPage = presenter.activeCategory,
+                    isLibraryEmpty = presenter.loadedManga.isEmpty(),
+                    showPageTabs = presenter.tabVisibility,
+                    showMangaCount = presenter.mangaCountVisibility,
+                    onChangeCurrentPage = { presenter.activeCategory = it },
+                    onMangaClicked = onMangaClicked,
+                    onToggleSelection = { presenter.toggleSelection(it) },
+                    onRefresh = onClickRefresh,
+                    onGlobalSearchClicked = onGlobalSearchClicked,
+                    getNumberOfMangaForCategory = { presenter.getMangaCountForCategory(it) },
+                    getDisplayModeForPage = { presenter.getDisplayMode(index = it) },
+                    getColumnsForOrientation = { presenter.getColumnsPreferenceForCurrentOrientation(it) },
+                    getLibraryForPage = { presenter.getMangaForCategory(page = it) },
+                    isIncognitoMode = presenter.isIncognitoMode,
+                    isDownloadOnly = presenter.isDownloadOnly,
+                    // SY -->
+                    onOpenReader = onOpenReader,
+                    // SY <--
+                )
+            }
+        }
     }
 }
