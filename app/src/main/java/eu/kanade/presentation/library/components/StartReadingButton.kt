4cdf509e14d6ced79bfef51c7ad14f09dde9f325
Jobobby04 <jobobby04@users.noreply.github.com>
2022-07-29 22:25:44 -0400

    Remove useless border on play button

--- app/src/main/java/eu/kanade/presentation/library/components/StartReadingButton.kt
+++ app/src/main/java/eu/kanade/presentation/library/components/StartReadingButton.kt
@@ -1,12 +1,12 @@
 package eu.kanade.presentation.library.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Surface
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +15,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import eu.kanade.tachiyomi.R
 
@@ -24,22 +25,33 @@ fun StartReadingButton(
     onOpenReader: () -> Unit,
 ) {
     Box(
-        modifier then Modifier.size(50.dp)
+        modifier then Modifier
+            .size(50.dp)
             .clip(CircleShape),
         contentAlignment = Alignment.Center,
     ) {
         Box(
-            Modifier.size(32.dp)
+            Modifier
+                .size(32.dp)
                 .clip(CircleShape)
                 .background(Color(0xAD212121))
-                .border(0.1.dp, Color(0xFFEDEDED))
                 .clickable(onClick = onOpenReader),
         )
         Icon(
             painter = painterResource(R.drawable.ic_start_reading_24dp),
             contentDescription = stringResource(R.string.action_start_reading),
             tint = Color.White,
-            modifier = Modifier.size(24.dp).padding(3.dp),
+            modifier = Modifier
+                .size(24.dp)
+                .padding(3.dp),
         )
+    }
+}
+
+@Preview
+@Composable
+fun StartReadingPreview() {
+    Surface {
+        StartReadingButton {}
     }
 }
