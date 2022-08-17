9fe7f23b8197252323523a49ccf4328546ee38b1
Jobobby04 <jobobby04@users.noreply.github.com>
2022-08-17 13:52:00 -0400

    Minor fix

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/setting/SortDirectionSetting.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/setting/SortDirectionSetting.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.ui.library.setting
 
 import eu.kanade.domain.category.model.Category
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
+import eu.kanade.tachiyomi.ui.library.LibraryGroup
 
 enum class SortDirectionSetting(val flag: Long) {
     ASCENDING(0b01000000),
@@ -15,7 +16,7 @@ enum class SortDirectionSetting(val flag: Long) {
         }
 
         fun get(preferences: PreferencesHelper, category: Category?): SortDirectionSetting {
-            return if (preferences.categorizedDisplaySettings().get() && category != null && category.id != 0L) {
+            return if (preferences.categorizedDisplaySettings().get() && category != null && category.id != 0L /* SY --> */ && preferences.groupLibraryBy().get() == LibraryGroup.BY_DEFAULT/* SY <-- */) {
                 fromFlag(category.sortDirection)
             } else {
                 preferences.librarySortingAscending().get()
