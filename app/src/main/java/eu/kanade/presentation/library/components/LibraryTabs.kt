bab663f856a93cd98e1ad760f3dedc66f9bc339a
arkon <eugcheung94@gmail.com>
2022-09-25 10:50:02 -0400

    Temporarily replace library category tabs divider with our own
    Fixes #7789
    
    (cherry picked from commit db91d04e82a5fef55b94624daf3271c9df7655ea)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/presentation/library/components/LibraryTabs.kt

--- app/src/main/java/eu/kanade/presentation/library/components/LibraryTabs.kt
+++ app/src/main/java/eu/kanade/presentation/library/components/LibraryTabs.kt
@@ -14,6 +14,7 @@ import com.google.accompanist.pager.PagerState
 import eu.kanade.domain.category.model.Category
 import eu.kanade.presentation.category.visualName
 import eu.kanade.presentation.components.AppStateBanners
+import eu.kanade.presentation.components.Divider
 import eu.kanade.presentation.components.TabIndicator
 import eu.kanade.presentation.components.TabText
 import kotlinx.coroutines.launch
@@ -37,6 +38,9 @@ fun LibraryTabs(
             selectedTabIndex = state.currentPage.coerceAtMost(categories.lastIndex),
             edgePadding = 0.dp,
             indicator = { TabIndicator(it[state.currentPage.coerceAtMost(categories.lastIndex)]) },
+            // TODO: use default when width is fixed upstream
+            // https://issuetracker.google.com/issues/242879624
+            divider = {},
         ) {
             categories.forEachIndexed { index, category ->
                 val count by if (showMangaCount) {
@@ -59,6 +63,8 @@ fun LibraryTabs(
                 )
             }
         }
+
+        Divider()
 
         AppStateBanners(isDownloadOnly, isIncognitoMode)
     }
