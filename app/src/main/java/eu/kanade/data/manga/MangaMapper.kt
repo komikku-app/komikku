e704b2bde4424c723fdfc0e9c04571c53587790f
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2022-10-01 20:33:03 +0600

    Remove mangaChapterMapper (#8124)
    Was used by updates screen previously
    
    (cherry picked from commit 5c22cbf28e86079994874b08cd4bdfb1bff2b0d9)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/data/manga/MangaMapper.kt

--- app/src/main/java/eu/kanade/data/manga/MangaMapper.kt
+++ app/src/main/java/eu/kanade/data/manga/MangaMapper.kt
@@ -1,7 +1,6 @@
 package eu.kanade.data.manga
 
 import eu.kanade.data.listOfStringsAndAdapter
-import eu.kanade.domain.chapter.model.Chapter
 import eu.kanade.domain.manga.model.Manga
 import eu.kanade.tachiyomi.data.database.models.LibraryManga
 import eu.kanade.tachiyomi.source.model.UpdateStrategy
@@ -35,50 +34,8 @@ val mangaMapper: (Long, Long, String, String?, String?, String?, List<String>?, 
         )
     }
 
-val mangaChapterMapper: (Long, Long, String, String?, String?, String?, List<String>?, String, Long, String?, Boolean, Long?, Long?, Boolean, Long, Long, Long, Long, UpdateStrategy, List<String>?, Long, Long, String, String, String?, Boolean, Boolean, Long, Float, Long, Long, Long) -> Pair<Manga, Chapter> =
-    { _id, source, url, artist, author, description, genre, title, status, thumbnailUrl, favorite, lastUpdate, next_update, initialized, viewerFlags, chapterFlags, coverLastModified, dateAdded, updateStrategy, filteredScanlators, chapterId, mangaId, chapterUrl, name, scanlator, read, bookmark, lastPageRead, chapterNumber, sourceOrder, dateFetch, dateUpload ->
-        Manga(
-            id = _id,
-            source = source,
-            favorite = favorite,
-            lastUpdate = lastUpdate ?: 0,
-            dateAdded = dateAdded,
-            viewerFlags = viewerFlags,
-            chapterFlags = chapterFlags,
-            coverLastModified = coverLastModified,
-            url = url,
-            // SY -->
-            ogTitle = title,
-            ogArtist = artist,
-            ogAuthor = author,
-            ogDescription = description,
-            ogGenre = genre,
-            ogStatus = status,
-            // SY <--
-            thumbnailUrl = thumbnailUrl,
-            updateStrategy = updateStrategy,
-            initialized = initialized,
-            // SY -->
-            filteredScanlators = filteredScanlators,
-            // SY <--
-        ) to Chapter(
-            id = chapterId,
-            mangaId = mangaId,
-            read = read,
-            bookmark = bookmark,
-            lastPageRead = lastPageRead,
-            dateFetch = dateFetch,
-            sourceOrder = sourceOrder,
-            url = chapterUrl,
-            name = name,
-            dateUpload = dateUpload,
-            chapterNumber = chapterNumber,
-            scanlator = scanlator,
-        )
-    }
-
-val libraryManga: (Long, Long, String, String?, String?, String?, List<String>?, String, Long, String?, Boolean, Long?, Long?, Boolean, Long, Long, Long, Long, UpdateStrategy, List<String>?, Long, Long, Long) -> LibraryManga =
-    { _id, source, url, artist, author, description, genre, title, status, thumbnail_url, favorite, last_update, next_update, initialized, viewer, chapter_flags, cover_last_modified, date_added, update_strategy, filtered_scanlators, unread_count, read_count, category ->
+val libraryManga: (Long, Long, String, String?, String?, String?, List<String>?, String, Long, String?, Boolean, Long?, Long?, Boolean, Long, Long, Long, Long, List<String>?, UpdateStrategy, Long, Long, Long) -> LibraryManga =
+    { _id, source, url, artist, author, description, genre, title, status, thumbnail_url, favorite, last_update, next_update, initialized, viewer, chapter_flags, cover_last_modified, date_added, filtered_scanlators, update_strategy, unread_count, read_count, category ->
         LibraryManga().apply {
             this.id = _id
             this.source = source
