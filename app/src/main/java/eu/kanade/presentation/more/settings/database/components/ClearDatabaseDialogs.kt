f71f9dbb93cc39cf27e9696104d6a2423284c2a0
arkon <eugcheung94@gmail.com>
2022-10-09 11:48:46 -0400

    Document copied M3 component customizations
    (cherry picked from commit d0950cb02687ad99a1f7947b136419ee27ca5b7d)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/presentation/category/components/CategoryDialogs.kt
    #	app/src/main/java/eu/kanade/presentation/more/settings/database/components/ClearDatabaseDialogs.kt

--- app/src/main/java/eu/kanade/presentation/more/settings/database/components/ClearDatabaseDialogs.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/database/components/ClearDatabaseDialogs.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import eu.kanade.presentation.components.TextButton
 import eu.kanade.tachiyomi.R
 
 @Composable
