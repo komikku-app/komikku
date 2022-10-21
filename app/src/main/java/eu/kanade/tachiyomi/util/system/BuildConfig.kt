b5f3114cb429d3f0a379a728029f836763d5b4f2
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2022-10-21 08:41:42 +0600

    Hide reader "Split tall images" option in release build (#8235)
    (cherry picked from commit 44cabf2f0b9db3ae60d5739676fb8777c474ea6b)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsReaderScreen.kt
    #	app/src/main/java/eu/kanade/tachiyomi/ui/reader/setting/ReaderPreferences.kt

--- app/src/main/java/eu/kanade/tachiyomi/util/system/BuildConfig.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/system/BuildConfig.kt
@@ -1,6 +1,10 @@
 package eu.kanade.tachiyomi.util.system
 
 import eu.kanade.tachiyomi.BuildConfig
+import exh.syDebugVersion
 
 val isDevFlavor: Boolean
     get() = BuildConfig.FLAVOR == "dev"
+
+val isReleaseFlavor: Boolean
+    get() = BuildConfig.BUILD_TYPE == "release" /* SY --> */ && syDebugVersion == "0" /* SY <-- */
