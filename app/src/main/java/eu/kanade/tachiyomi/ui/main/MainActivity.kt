f549730c8fee970f057bc02757c0c2b8c17f823e
arkon <eugcheung94@gmail.com>
2022-10-27 16:50:44 -0400

    Remove side nav icon alignment preference
    Simplifying it to just always be centered, which is what most Google apps seem to be doing anyway.
    
    (cherry picked from commit ccc4144f3cc2264c3ae75d492983e9577f352af1)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
@@ -7,7 +7,6 @@ import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.os.Looper
-import android.view.Gravity
 import android.view.Menu
 import android.view.ViewGroup
 import android.view.Window
@@ -189,18 +188,6 @@ class MainActivity : BaseActivity() {
             elapsed <= SPLASH_MIN_DURATION || (!ready && elapsed <= SPLASH_MAX_DURATION)
         }
         setSplashScreenExitAnimation(splashScreen)
-
-        if (binding.sideNav != null) {
-            uiPreferences.sideNavIconAlignment()
-                .asHotFlow {
-                    binding.sideNav?.menuGravity = when (it) {
-                        1 -> Gravity.CENTER
-                        2 -> Gravity.BOTTOM
-                        else -> Gravity.TOP
-                    }
-                }
-                .launchIn(lifecycleScope)
-        }
 
         nav.setOnItemSelectedListener { item ->
             val id = item.itemId
