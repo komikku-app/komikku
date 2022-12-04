de4567a50831a43a8093de2f67b05c648a7fadf4
Jobobby04 <jobobby04@users.noreply.github.com>
2022-12-04 16:21:06 -0500

    Fix crash with migration list screen being put in the background

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/advanced/process/MigrationListScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/advanced/process/MigrationListScreen.kt
@@ -24,6 +24,7 @@ import eu.kanade.tachiyomi.util.system.toast
 
 class MigrationListScreen(private val config: MigrationProcedureConfig) : Screen {
 
+    @delegate:Transient
     var newSelectedItem by mutableStateOf<Pair<Long, Long>?>(null)
 
     @Composable
