0663928b6bcce89e6b7fa124bb2e827f75c8298a
arkon <eugcheung94@gmail.com>
2022-12-05 10:06:41 -0500

    Always show library tabs and counts when searching
    Closes #8680
    
    (cherry picked from commit 3f9820ac797090185bd73de7a217561a5914a21d)

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryScreenModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryScreenModel.kt
@@ -1312,7 +1312,6 @@ class LibraryScreenModel(
         val selectionMode = selection.isNotEmpty()
 
         val categories = library.keys.toList()
-        private val onlyDefaultCategory = categories.size == 1 && categories[0].isSystemCategory
 
         val libraryCount by lazy {
             library.values
@@ -1343,7 +1342,7 @@ class LibraryScreenModel(
         }
 
         fun getMangaCountForCategory(category: Category): Int? {
-            return if (showMangaCount) library[category]?.size else null
+            return if (showMangaCount || !searchQuery.isNullOrEmpty()) library[category]?.size else null
         }
 
         fun getToolbarTitle(
