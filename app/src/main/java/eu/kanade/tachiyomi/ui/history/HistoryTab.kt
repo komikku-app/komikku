4c2e9aa509f081d28aebc68d2a3f1c66f86fd3d9
arkon <eugcheung94@gmail.com>
2023-01-09 22:50:11 -0500

    Fix double tapping History not working consistently
    Fixes #8875
    
    (cherry picked from commit 46774771ec82328816d01918b5cb43cb807e23ea)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/history/HistoryTab.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/history/HistoryTab.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/history/HistoryTab.kt
@@ -32,7 +32,7 @@ import eu.kanade.tachiyomi.ui.manga.MangaScreen
 import eu.kanade.tachiyomi.ui.reader.ReaderActivity
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
@@ -126,7 +126,7 @@ object HistoryTab : Tab {
         }
 
         LaunchedEffect(Unit) {
-            resumeLastChapterReadEvent.consumeAsFlow().collectLatest {
+            resumeLastChapterReadEvent.receiveAsFlow().collectLatest {
                 openChapter(context, screenModel.getNextChapter())
             }
         }
