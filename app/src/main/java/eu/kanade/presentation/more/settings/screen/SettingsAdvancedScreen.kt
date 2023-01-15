fe303435c8ef58af3f9d1d71e4975a920303e5fd
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2023-01-15 23:32:27 +0800

    Fix installing extensions on MIUI (#8916)
    * Fix installing extensions on MIUI
    
    * isShizukuReady -> isShizukuInstalled
    
    (cherry picked from commit 293b96785865d9cf8c62b3e12046347b32be9d4d)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsAdvancedScreen.kt

--- app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsAdvancedScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsAdvancedScreen.kt
@@ -76,6 +76,9 @@ import eu.kanade.tachiyomi.util.lang.withUIContext
 import eu.kanade.tachiyomi.util.storage.DiskUtil
 import eu.kanade.tachiyomi.util.system.DeviceUtil
 import eu.kanade.tachiyomi.util.system.isPackageInstalled
+import eu.kanade.tachiyomi.util.system.isPreviewBuildType
+import eu.kanade.tachiyomi.util.system.isReleaseBuildType
+import eu.kanade.tachiyomi.util.system.isShizukuInstalled
 import eu.kanade.tachiyomi.util.system.logcat
 import eu.kanade.tachiyomi.util.system.powerManager
 import eu.kanade.tachiyomi.util.system.setDefaultSettings
@@ -91,7 +94,6 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import logcat.LogPriority
 import okhttp3.Headers
-import rikka.sui.Sui
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 import java.io.File
@@ -403,6 +405,7 @@ object SettingsAdvancedScreen : SearchableSettings {
     ): Preference.PreferenceGroup {
         val context = LocalContext.current
         val uriHandler = LocalUriHandler.current
+        val extensionInstallerPref = basePreferences.extensionInstaller()
         var shizukuMissing by rememberSaveable { mutableStateOf(false) }
 
         if (shizukuMissing) {
@@ -432,19 +435,13 @@ object SettingsAdvancedScreen : SearchableSettings {
             title = stringResource(R.string.label_extensions),
             preferenceItems = listOf(
                 Preference.PreferenceItem.ListPreference(
-                    pref = basePreferences.extensionInstaller(),
+                    pref = extensionInstallerPref,
                     title = stringResource(R.string.ext_installer_pref),
-                    entries = PreferenceValues.ExtensionInstaller.values()
-                        .run {
-                            if (DeviceUtil.isMiui) {
-                                filter { it != PreferenceValues.ExtensionInstaller.PACKAGEINSTALLER }
-                            } else {
-                                toList()
-                            }
-                        }.associateWith { stringResource(it.titleResId) },
+                    entries = extensionInstallerPref.entries
+                        .associateWith { stringResource(it.titleResId) },
                     onValueChanged = {
                         if (it == PreferenceValues.ExtensionInstaller.SHIZUKU &&
-                            !(context.isPackageInstalled("moe.shizuku.privileged.api") || Sui.isSui())
+                            !context.isShizukuInstalled
                         ) {
                             shizukuMissing = true
                             false
