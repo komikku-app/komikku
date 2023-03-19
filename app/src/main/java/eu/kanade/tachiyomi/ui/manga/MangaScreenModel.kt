639a2a9c38f731dd0308a31f4777df3325908cf8
arkon <eugcheung94@gmail.com>
2023-03-19 17:23:51 -0400

    Show proper string in manga detail screen for SourceNotInstalledException
    (cherry picked from commit 14d1bcacc9cba0e14cdf90a7972bf85660465c0b)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
    (cherry picked from commit 85af8ea5175de5aef7ad5ec297525c4d889a6fde)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
@@ -1517,6 +1517,14 @@ class MangaInfoScreenModel(
         }
     }
 
+    private val Throwable.snackbarMessage: String
+        get() = when (val className = this::class.simpleName) {
+            null -> message ?: ""
+            "SourceNotInstalledException" -> context.getString(R.string.loader_not_implemented_error)
+            "Exception", "HttpException", "IOException" -> message ?: className
+            else -> "$className: $message"
+        }
+
     // SY -->
     fun showEditMangaInfoDialog() {
         mutableState.update { state ->
@@ -1645,13 +1653,6 @@ val chapterDecimalFormat = DecimalFormat(
     DecimalFormatSymbols()
         .apply { decimalSeparator = '.' },
 )
-
-private val Throwable.snackbarMessage: String
-    get() = when (val className = this::class.simpleName) {
-        null -> message ?: ""
-        "Exception", "HttpException", "IOException", "SourceNotInstalledException" -> message ?: className
-        else -> "$className: $message"
-    }
 
 // SY -->
 sealed class PagePreviewState {
