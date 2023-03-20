Ramsesdb/gexu-legacy-fork

Migrate things to use newer data models (#9239)
* Remove old database models from Coil

* Remove old database models from TrackInfoDialogHome

* Remove old database models from Backup Manager

app/src/main/java/eu/kanade/tachiyomi/data/backup/models/BackupManga.kt
@@ -1,13 +1,12 @@
 package eu.kanade.tachiyomi.data.backup.models
 
-import eu.kanade.tachiyomi.data.database.models.ChapterImpl
-import eu.kanade.tachiyomi.data.database.models.MangaImpl
-import eu.kanade.tachiyomi.data.database.models.TrackImpl
 import eu.kanade.tachiyomi.source.model.UpdateStrategy
 import eu.kanade.tachiyomi.ui.reader.setting.ReadingModeType
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.protobuf.ProtoNumber
+import tachiyomi.domain.chapter.model.Chapter
 import tachiyomi.domain.manga.model.Manga
+import tachiyomi.domain.track.model.Track
 
 @Suppress("DEPRECATION")
 @Serializable
@@ -41,32 +40,32 @@ data class BackupManga(
     @ProtoNumber(104) var history: List<BackupHistory> = emptyList(),
     @ProtoNumber(105) var updateStrategy: UpdateStrategy = UpdateStrategy.ALWAYS_UPDATE,
 ) {
-    fun getMangaImpl(): MangaImpl {
-        return MangaImpl().apply {
-            url = this@BackupManga.url
-            title = this@BackupManga.title
-            artist = this@BackupManga.artist
-            author = this@BackupManga.author
-            description = this@BackupManga.description
-            genre = this@BackupManga.genre.joinToString()
-            status = this@BackupManga.status
-            thumbnail_url = this@BackupManga.thumbnailUrl
-            favorite = this@BackupManga.favorite
-            source = this@BackupManga.source
-            date_added = this@BackupManga.dateAdded
-            viewer_flags = this@BackupManga.viewer_flags ?: this@BackupManga.viewer
-            chapter_flags = this@BackupManga.chapterFlags
-            update_strategy = this@BackupManga.updateStrategy
-        }
+    fun getMangaImpl(): Manga {
+        return Manga.create().copy(
+            url = this@BackupManga.url,
+            title = this@BackupManga.title,
+            artist = this@BackupManga.artist,
+            author = this@BackupManga.author,
+            description = this@BackupManga.description,
+            genre = this@BackupManga.genre,
+            status = this@BackupManga.status.toLong(),
+            thumbnailUrl = this@BackupManga.thumbnailUrl,
+            favorite = this@BackupManga.favorite,
+            source = this@BackupManga.source,
+            dateAdded = this@BackupManga.dateAdded,
+            viewerFlags = (this@BackupManga.viewer_flags ?: this@BackupManga.viewer).toLong(),
+            chapterFlags = this@BackupManga.chapterFlags.toLong(),
+            updateStrategy = this@BackupManga.updateStrategy,
+        )
     }
 
-    fun getChaptersImpl(): List<ChapterImpl> {
+    fun getChaptersImpl(): List<Chapter> {
         return chapters.map {
             it.toChapterImpl()
         }
     }
 
-    fun getTrackingImpl(): List<TrackImpl> {
+    fun getTrackingImpl(): List<Track> {
         return tracking.map {
             it.getTrackingImpl()
         }

app/src/main/java/eu/kanade/tachiyomi/source/model/SMangaExtensions.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.source.model
 
 import tachiyomi.data.Mangas
+import tachiyomi.domain.manga.model.Manga
 
 fun SManga.copyFrom(other: Mangas) {
     if (other.author != null) {
@@ -28,4 +29,34 @@ fun SManga.copyFrom(other: Mangas) {
     if (!initialized) {
         initialized = other.initialized
     }
+}
+
+fun Manga.copyFrom(other: Mangas): Manga {
+    var manga = this
+    if (other.author != null) {
+        manga = manga.copy(author = other.author)
+    }
+
+    if (other.artist != null) {
+        manga = manga.copy(artist = other.artist)
+    }
+
+    if (other.description != null) {
+        manga = manga.copy(description = other.description)
+    }
+
+    if (other.genre != null) {
+        manga = manga.copy(genre = other.genre)
+    }
+
+    if (other.thumbnail_url != null) {
+        manga = manga.copy(thumbnailUrl = other.thumbnail_url)
+    }
+
+    manga = manga.copy(status = other.status)
+
+    if (!initialized) {
+        manga = manga.copy(initialized = other.initialized)
+    }
+    return manga
 }

komikku-app/komikku

(cherry picked from commit dfdb688b437c38954b1072b7a1df15921a437868)
# Conflicts:
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupManager.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupRestorer.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/models/BackupManga.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/database/models/Manga.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/database/models/MangaImpl.kt
#	app/src/main/java/eu/kanade/tachiyomi/source/model/SMangaExtensions.kt
#	app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt

app/src/main/java/eu/kanade/tachiyomi/data/backup/models/BackupManga.kt
@@ -1,15 +1,14 @@
 package eu.kanade.tachiyomi.data.backup.models
 
-import eu.kanade.tachiyomi.data.database.models.ChapterImpl
-import eu.kanade.tachiyomi.data.database.models.MangaImpl
-import eu.kanade.tachiyomi.data.database.models.TrackImpl
 import eu.kanade.tachiyomi.source.model.UpdateStrategy
 import eu.kanade.tachiyomi.ui.reader.setting.ReadingModeType
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.protobuf.ProtoNumber
 import tachiyomi.data.listOfStringsAndAdapter
+import tachiyomi.domain.chapter.model.Chapter
 import tachiyomi.domain.manga.model.CustomMangaInfo
 import tachiyomi.domain.manga.model.Manga
+import tachiyomi.domain.track.model.Track
 
 @Suppress("DEPRECATION")
 @Serializable
@@ -59,27 +58,29 @@ data class BackupManga(
     // Neko specific values
     @ProtoNumber(901) var filtered_scanlators: String? = null,
 ) {
-    fun getMangaImpl(): MangaImpl {
-        return MangaImpl().apply {
-            url = this@BackupManga.url
-            title = this@BackupManga.title
-            artist = this@BackupManga.artist
-            author = this@BackupManga.author
-            description = this@BackupManga.description
-            genre = this@BackupManga.genre.joinToString()
-            status = this@BackupManga.status
-            thumbnail_url = this@BackupManga.thumbnailUrl
-            favorite = this@BackupManga.favorite
-            source = this@BackupManga.source
-            date_added = this@BackupManga.dateAdded
-            viewer_flags = this@BackupManga.viewer_flags ?: this@BackupManga.viewer
-            chapter_flags = this@BackupManga.chapterFlags
-            update_strategy = this@BackupManga.updateStrategy
-            filtered_scanlators = this@BackupManga.filtered_scanlators
-        }
+    fun getMangaImpl(): Manga {
+        return Manga.create().copy(
+            url = this@BackupManga.url,
+            // SY -->
+            ogTitle = this@BackupManga.title,
+            ogArtist = this@BackupManga.artist,
+            ogAuthor = this@BackupManga.author,
+            ogDescription = this@BackupManga.description,
+            ogGenre = this@BackupManga.genre,
+            ogStatus = this@BackupManga.status.toLong(),
+            // SY <--
+            thumbnailUrl = this@BackupManga.thumbnailUrl,
+            favorite = this@BackupManga.favorite,
+            source = this@BackupManga.source,
+            dateAdded = this@BackupManga.dateAdded,
+            viewerFlags = (this@BackupManga.viewer_flags ?: this@BackupManga.viewer).toLong(),
+            chapterFlags = this@BackupManga.chapterFlags.toLong(),
+            updateStrategy = this@BackupManga.updateStrategy,
+            filteredScanlators = this@BackupManga.filtered_scanlators?.let(listOfStringsAndAdapter::decode),
+        )
     }
 
-    fun getChaptersImpl(): List<ChapterImpl> {
+    fun getChaptersImpl(): List<Chapter> {
         return chapters.map {
             it.toChapterImpl()
         }
@@ -108,7 +109,7 @@ data class BackupManga(
     }
     // SY <--
 
-    fun getTrackingImpl(): List<TrackImpl> {
+    fun getTrackingImpl(): List<Track> {
         return tracking.map {
             it.getTrackingImpl()
         }

app/src/main/java/eu/kanade/tachiyomi/source/model/SMangaExtensions.kt
@@ -1,20 +1,12 @@
 package eu.kanade.tachiyomi.source.model
 
-import eu.kanade.tachiyomi.data.database.models.Manga
-import eu.kanade.tachiyomi.data.download.DownloadManager
 import tachiyomi.data.Mangas
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
+import tachiyomi.domain.manga.model.Manga
 
 fun SManga.copyFrom(other: Mangas) {
     // EXH -->
     if (other.title.isNotBlank() && originalTitle != other.title) {
-        val oldTitle = originalTitle
         title = other.title
-        val source = (this as? Manga)?.source
-        if (source != null) {
-            Injekt.get<DownloadManager>().renameMangaDir(oldTitle, other.title, source)
-        }
     }
     // EXH <--
 
@@ -43,4 +35,34 @@ fun SManga.copyFrom(other: Mangas) {
     if (!initialized) {
         initialized = other.initialized
     }
+}
+
+fun Manga.copyFrom(other: Mangas): Manga {
+    var manga = this
+    if (other.author != null) {
+        manga = manga.copy(ogAuthor = other.author)
+    }
+
+    if (other.artist != null) {
+        manga = manga.copy(ogArtist = other.artist)
+    }
+
+    if (other.description != null) {
+        manga = manga.copy(ogDescription = other.description)
+    }
+
+    if (other.genre != null) {
+        manga = manga.copy(ogGenre = other.genre)
+    }
+
+    if (other.thumbnail_url != null) {
+        manga = manga.copy(thumbnailUrl = other.thumbnail_url)
+    }
+
+    manga = manga.copy(ogStatus = other.status)
+
+    if (!initialized) {
+        manga = manga.copy(initialized = other.initialized)
+    }
+    return manga
 }