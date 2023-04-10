d013578fb5d5ef6e796810869a90a7989caf51b2
Jobobby04 <jobobby04@users.noreply.github.com>
2023-04-09 22:07:24 -0400

    Fix manga plus chapters

--- app/src/main/java/exh/md/dto/MangaPlusDto.kt
+++ app/src/main/java/exh/md/dto/MangaPlusDto.kt
@@ -1,28 +1,27 @@
 package exh.md.dto
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.protobuf.ProtoNumber
 
 @Serializable
 data class MangaPlusResponse(
-    @ProtoNumber(1) val success: SuccessResult? = null,
+    val success: SuccessResult? = null,
 )
 
 @Serializable
 data class SuccessResult(
-    @ProtoNumber(10) val mangaViewer: MangaViewer? = null,
+    val mangaViewer: MangaViewer? = null,
 )
 
 @Serializable
-data class MangaViewer(@ProtoNumber(1) val pages: List<MangaPlusPage> = emptyList())
+data class MangaViewer(val pages: List<MangaPlusPage> = emptyList())
 
 @Serializable
-data class MangaPlusPage(@ProtoNumber(1) val page: MangaPage? = null)
+data class MangaPlusPage(val mangaPage: MangaPage? = null)
 
 @Serializable
 data class MangaPage(
-    @ProtoNumber(1) val imageUrl: String,
-    @ProtoNumber(2) val width: Int,
-    @ProtoNumber(3) val height: Int,
-    @ProtoNumber(5) val encryptionKey: String? = null,
+    val imageUrl: String,
+    val width: Int,
+    val height: Int,
+    val encryptionKey: String? = null,
 )
