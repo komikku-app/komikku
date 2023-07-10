4d346ee8d98b7309b6a17d32bcf8417417e6bc42
arkon <eugcheung94@gmail.com>
2023-07-10 18:55:20 -0400

    Remove unused tabbed sheet layouts/classes
    (cherry picked from commit 813d7e49cd8f8811d38c16781b706d866584f96a)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/ui/reader/setting/ReaderReadingModeSettings.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/advanced/design/MigrationBottomSheetDialog.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/advanced/design/MigrationBottomSheetDialog.kt
@@ -2,40 +2,72 @@ package eu.kanade.tachiyomi.ui.browse.migration.advanced.design
 
 import android.content.Context
 import android.content.res.Resources
+import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.CompoundButton
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.getElevation
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.databinding.MigrationBottomSheetBinding
 import eu.kanade.tachiyomi.ui.browse.migration.MigrationFlags
+import eu.kanade.tachiyomi.util.system.displayCompat
+import eu.kanade.tachiyomi.util.system.dpToPx
+import eu.kanade.tachiyomi.util.system.isNightMode
+import eu.kanade.tachiyomi.util.system.isTabletUi
 import eu.kanade.tachiyomi.util.system.toast
-import eu.kanade.tachiyomi.widget.sheet.BaseBottomSheetDialog
+import eu.kanade.tachiyomi.util.view.setNavigationBarTransparentCompat
 import tachiyomi.core.preference.Preference
 import tachiyomi.core.util.lang.toLong
 import tachiyomi.domain.UnsortedPreferences
 import uy.kohesive.injekt.injectLazy
 
-class MigrationBottomSheetDialog(private val baseContext: Context, private val listener: StartMigrationListener) : BaseBottomSheetDialog(baseContext) {
+class MigrationBottomSheetDialog(private val baseContext: Context, private val listener: StartMigrationListener) : BottomSheetDialog(baseContext) {
     private val preferences: UnsortedPreferences by injectLazy()
 
     lateinit var binding: MigrationBottomSheetBinding
-
-    override fun createView(inflater: LayoutInflater): View {
-        binding = MigrationBottomSheetBinding.inflate(LayoutInflater.from(baseContext))
-        return binding.root
-    }
-
-    /**
-     * Called when the sheet is created. It initializes the listeners and values of the preferences.
-     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val rootView = createView()
+        setContentView(rootView)
+
+        // Enforce max width for tablets
+        if (context.isTabletUi()) {
+            behavior.maxWidth = 480.dpToPx
+        } else {
+            behavior.maxWidth = 0.dpToPx
+        }
+
+        // Set peek height to 50% display height
+        context.displayCompat?.let {
+            val metrics = DisplayMetrics()
+            it.getRealMetrics(metrics)
+            behavior.peekHeight = metrics.heightPixels / 2
+        }
+
+        // Set navbar color to transparent for edge-to-edge bottom sheet if we can use light navigation bar
+        // TODO Replace deprecated systemUiVisibility when material-components uses new API to modify status bar icons
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            window?.setNavigationBarTransparentCompat(context, behavior.getElevation())
+            val bottomSheet = rootView.parent as ViewGroup
+            var flags = bottomSheet.systemUiVisibility
+            flags = if (context.isNightMode()) {
+                flags and View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
+            } else {
+                flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            }
+            bottomSheet.systemUiVisibility = flags
+        }
 
         initPreferences()
 
@@ -54,6 +86,11 @@ class MigrationBottomSheetDialog(private val baseContext: Context, private val l
 
         behavior.peekHeight = Resources.getSystem().displayMetrics.heightPixels
         behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
+
+    fun createView(): View {
+        binding = MigrationBottomSheetBinding.inflate(LayoutInflater.from(baseContext))
+        return binding.root
     }
 
     /**
