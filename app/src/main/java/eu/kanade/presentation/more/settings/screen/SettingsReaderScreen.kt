d2bd0055e0cd7d204affc94f6271ba97c2154503
arkon <eugcheung94@gmail.com>
2023-07-11 22:32:20 -0400

    Use system APIs to localize percentage numbers
    (cherry picked from commit 8775596a822e8de217a07ed82d1ac49e552607d1)

--- app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsReaderScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsReaderScreen.kt
@@ -20,6 +20,7 @@ import eu.kanade.tachiyomi.ui.reader.viewer.pager.PagerConfig
 import eu.kanade.tachiyomi.util.system.isReleaseBuildType
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
+import java.text.NumberFormat
 
 object SettingsReaderScreen : SearchableSettings {
 
@@ -287,6 +288,8 @@ object SettingsReaderScreen : SearchableSettings {
 
     @Composable
     private fun getWebtoonGroup(readerPreferences: ReaderPreferences): Preference.PreferenceGroup {
+        val numberFormat = remember { NumberFormat.getPercentInstance() }
+
         val navModePref = readerPreferences.navigationModeWebtoon()
         val dualPageSplitPref = readerPreferences.dualPageSplitWebtoon()
         val webtoonSidePaddingPref = readerPreferences.webtoonSidePadding()
@@ -319,7 +322,7 @@ object SettingsReaderScreen : SearchableSettings {
                 Preference.PreferenceItem.SliderPreference(
                     value = webtoonSidePadding,
                     title = stringResource(R.string.pref_webtoon_side_padding),
-                    subtitle = stringResource(R.string.percentage, webtoonSidePadding),
+                    subtitle = numberFormat.format(webtoonSidePadding / 100f),
                     min = ReaderPreferences.WEBTOON_PADDING_MIN,
                     max = ReaderPreferences.WEBTOON_PADDING_MAX,
                     onValueChanged = {
