095c5f829e987f005f3a8ebe497d26aa14664a2f
arkon <eugcheung94@gmail.com>
2023-08-04 16:15:25 -0400

    Remove unnecessary profileable flag in manifest
    (cherry picked from commit 400ca48456c3aad6572aaa1893ba11466bbad6f5)
    
    # Conflicts:
    #	app/src/main/baseline-prof.txt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,7 @@ android {
             signingConfig = signingConfigs.getByName("debug")
             matchingFallbacks.add("release")
             isDebuggable = false
+            isProfileable = true
             versionNameSuffix = "-benchmark"
             applicationIdSuffix = ".benchmark"
         }
