Ramsesdb/gexu-legacy-fork

Add app settings to backups
This should be compatible with Aniyomi's implementation.
Related to #1857

Co-authored-by: jmir1 <jmir1@users.noreply.github.com>

app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupConst.kt
@@ -4,11 +4,18 @@ package eu.kanade.tachiyomi.data.backup
 internal object BackupConst {
     const val BACKUP_CATEGORY = 0x1
     const val BACKUP_CATEGORY_MASK = 0x1
+
     const val BACKUP_CHAPTER = 0x2
     const val BACKUP_CHAPTER_MASK = 0x2
+
     const val BACKUP_HISTORY = 0x4
     const val BACKUP_HISTORY_MASK = 0x4
+
     const val BACKUP_TRACK = 0x8
     const val BACKUP_TRACK_MASK = 0x8
-    const val BACKUP_ALL = 0xF
+
+    const val BACKUP_APP_PREFS = 0x10
+    const val BACKUP_APP_PREFS_MASK = 0x10
+
+    const val BACKUP_ALL = 0x1F
 }

komikku-app/komikku

(cherry picked from commit 72024aa44af1622fd148b859c8cea0c3404dc370)
# Conflicts:
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsBackupScreen.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupConst.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupManager.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupRestorer.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/models/Backup.kt

app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupConst.kt
@@ -4,18 +4,26 @@ package eu.kanade.tachiyomi.data.backup
 internal object BackupConst {
     const val BACKUP_CATEGORY = 0x1
     const val BACKUP_CATEGORY_MASK = 0x1
+
     const val BACKUP_CHAPTER = 0x2
     const val BACKUP_CHAPTER_MASK = 0x2
+
     const val BACKUP_HISTORY = 0x4
     const val BACKUP_HISTORY_MASK = 0x4
+
     const val BACKUP_TRACK = 0x8
     const val BACKUP_TRACK_MASK = 0x8
 
+    const val BACKUP_APP_PREFS = 0x10
+    const val BACKUP_APP_PREFS_MASK = 0x10
+
     // SY -->
-    const val BACKUP_CUSTOM_INFO = 0x10
-    const val BACKUP_CUSTOM_INFO_MASK = 0x10
-    const val BACKUP_READ_MANGA = 0x20
-    const val BACKUP_READ_MANGA_MASK = 0x20
-    const val BACKUP_ALL = 0x3F
+    const val BACKUP_CUSTOM_INFO = 0x20
+    const val BACKUP_CUSTOM_INFO_MASK = 0x20
+
+    const val BACKUP_READ_MANGA = 0x40
+    const val BACKUP_READ_MANGA_MASK = 0x40
+
+    const val BACKUP_ALL = 0x7F
     // SY <--
 }