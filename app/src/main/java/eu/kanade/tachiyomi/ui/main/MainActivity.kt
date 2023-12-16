4c634910f773fdf9189b61de177d544f3270cb8e
Ivan Iskandar <iiiiskandar14@gmail.com>
2023-12-16 22:48:34 +0700

    Refactor onboarding steps
    (cherry picked from commit 2ca3ab077192a7e5e2e7a5fb00c303a5a633372e)
    (cherry picked from commit 65e1e2cf4f76b48575fe33dd0848b38720a55744)

--- app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
@@ -414,7 +414,7 @@ class MainActivity : BaseActivity() {
         val navigator = LocalNavigator.currentOrThrow
 
         LaunchedEffect(Unit) {
-            if (!preferences.shownOnboardingFlow().get()) {
+            if (!preferences.shownOnboardingFlow().get() && navigator.lastItem !is OnboardingScreen) {
                 navigator.push(OnboardingScreen())
             }
         }
