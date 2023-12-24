Ramsesdb/gexu-legacy-fork

Address more Compose lint warnings

app/src/main/java/eu/kanade/presentation/browse/components/BrowseSourceToolbar.kt
@@ -20,6 +20,7 @@ import eu.kanade.presentation.components.SearchToolbar
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.Source
+import kotlinx.collections.immutable.persistentListOf
 import tachiyomi.domain.library.model.LibraryDisplayMode
 import tachiyomi.source.local.LocalSource
 
@@ -53,32 +54,44 @@ fun BrowseSourceToolbar(
         onClickCloseSearch = navigateUp,
         actions = {
             AppBarActions(
-                actions = listOfNotNull(
-                    AppBar.Action(
-                        title = stringResource(R.string.action_display_mode),
-                        icon = if (displayMode == LibraryDisplayMode.List) {
-                            Icons.AutoMirrored.Filled.ViewList
+                actions = persistentListOf<AppBar.AppBarAction>().builder()
+                    .apply {
+                        add(
+                            AppBar.Action(
+                                title = stringResource(R.string.action_display_mode),
+                                icon = if (displayMode == LibraryDisplayMode.List) {
+                                    Icons.AutoMirrored.Filled.ViewList
+                                } else {
+                                    Icons.Filled.ViewModule
+                                },
+                                onClick = { selectingDisplayMode = true },
+                            ),
+                        )
+                        if (isLocalSource) {
+                            add(
+                                AppBar.OverflowAction(
+                                    title = stringResource(R.string.label_help),
+                                    onClick = onHelpClick,
+                                ),
+                            )
                         } else {
-                            Icons.Filled.ViewModule
-                        },
-                        onClick = { selectingDisplayMode = true },
-                    ),
-                    if (isLocalSource) {
-                        AppBar.OverflowAction(
-                            title = stringResource(R.string.label_help),
-                            onClick = onHelpClick,
-                        )
-                    } else {
-                        AppBar.OverflowAction(
-                            title = stringResource(R.string.action_open_in_web_view),
-                            onClick = onWebViewClick,
-                        )
-                    },
-                    AppBar.OverflowAction(
-                        title = stringResource(R.string.action_settings),
-                        onClick = onSettingsClick,
-                    ).takeIf { isConfigurableSource },
-                ),
+                            add(
+                                AppBar.OverflowAction(
+                                    title = stringResource(R.string.action_open_in_web_view),
+                                    onClick = onWebViewClick,
+                                ),
+                            )
+                        }
+                        if (isConfigurableSource) {
+                            add(
+                                AppBar.OverflowAction(
+                                    title = stringResource(R.string.action_settings),
+                                    onClick = onSettingsClick,
+                                ),
+                            )
+                        }
+                    }
+                    .build(),
             )
 
             DropdownMenu(

komikku-app/komikku

(cherry picked from commit 2c032ff70d607f0e0197091e53763491775a0ab3)
# Conflicts:
#	app/src/main/java/eu/kanade/presentation/browse/MigrateSourceScreen.kt
#	app/src/main/java/eu/kanade/presentation/browse/components/BrowseSourceToolbar.kt
#	app/src/main/java/eu/kanade/presentation/library/LibrarySettingsDialog.kt
#	app/src/main/java/eu/kanade/presentation/library/components/LibraryToolbar.kt
#	app/src/main/java/eu/kanade/presentation/manga/components/MangaToolbar.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsMainScreen.kt
#	app/src/main/java/eu/kanade/presentation/reader/appbars/ReaderAppBars.kt
#	app/src/main/java/eu/kanade/tachiyomi/ui/browse/BrowseTab.kt
#	app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/MigrateSourceTab.kt
#	app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/SourcesTab.kt

app/src/main/java/eu/kanade/presentation/browse/components/BrowseSourceToolbar.kt
@@ -23,6 +23,7 @@ import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.Source
 import exh.source.anyIs
+import kotlinx.collections.immutable.persistentListOf
 import tachiyomi.domain.library.model.LibraryDisplayMode
 import tachiyomi.source.local.LocalSource
 
@@ -56,49 +57,67 @@ fun BrowseSourceToolbar(
         onClickCloseSearch = navigateUp,
         actions = {
             AppBarActions(
-                actions = listOfNotNull(
-                    AppBar.Action(
-                        title = stringResource(R.string.action_display_mode),
-                        icon = if (displayMode == LibraryDisplayMode.List) {
-                            Icons.AutoMirrored.Filled.ViewList
-                        } else {
-                            Icons.Filled.ViewModule
-                        },
-                        onClick = { selectingDisplayMode = true },
-                    ).takeIf { displayMode != null },
-                    if (isLocalSource) {
-                        if (isConfigurableSource && displayMode != null) {
-                            AppBar.OverflowAction(
-                                title = stringResource(R.string.label_help),
-                                onClick = onHelpClick,
-                            )
-                        } else {
-                            AppBar.Action(
-                                title = stringResource(R.string.label_help),
-                                icon = Icons.Outlined.Help,
-                                onClick = onHelpClick,
+                actions = persistentListOf<AppBar.AppBarAction>().builder()
+                    .apply {
+                        if (displayMode != null) {
+                            add(
+                                AppBar.Action(
+                                    title = stringResource(R.string.action_display_mode),
+                                    icon = if (displayMode == LibraryDisplayMode.List) {
+                                        Icons.AutoMirrored.Filled.ViewList
+                                    } else {
+                                        Icons.Filled.ViewModule
+                                    },
+                                    onClick = { selectingDisplayMode = true },
+                                ),
                             )
                         }
-                    } else {
-                        if (isConfigurableSource && displayMode != null) {
-                            AppBar.OverflowAction(
-                                title = stringResource(R.string.action_web_view),
-                                onClick = onWebViewClick,
-                            )
+                        if (isLocalSource) {
+                            if (isConfigurableSource && displayMode != null) {
+                                add(
+                                    AppBar.OverflowAction(
+                                        title = stringResource(R.string.label_help),
+                                        onClick = onHelpClick,
+                                    ),
+                                )
+                            } else {
+                                add(
+                                    AppBar.Action(
+                                        title = stringResource(R.string.label_help),
+                                        icon = Icons.Outlined.Help,
+                                        onClick = onHelpClick,
+                                    ),
+                                )
+                            }
                         } else {
-                            AppBar.Action(
-                                title = stringResource(R.string.action_web_view),
-                                icon = Icons.Outlined.Public,
-                                onClick = onWebViewClick,
+                            if (isConfigurableSource && displayMode != null) {
+                                add(
+                                    AppBar.OverflowAction(
+                                        title = stringResource(R.string.action_web_view),
+                                        onClick = onWebViewClick,
+                                    )
+                                )
+                            } else {
+                                add(
+                                    AppBar.Action(
+                                        title = stringResource(R.string.action_web_view),
+                                        icon = Icons.Outlined.Public,
+                                        onClick = onWebViewClick,
+                                    )
+                                )
+                            }
+                        }
+                        // SY <--
+                        if (isConfigurableSource) {
+                            add(
+                                AppBar.OverflowAction(
+                                    title = stringResource(R.string.action_settings),
+                                    onClick = onSettingsClick,
+                                ),
                             )
                         }
-                    },
-                    // SY <--
-                    AppBar.OverflowAction(
-                        title = stringResource(R.string.action_settings),
-                        onClick = onSettingsClick,
-                    ).takeIf { isConfigurableSource },
-                ),
+                    }
+                    .build(),
             )
 
             DropdownMenu(