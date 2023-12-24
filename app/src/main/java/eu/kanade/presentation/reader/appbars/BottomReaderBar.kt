Ramsesdb/gexu-legacy-fork

Migrate to multiplatform string resources (#10147)
* Migrate to multiplatform string resources

* Move plurals translations into separate files

* Fix lint check on generated files

app/src/main/java/eu/kanade/presentation/reader/appbars/BottomReaderBar.kt
@@ -14,11 +14,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.ui.reader.setting.ReaderOrientation
 import eu.kanade.tachiyomi.ui.reader.setting.ReadingMode
+import tachiyomi.i18n.MR
+import tachiyomi.presentation.core.i18n.localize
 
 @Composable
 fun BottomReaderBar(
@@ -42,28 +43,28 @@ fun BottomReaderBar(
         IconButton(onClick = onClickReadingMode) {
             Icon(
                 painter = painterResource(readingMode.iconRes),
-                contentDescription = stringResource(R.string.viewer),
+                contentDescription = localize(MR.strings.viewer),
             )
         }
 
         IconButton(onClick = onClickOrientation) {
             Icon(
                 painter = painterResource(orientation.iconRes),
-                contentDescription = stringResource(R.string.rotation_type),
+                contentDescription = localize(MR.strings.rotation_type),
             )
         }
 
         IconButton(onClick = onClickCropBorder) {
             Icon(
                 painter = painterResource(if (cropEnabled) R.drawable.ic_crop_24dp else R.drawable.ic_crop_off_24dp),
-                contentDescription = stringResource(R.string.pref_crop_borders),
+                contentDescription = localize(MR.strings.pref_crop_borders),
             )
         }
 
         IconButton(onClick = onClickSettings) {
             Icon(
                 imageVector = Icons.Outlined.Settings,
-                contentDescription = stringResource(R.string.action_settings),
+                contentDescription = localize(MR.strings.action_settings),
             )
         }
     }

komikku-app/komikku

(cherry picked from commit 46e734fc8eefb8c06a880194e702559ca3fa769d)
# Conflicts:
#	app/src/main/java/eu/kanade/presentation/browse/BrowseSourceScreen.kt
#	app/src/main/java/eu/kanade/presentation/browse/ExtensionDetailsScreen.kt
#	app/src/main/java/eu/kanade/presentation/browse/ExtensionsScreen.kt
#	app/src/main/java/eu/kanade/presentation/browse/SourcesScreen.kt
#	app/src/main/java/eu/kanade/presentation/browse/components/BrowseSourceDialogs.kt
#	app/src/main/java/eu/kanade/presentation/browse/components/BrowseSourceToolbar.kt
#	app/src/main/java/eu/kanade/presentation/category/components/CategoryDialogs.kt
#	app/src/main/java/eu/kanade/presentation/library/LibrarySettingsDialog.kt
#	app/src/main/java/eu/kanade/presentation/manga/components/MangaBottomActionMenu.kt
#	app/src/main/java/eu/kanade/presentation/manga/components/MangaDialogs.kt
#	app/src/main/java/eu/kanade/presentation/more/MoreScreen.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsAdvancedScreen.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsBrowseScreen.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsLibraryScreen.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsMainScreen.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsReaderScreen.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsSecurityScreen.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/about/AboutScreen.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/advanced/ClearDatabaseScreen.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/data/CreateBackupScreen.kt
#	app/src/main/java/eu/kanade/presentation/reader/ReaderPageActionsDialog.kt
#	app/src/main/java/eu/kanade/presentation/reader/appbars/BottomReaderBar.kt
#	app/src/main/java/eu/kanade/presentation/reader/appbars/ReaderAppBars.kt
#	app/src/main/java/eu/kanade/presentation/reader/co

app/src/main/java/eu/kanade/presentation/reader/appbars/BottomReaderBar.kt
@@ -17,12 +17,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.ui.reader.setting.ReaderBottomButton
 import eu.kanade.tachiyomi.ui.reader.setting.ReaderOrientation
 import eu.kanade.tachiyomi.ui.reader.setting.ReadingMode
+import tachiyomi.i18n.MR
+import tachiyomi.presentation.core.i18n.localize
 
 @Composable
 fun BottomReaderBar(
@@ -60,7 +61,7 @@ fun BottomReaderBar(
             IconButton(onClick = onClickChapterList) {
                 Icon(
                     imageVector = Icons.Outlined.FormatListNumbered,
-                    contentDescription = stringResource(R.string.chapters),
+                    contentDescription = localize(MR.strings.chapters),
                 )
             }
         }
@@ -69,7 +70,7 @@ fun BottomReaderBar(
             IconButton(onClick = onClickWebView) {
                 Icon(
                     imageVector = Icons.Outlined.Public,
-                    contentDescription = stringResource(R.string.action_open_in_web_view),
+                    contentDescription = localize(MR.strings.action_open_in_web_view),
                 )
             }
         }
@@ -78,7 +79,7 @@ fun BottomReaderBar(
             IconButton(onClick = onClickShare) {
                 Icon(
                     imageVector = Icons.Outlined.Share,
-                    contentDescription = stringResource(R.string.action_share),
+                    contentDescription = localize(MR.strings.action_share),
                 )
             }
         }
@@ -87,7 +88,7 @@ fun BottomReaderBar(
             IconButton(onClick = onClickReadingMode) {
                 Icon(
                     painter = painterResource(readingMode.iconRes),
-                    contentDescription = stringResource(R.string.viewer),
+                    contentDescription = localize(MR.strings.viewer),
                 )
             }
         }
@@ -96,7 +97,7 @@ fun BottomReaderBar(
             IconButton(onClick = onClickOrientation) {
                 Icon(
                     painter = painterResource(orientation.iconRes),
-                    contentDescription = stringResource(R.string.pref_rotation_type),
+                    contentDescription = localize(MR.strings.pref_rotation_type),
                 )
             }
         }
@@ -110,7 +111,7 @@ fun BottomReaderBar(
             IconButton(onClick = onClickCropBorder) {
                 Icon(
                     painter = painterResource(if (cropEnabled) R.drawable.ic_crop_24dp else R.drawable.ic_crop_off_24dp),
-                    contentDescription = stringResource(R.string.pref_crop_borders),
+                    contentDescription = localize(MR.strings.pref_crop_borders),
                 )
             }
         }
@@ -123,7 +124,7 @@ fun BottomReaderBar(
             IconButton(onClick = onClickPageLayout) {
                 Icon(
                     painter = painterResource(R.drawable.ic_book_open_variant_24dp),
-                    contentDescription = stringResource(R.string.page_layout),
+                    contentDescription = localize(MR.strings.page_layout),
                 )
             }
         }
@@ -132,7 +133,7 @@ fun BottomReaderBar(
             IconButton(onClick = onClickShiftPage) {
                 Icon(
                     painter = painterResource(R.drawable.ic_page_next_outline_24dp),
-                    contentDescription = stringResource(R.string.shift_double_pages),
+                    contentDescription = localize(MR.strings.shift_double_pages),
                 )
             }
         }
@@ -140,7 +141,7 @@ fun BottomReaderBar(
         IconButton(onClick = onClickSettings) {
             Icon(
                 imageVector = Icons.Outlined.Settings,
-                contentDescription = stringResource(R.string.action_settings),
+                contentDescription = localize(MR.stringss.action_settings),
             )
         }
         // SY <--