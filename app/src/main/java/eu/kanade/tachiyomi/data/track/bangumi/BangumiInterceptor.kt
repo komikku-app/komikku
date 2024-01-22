576884f6a9f92e07593766baa6a0e197057e5ef5
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2024-01-22 20:20:11 +0600

    Lint
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiInterceptor.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiInterceptor.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiInterceptor.kt
@@ -4,7 +4,6 @@ import eu.kanade.tachiyomi.BuildConfig
 import kotlinx.serialization.json.Json
 import okhttp3.FormBody
 import okhttp3.Interceptor
-import okhttp3.Request
 import okhttp3.Response
 import uy.kohesive.injekt.injectLazy
 
@@ -32,7 +31,10 @@ class BangumiInterceptor(private val bangumi: Bangumi) : Interceptor {
         }
 
         return originalRequest.newBuilder()
-            .header("User-Agent", "antsylich/Komikku/v${BuildConfig.VERSION_NAME} (Android) (http://github.com/cuong-tran/komikku)")
+            .header(
+                "User-Agent",
+                "antsylich/Komikku/v${BuildConfig.VERSION_NAME} (Android) (http://github.com/mihonapp/mihon)",
+            )
             .apply {
                 if (originalRequest.method == "GET") {
                     val newUrl = originalRequest.url.newBuilder()
