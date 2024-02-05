97efbd2efcece7c01e0151b31c7b028592aa3b56
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-02-05 18:32:26 +0700

    Update source repo URL

--- app/src/main/java/eu/kanade/domain/extension/interactor/CreateExtensionRepo.kt
+++ app/src/main/java/eu/kanade/domain/extension/interactor/CreateExtensionRepo.kt
@@ -22,5 +22,5 @@ class CreateExtensionRepo(private val preferences: SourcePreferences) {
     }
 }
 
-const val OFFICIAL_REPO_BASE_URL = "https://raw.githubusercontent.com/cuong-tran/tachiyomi-extensions/repo"
+const val OFFICIAL_REPO_BASE_URL = "https://raw.githubusercontent.com/cuong-tran/komikku-extensions/repo"
 private val repoRegex = """^https://.*/index\.min\.json$""".toRegex()
