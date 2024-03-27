7a482c64e406baf19e9769f58afb53b5feb006d7
Jobobby04 <jobobby04@users.noreply.github.com>
2024-03-27 16:40:40 -0400

    Revert "Migrated from Accompanist Webview to KevinZou WebView (#569)"
    This reverts commit 268b4831826df121f2b7fb7529ad367389bc731c.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -165,6 +165,7 @@ dependencies {
     debugImplementation(compose.ui.tooling)
     implementation(compose.ui.tooling.preview)
     implementation(compose.ui.util)
+    implementation(compose.accompanist.webview)
     implementation(compose.accompanist.systemuicontroller)
 
     implementation(androidx.paging.runtime)
@@ -246,8 +247,6 @@ dependencies {
     implementation(libs.bundles.voyager)
     implementation(libs.compose.materialmotion)
     implementation(libs.swipe)
-    implementation(libs.compose.webview)
-
 
     implementation(libs.google.api.services.drive)
     implementation(libs.google.api.client.oauth)
