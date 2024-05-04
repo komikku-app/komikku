4ea7aad28023a02a3d265efdfc46cef1e5c0c344
Jobobby04 <jobobby04@users.noreply.github.com>
2024-05-04 18:41:03 -0400

    Update dependencies and cleanup
    (cherry picked from commit 2d1240b27455fd781fece76173b5f35641df7cd1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -266,7 +266,6 @@ dependencies {
     implementation(libs.logcat)
 
     // Crash reports/analytics
-    // implementation(libs.bundles.acra)
     // "standardImplementation"(libs.firebase.analytics)
 
     // Shizuku
