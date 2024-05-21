2d5eb656db00218325ff4fc95a44c8072e9b8f6c
Cuong Tran <cuongtran.tm@gmail.com>
2024-05-22 05:17:20 +0700

    Fix: sync tracking 2 ways for all trackers

--- app/src/main/java/eu/kanade/domain/track/interactor/SyncChapterProgressWithTrack.kt
+++ app/src/main/java/eu/kanade/domain/track/interactor/SyncChapterProgressWithTrack.kt
@@ -1,7 +1,6 @@
 package eu.kanade.domain.track.interactor
 
 import eu.kanade.domain.track.model.toDbTrack
-import eu.kanade.tachiyomi.data.track.EnhancedTracker
 import eu.kanade.tachiyomi.data.track.Tracker
 import logcat.LogPriority
 import tachiyomi.core.common.util.system.logcat
@@ -22,26 +21,36 @@ class SyncChapterProgressWithTrack(
         remoteTrack: Track,
         tracker: Tracker,
     ) {
-        if (tracker !is EnhancedTracker) {
-            return
-        }
+        // KKM -->
+        // if (tracker !is EnhancedTracker) {
+        //     return
+        // }
+        // <-- KKM
 
+        // Current chapters in database
         val sortedChapters = getChaptersByMangaId.await(mangaId)
             .sortedBy { it.chapterNumber }
             .filter { it.isRecognizedNumber }
 
+        // Chapters to update to follow tracker
         val chapterUpdates = sortedChapters
             .filter { chapter -> chapter.chapterNumber <= remoteTrack.lastChapterRead && !chapter.read }
             .map { it.copy(read = true).toChapterUpdate() }
 
         // only take into account continuous reading
         val localLastRead = sortedChapters.takeWhile { it.read }.lastOrNull()?.chapterNumber ?: 0F
+        // Tracker will update to latest read chapter
         val updatedTrack = remoteTrack.copy(lastChapterRead = localLastRead.toDouble())
 
         try {
-            tracker.update(updatedTrack.toDbTrack())
+            // Update Tracker to localLastRead if needed
+            if (updatedTrack.lastChapterRead > remoteTrack.lastChapterRead) {
+                tracker.update(updatedTrack.toDbTrack())
+                // update Track in database
+                insertTrack.await(updatedTrack)
+            }
+            // Update local chapters following Tracker
             updateChapter.awaitAll(chapterUpdates)
-            insertTrack.await(updatedTrack)
         } catch (e: Throwable) {
             logcat(LogPriority.WARN, e)
         }
