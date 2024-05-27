69570c88cd0b42524373651b3eecacd3514c1311
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-05-27 17:41:19 +0700

    Remove palette buttons

--- app/src/main/java/eu/kanade/presentation/manga/MangaScreen.kt
+++ app/src/main/java/eu/kanade/presentation/manga/MangaScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
@@ -43,7 +42,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
@@ -51,7 +49,6 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastAll
 import androidx.compose.ui.util.fastAny
 import androidx.compose.ui.util.fastMap
@@ -108,8 +105,6 @@ import tachiyomi.i18n.MR
 import tachiyomi.i18n.sy.SYMR
 import tachiyomi.presentation.core.components.TwoPanelBox
 import tachiyomi.presentation.core.components.VerticalFastScroller
-import tachiyomi.presentation.core.components.material.Button
-import tachiyomi.presentation.core.components.material.ButtonDefaults
 import tachiyomi.presentation.core.components.material.ExtendedFloatingActionButton
 import tachiyomi.presentation.core.components.material.PullRefresh
 import tachiyomi.presentation.core.components.material.Scaffold
@@ -586,32 +581,6 @@ private fun MangaScreenSmallImpl(
                             // SY <--
                         )
                     }
-
-                    item {
-                        Column {
-                            ButtonsColor(
-                                "accent & onPrimary",
-                                state.accentColor() ?: MaterialTheme.colorScheme.primary,
-                                "accent & contentColor",
-                                state.accentColor() ?: MaterialTheme.colorScheme.primary,
-                                MaterialTheme.colorScheme.onPrimary,
-                            )
-
-                            ButtonsColor("primary", MaterialTheme.colorScheme.primary, "primaryContainer", MaterialTheme.colorScheme.primaryContainer)
-                            ButtonsColor("secondary", MaterialTheme.colorScheme.secondary, "secondaryContainer", MaterialTheme.colorScheme.secondaryContainer)
-                            ButtonsColor("tertiary", MaterialTheme.colorScheme.tertiary, "tertiaryContainer", MaterialTheme.colorScheme.tertiaryContainer)
-                            ButtonsColor("surface", MaterialTheme.colorScheme.surface, "surfaceVariant", MaterialTheme.colorScheme.surfaceVariant)
-                            ButtonsColor("inverseSurface", MaterialTheme.colorScheme.inverseSurface, "surfaceTint", MaterialTheme.colorScheme.surfaceTint)
-                            ButtonsColor("inversePrimary", MaterialTheme.colorScheme.inversePrimary, "background", MaterialTheme.colorScheme.background)
-                            ButtonsColor("error", MaterialTheme.colorScheme.error, "errorContainer", MaterialTheme.colorScheme.errorContainer)
-                            ButtonsColor("outline", MaterialTheme.colorScheme.outline, "outlineVariant", MaterialTheme.colorScheme.outlineVariant)
-                            ButtonsColor("scrim", MaterialTheme.colorScheme.scrim, "surfaceBright", MaterialTheme.colorScheme.surfaceBright)
-                            ButtonsColor("surfaceDim", MaterialTheme.colorScheme.surfaceDim, "surfaceContainer", MaterialTheme.colorScheme.surfaceContainer)
-                            ButtonsColor("surfaceContainerHigh", MaterialTheme.colorScheme.surfaceContainerHigh, "surfaceContainerHighest", MaterialTheme.colorScheme.surfaceContainerHighest)
-                            ButtonsColor("surfaceContainerLow", MaterialTheme.colorScheme.surfaceContainerLow, "surfaceContainerLowest", MaterialTheme.colorScheme.surfaceContainerLowest)
-                        }
-                    }
-
 
                     // KMK -->
                     if (state.source !is StubSource && relatedMangasEnabled) {
@@ -1234,46 +1203,3 @@ fun metadataDescription(source: Source): MetadataDescriptionComposable? {
     }
 }
 // SY <--
-
-@Composable
-fun ButtonsColor(
-    name1: String,
-    color1: Color,
-    name2: String,
-    color2: Color,
-    contentColor1: Color = contentColorFor(backgroundColor = color1),
-    contentColor2: Color = contentColorFor(backgroundColor = color2),
-) {
-    Row(modifier = Modifier
-        .padding(horizontal = 16.dp)
-        .fillMaxWidth()) {
-        Button(
-            onClick = { },
-            Modifier
-                .weight(1f)
-                .padding(horizontal = 16.dp, vertical = 4.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = color1,
-                contentColor = contentColor1,
-            )
-        ) {
-            Text(
-                text = name1,
-            )
-        }
-        Button(
-            onClick = { },
-            Modifier
-                .weight(1f)
-                .padding(horizontal = 16.dp, vertical = 4.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = color2,
-                contentColor = contentColor2,
-            ),
-        ) {
-            Text(
-                text = name2
-            )
-        }
-    }
-}
