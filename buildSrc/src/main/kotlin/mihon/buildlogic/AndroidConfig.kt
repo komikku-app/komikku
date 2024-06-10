Ramsesdb/gexu-legacy-fork

Make the app Android 8+

buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,6 +1,6 @@
 object AndroidConfig {
     const val compileSdk = 34
-    const val minSdk = 23
+    const val minSdk = 26
     const val targetSdk = 34
     const val ndk = "26.1.10909125"
 }

komikku-app/komikku

(cherry picked from commit dfb3091e380dda3e9bfb64bf5c9a685cf3a03d0e)
# Conflicts:
#	README.md
#	app/src/debug/res/mipmap-hdpi/ic_launcher.png
#	app/src/debug/res/mipmap-hdpi/ic_launcher_round.png
#	app/src/debug/res/mipmap-mdpi/ic_launcher.png
#	app/src/debug/res/mipmap-mdpi/ic_launcher_round.png
#	app/src/debug/res/mipmap-xhdpi/ic_launcher.png
#	app/src/debug/res/mipmap-xhdpi/ic_launcher_round.png
#	app/src/debug/res/mipmap-xxhdpi/ic_launcher.png
#	app/src/debug/res/mipmap-xxhdpi/ic_launcher_round.png
#	app/src/debug/res/mipmap-xxxhdpi/ic_launcher.png
#	app/src/debug/res/mipmap-xxxhdpi/ic_launcher_round.png
#	app/src/main/java/eu/kanade/domain/ui/UiPreferences.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsAdvancedScreen.kt
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsReaderScreen.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/coil/TachiyomiImageDecoder.kt
#	app/src/main/java/eu/kanade/tachiyomi/extension/installer/ShizukuInstaller.kt
#	app/src/main/java/eu/kanade/tachiyomi/ui/reader/loader/ZipPageLoader.kt
#	app/src/main/res/mipmap-hdpi/ic_launcher.png
#	app/src/main/res/mipmap-hdpi/ic_launcher_round.png
#	app/src/main/res/mipmap-mdpi/ic_launcher.png
#	app/src/main/res/mipmap-mdpi/ic_launcher_round.png
#	app/src/main/res/mipmap-xhdpi/ic_launcher.png
#	app/src/main/res/mipmap-xhdpi/ic_launcher_round.png
#	app/src/main/res/mipmap-xxhdpi/ic_launcher.png
#	app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
#	app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
#	app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png
#	buildSrc/src/main/kotlin/AndroidConfig.kt

buildSrc/src/main/kotlin/mihon/buildlogic/AndroidConfig.kt
@@ -5,7 +5,7 @@ import org.gradle.api.JavaVersion as GradleJavaVersion
 object AndroidConfig {
     const val COMPILE_SDK = 34
     const val TARGET_SDK = 34
-    const val MIN_SDK = 23
+    const val MIN_SDK = 26
     const val NDK = "26.1.10909125"
     val JavaVersion = GradleJavaVersion.VERSION_17
 }