e5e3851b723e1ec0241c660b87e28ed4d74539ce
Jobobby04 <jobobby04@users.noreply.github.com>
2024-06-26 20:38:12 -0400

    Fix build errors
    (cherry picked from commit 7899474a36c3e097163246e5081ff57def79263c)

--- app/src/main/java/eu/kanade/tachiyomi/data/sync/service/GoogleDriveSyncService.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/sync/service/GoogleDriveSyncService.kt
@@ -19,7 +19,6 @@ import com.google.api.services.drive.DriveScopes
 import com.google.api.services.drive.model.File
 import eu.kanade.domain.sync.SyncPreferences
 import eu.kanade.tachiyomi.data.backup.models.Backup
-import eu.kanade.tachiyomi.data.backup.models.BackupSerializer
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.protobuf.ProtoBuf
@@ -124,7 +123,7 @@ class GoogleDriveSyncService(context: Context, json: Json, syncPreferences: Sync
             drive.files().get(gdriveFileId).executeMediaAsInputStream().use { inputStream ->
                 GZIPInputStream(inputStream).use { gzipInputStream ->
                     val byteArray = gzipInputStream.readBytes()
-                    val backup = protoBuf.decodeFromByteArray(BackupSerializer, byteArray)
+                    val backup = protoBuf.decodeFromByteArray(Backup.serializer(), byteArray)
                     val deviceId = fileList[0].appProperties["deviceId"] ?: ""
                     return SyncData(deviceId = deviceId, backup = backup)
                 }
@@ -142,7 +141,7 @@ class GoogleDriveSyncService(context: Context, json: Json, syncPreferences: Sync
         val fileList = getAppDataFileList(drive)
         val backup = syncData.backup ?: return
 
-        val byteArray = protoBuf.encodeToByteArray(BackupSerializer, backup)
+        val byteArray = protoBuf.encodeToByteArray(Backup.serializer(), backup)
         if (byteArray.isEmpty()) {
             throw IllegalStateException(context.stringResource(MR.strings.empty_backup_error))
         }
