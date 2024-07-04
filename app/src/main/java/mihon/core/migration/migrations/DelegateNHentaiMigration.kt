e16a166ece0f2c9563e887e938b5d2cc782eabd7
Cuong-Tran <cuongtran.tm@gmail.com>
2024-07-04 16:58:54 +0700

    refactor: Update usage of delegated sources' ids

--- app/src/main/java/mihon/core/migration/migrations/DelegateNHentaiMigration.kt
+++ app/src/main/java/mihon/core/migration/migrations/DelegateNHentaiMigration.kt
@@ -1,6 +1,7 @@
 package mihon.core.migration.migrations
 
-import eu.kanade.tachiyomi.source.online.all.NHentai
+import exh.source.NHENTAI_OLD_ID
+import exh.source.NHENTAI_SOURCE_ID
 import mihon.core.migration.MigrateUtils
 import mihon.core.migration.Migration
 import mihon.core.migration.MigrationContext
@@ -10,7 +11,7 @@ class DelegateNHentaiMigration : Migration {
     override val version: Float = 6f
 
     override suspend fun invoke(migrationContext: MigrationContext): Boolean = withIOContext {
-        MigrateUtils.updateSourceId(migrationContext, NHentai.otherId, 6907)
+        MigrateUtils.updateSourceId(migrationContext, NHENTAI_SOURCE_ID, NHENTAI_OLD_ID)
 
         return@withIOContext true
     }
