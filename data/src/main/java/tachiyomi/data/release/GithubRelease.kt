922c453622e36997891281c59712a50c15c47bda
Cuong-Tran <cuongtran.tm@gmail.com>
2024-07-10 13:34:22 +0700

    Update Github's release to matching vx.xx.xx and rxxxx format

--- data/src/main/java/tachiyomi/data/release/GithubRelease.kt
+++ data/src/main/java/tachiyomi/data/release/GithubRelease.kt
@@ -48,9 +48,8 @@ val gitHubUsernameMentionRegex =
  * to: [komikku-app/komikku@23d862d17...48fb4a2e6](https://github.com/komikku-app/komikku/compare/23d862d17...48fb4a2e6)
  */
 val gitHubCommitsCompareRegex =
-    """https://github.com/(?<owner>[^/]+)/(?<repo>[^/]+)/compare/(?<from>[0-9a-f]+)...(?<to>[0-9a-f]+)""".toRegex(
-        RegexOption.IGNORE_CASE,
-    )
+    """https://github.com/(?<owner>[^/]+)/(?<repo>[^/]+)/compare/(?<from>[0-9a-f.rv]+)\.\.\.(?<to>[0-9a-f.rv]+)"""
+        .toRegex(RegexOption.IGNORE_CASE)
 // KMK <--
 
 val releaseMapper: (GithubRelease) -> Release = {
