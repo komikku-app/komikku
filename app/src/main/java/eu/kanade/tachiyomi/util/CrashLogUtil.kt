be9735f42d5c2a16b72d2dcc4ae67e5a51df5ae3
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2024-08-07 12:02:49 +0600

    Rename backup restore error log file
    (cherry picked from commit 2858ef835fec8d7278b1d0cad1b5664104d1e4b0)

--- app/src/main/java/eu/kanade/tachiyomi/util/CrashLogUtil.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/CrashLogUtil.kt
@@ -21,7 +21,7 @@ class CrashLogUtil(
 
     suspend fun dumpLogs(exception: Throwable? = null) = withNonCancellableContext {
         try {
-            val file = context.createFileInCacheDir("tachiyomi_crash_logs.txt")
+            val file = context.createFileInCacheDir("komikku_crash_logs.txt")
 
             file.appendText(getDebugInfo() + "\n\n")
             getExtensionsInfo()?.let { file.appendText("$it\n\n") }
