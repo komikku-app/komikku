jobobby04/TachiyomiSY

Address spotless lint errors (mihonapp/mihon#1138)
* Add spotless (with ktlint)

* Run spotlessApply

* screaming case screaming case screaming case

* Update PagerViewerAdapter.kt

* Update ReaderTransitionView.kt

(cherry picked from commit d6252ab7703d52ecf9f43de3ee36fd63e665a31f)

app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiApi.kt
@@ -42,7 +42,7 @@ class BangumiApi(
                 .add("rating", track.score.toInt().toString())
                 .add("status", track.toBangumiStatus())
                 .build()
-            authClient.newCall(POST("$apiUrl/collection/${track.remote_id}/update", body = body))
+            authClient.newCall(POST("$API_URL/collection/${track.remote_id}/update", body = body))
                 .awaitSuccess()
             track
         }
@@ -55,7 +55,7 @@ class BangumiApi(
                 .add("rating", track.score.toInt().toString())
                 .add("status", track.toBangumiStatus())
                 .build()
-            authClient.newCall(POST("$apiUrl/collection/${track.remote_id}/update", body = sbody))
+            authClient.newCall(POST("$API_URL/collection/${track.remote_id}/update", body = sbody))
                 .awaitSuccess()
 
             // chapter update
@@ -64,7 +64,7 @@ class BangumiApi(
                 .build()
             authClient.newCall(
                 POST(
-                    "$apiUrl/subject/${track.remote_id}/update/watched_eps",
+                    "$API_URL/subject/${track.remote_id}/update/watched_eps",
                     body = body,
                 ),
             ).awaitSuccess()
@@ -75,7 +75,7 @@ class BangumiApi(
 
     suspend fun search(search: String): List<TrackSearch> {
         return withIOContext {
-            val url = "$apiUrl/search/subject/${URLEncoder.encode(search, StandardCharsets.UTF_8.name())}"
+            val url = "$API_URL/search/subject/${URLEncoder.encode(search, StandardCharsets.UTF_8.name())}"
                 .toUri()
                 .buildUpon()
                 .appendQueryParameter("max_results", "20")
@@ -124,7 +124,7 @@ class BangumiApi(
     suspend fun findLibManga(track: Track): Track? {
         return withIOContext {
             with(json) {
-                authClient.newCall(GET("$apiUrl/subject/${track.remote_id}"))
+                authClient.newCall(GET("$API_URL/subject/${track.remote_id}"))
                     .awaitSuccess()
                     .parseAs<JsonObject>()
                     .let { jsonToSearch(it) }
@@ -134,7 +134,7 @@ class BangumiApi(
 
     suspend fun statusLibManga(track: Track): Track? {
         return withIOContext {
-            val urlUserRead = "$apiUrl/collection/${track.remote_id}"
+            val urlUserRead = "$API_URL/collection/${track.remote_id}"
             val requestUserRead = Request.Builder()
                 .url(urlUserRead)
                 .cacheControl(CacheControl.FORCE_NETWORK)
@@ -171,41 +171,41 @@ class BangumiApi(
     }
 
     private fun accessTokenRequest(code: String) = POST(
-        oauthUrl,
+        OAUTH_URL,
         body = FormBody.Builder()
             .add("grant_type", "authorization_code")
-            .add("client_id", clientId)
-            .add("client_secret", clientSecret)
+            .add("client_id", CLIENT_ID)
+            .add("client_secret", CLIENT_SECRET)
             .add("code", code)
-            .add("redirect_uri", redirectUrl)
+            .add("redirect_uri", REDIRECT_URL)
             .build(),
     )
 
     companion object {
-        private const val clientId = "bgm291665acbd06a4c28"
-        private const val clientSecret = "43e5ce36b207de16e5d3cfd3e79118db"
+        private const val CLIENT_ID = "bgm291665acbd06a4c28"
+        private const val CLIENT_SECRET = "43e5ce36b207de16e5d3cfd3e79118db"
 
-        private const val apiUrl = "https://api.bgm.tv"
-        private const val oauthUrl = "https://bgm.tv/oauth/access_token"
-        private const val loginUrl = "https://bgm.tv/oauth/authorize"
+        private const val API_URL = "https://api.bgm.tv"
+        private const val OAUTH_URL = "https://bgm.tv/oauth/access_token"
+        private const val LOGIN_URL = "https://bgm.tv/oauth/authorize"
 
-        private const val redirectUrl = "mihon://bangumi-auth"
+        private const val REDIRECT_URL = "mihon://bangumi-auth"
 
         fun authUrl(): Uri =
-            loginUrl.toUri().buildUpon()
-                .appendQueryParameter("client_id", clientId)
+            LOGIN_URL.toUri().buildUpon()
+                .appendQueryParameter("client_id", CLIENT_ID)
                 .appendQueryParameter("response_type", "code")
-                .appendQueryParameter("redirect_uri", redirectUrl)
+                .appendQueryParameter("redirect_uri", REDIRECT_URL)
                 .build()
 
         fun refreshTokenRequest(token: String) = POST(
-            oauthUrl,
+            OAUTH_URL,
             body = FormBody.Builder()
                 .add("grant_type", "refresh_token")
-                .add("client_id", clientId)
-                .add("client_secret", clientSecret)
+                .add("client_id", CLIENT_ID)
+                .add("client_secret", CLIENT_SECRET)
                 .add("refresh_token", token)
-                .add("redirect_uri", redirectUrl)
+                .add("redirect_uri", REDIRECT_URL)
                 .build(),
         )
     }

app/src/main/java/eu/kanade/tachiyomi/data/track/shikimori/ShikimoriApi.kt
@@ -54,7 +54,7 @@ class ShikimoriApi(
                 }
                 authClient.newCall(
                     POST(
-                        "$apiUrl/v2/user_rates",
+                        "$API_URL/v2/user_rates",
                         body = payload.toString().toRequestBody(jsonMime),
                     ),
                 ).awaitSuccess()
@@ -72,14 +72,14 @@ class ShikimoriApi(
     suspend fun deleteLibManga(track: DomainTrack) {
         withIOContext {
             authClient
-                .newCall(DELETE("$apiUrl/v2/user_rates/${track.libraryId}"))
+                .newCall(DELETE("$API_URL/v2/user_rates/${track.libraryId}"))
                 .awaitSuccess()
         }
     }
 
     suspend fun search(search: String): List<TrackSearch> {
         return withIOContext {
-            val url = "$apiUrl/mangas".toUri().buildUpon()
+            val url = "$API_URL/mangas".toUri().buildUpon()
                 .appendQueryParameter("order", "popularity")
                 .appendQueryParameter("search", search)
                 .appendQueryParameter("limit", "20")
@@ -102,10 +102,10 @@ class ShikimoriApi(
             remote_id = obj["id"]!!.jsonPrimitive.long
             title = obj["name"]!!.jsonPrimitive.content
             total_chapters = obj["chapters"]!!.jsonPrimitive.long
-            cover_url = baseUrl + obj["image"]!!.jsonObject["preview"]!!.jsonPrimitive.content
+            cover_url = BASE_URL + obj["image"]!!.jsonObject["preview"]!!.jsonPrimitive.content
             summary = ""
             score = obj["score"]!!.jsonPrimitive.double
-            tracking_url = baseUrl + obj["url"]!!.jsonPrimitive.content
+            tracking_url = BASE_URL + obj["url"]!!.jsonPrimitive.content
             publishing_status = obj["status"]!!.jsonPrimitive.content
             publishing_type = obj["kind"]!!.jsonPrimitive.content
             start_date = obj["aired_on"]!!.jsonPrimitive.contentOrNull ?: ""
@@ -121,13 +121,13 @@ class ShikimoriApi(
             last_chapter_read = obj["chapters"]!!.jsonPrimitive.double
             score = obj["score"]!!.jsonPrimitive.int.toDouble()
             status = toTrackStatus(obj["status"]!!.jsonPrimitive.content)
-            tracking_url = baseUrl + mangas["url"]!!.jsonPrimitive.content
+            tracking_url = BASE_URL + mangas["url"]!!.jsonPrimitive.content
         }
     }
 
     suspend fun findLibManga(track: Track, userId: String): Track? {
         return withIOContext {
-            val urlMangas = "$apiUrl/mangas".toUri().buildUpon()
+            val urlMangas = "$API_URL/mangas".toUri().buildUpon()
                 .appendPath(track.remote_id.toString())
                 .build()
             val mangas = with(json) {
@@ -136,7 +136,7 @@ class ShikimoriApi(
                     .parseAs<JsonObject>()
             }
 
-            val url = "$apiUrl/v2/user_rates".toUri().buildUpon()
+            val url = "$API_URL/v2/user_rates".toUri().buildUpon()
                 .appendQueryParameter("user_id", userId)
                 .appendQueryParameter("target_id", track.remote_id.toString())
                 .appendQueryParameter("target_type", "Manga")
@@ -160,7 +160,7 @@ class ShikimoriApi(
 
     suspend fun getCurrentUser(): Int {
         return with(json) {
-            authClient.newCall(GET("$apiUrl/users/whoami"))
+            authClient.newCall(GET("$API_URL/users/whoami"))
                 .awaitSuccess()
                 .parseAs<JsonObject>()
                 .let {
@@ -180,39 +180,39 @@ class ShikimoriApi(
     }
 
     private fun accessTokenRequest(code: String) = POST(
-        oauthUrl,
+        OAUTH_URL,
         body = FormBody.Builder()
             .add("grant_type", "authorization_code")
-            .add("client_id", clientId)
-            .add("client_secret", clientSecret)
+            .add("client_id", CLIENT_ID)
+            .add("client_secret", CLIENT_SECRET)
             .add("code", code)
-            .add("redirect_uri", redirectUrl)
+            .add("redirect_uri", REDIRECT_URL)
             .build(),
     )
 
     companion object {
-        private const val clientId = "PB9dq8DzI405s7wdtwTdirYqHiyVMh--djnP7lBUqSA"
-        private const val clientSecret = "NajpZcOBKB9sJtgNcejf8OB9jBN1OYYoo-k4h2WWZus"
+        private const val CLIENT_ID = "PB9dq8DzI405s7wdtwTdirYqHiyVMh--djnP7lBUqSA"
+        private const val CLIENT_SECRET = "NajpZcOBKB9sJtgNcejf8OB9jBN1OYYoo-k4h2WWZus"
 
-        private const val baseUrl = "https://shikimori.one"
-        private const val apiUrl = "$baseUrl/api"
-        private const val oauthUrl = "$baseUrl/oauth/token"
-        private const val loginUrl = "$baseUrl/oauth/authorize"
+        private const val BASE_URL = "https://shikimori.one"
+        private const val API_URL = "$BASE_URL/api"
+        private const val OAUTH_URL = "$BASE_URL/oauth/token"
+        private const val LOGIN_URL = "$BASE_URL/oauth/authorize"
 
-        private const val redirectUrl = "mihon://shikimori-auth"
+        private const val REDIRECT_URL = "mihon://shikimori-auth"
 
-        fun authUrl(): Uri = loginUrl.toUri().buildUpon()
-            .appendQueryParameter("client_id", clientId)
-            .appendQueryParameter("redirect_uri", redirectUrl)
+        fun authUrl(): Uri = LOGIN_URL.toUri().buildUpon()
+            .appendQueryParameter("client_id", CLIENT_ID)
+            .appendQueryParameter("redirect_uri", REDIRECT_URL)
             .appendQueryParameter("response_type", "code")
             .build()
 
         fun refreshTokenRequest(token: String) = POST(
-            oauthUrl,
+            OAUTH_URL,
             body = FormBody.Builder()
                 .add("grant_type", "refresh_token")
-                .add("client_id", clientId)
-                .add("client_secret", clientSecret)
+                .add("client_id", CLIENT_ID)
+                .add("client_secret", CLIENT_SECRET)
                 .add("refresh_token", token)
                 .build(),
         )

komikku-app/komikku

app/src/main/java/eu/kanade/tachiyomi/data/track/bangumi/BangumiApi.kt
@@ -42,7 +42,7 @@ class BangumiApi(
                 .add("rating", track.score.toInt().toString())
                 .add("status", track.toBangumiStatus())
                 .build()
-            authClient.newCall(POST("$apiUrl/collection/${track.remote_id}/update", body = body))
+            authClient.newCall(POST("$API_URL/collection/${track.remote_id}/update", body = body))
                 .awaitSuccess()
             track
         }
@@ -55,7 +55,7 @@ class BangumiApi(
                 .add("rating", track.score.toInt().toString())
                 .add("status", track.toBangumiStatus())
                 .build()
-            authClient.newCall(POST("$apiUrl/collection/${track.remote_id}/update", body = sbody))
+            authClient.newCall(POST("$API_URL/collection/${track.remote_id}/update", body = sbody))
                 .awaitSuccess()
 
             // chapter update
@@ -64,7 +64,7 @@ class BangumiApi(
                 .build()
             authClient.newCall(
                 POST(
-                    "$apiUrl/subject/${track.remote_id}/update/watched_eps",
+                    "$API_URL/subject/${track.remote_id}/update/watched_eps",
                     body = body,
                 ),
             ).awaitSuccess()
@@ -75,7 +75,7 @@ class BangumiApi(
 
     suspend fun search(search: String): List<TrackSearch> {
         return withIOContext {
-            val url = "$apiUrl/search/subject/${URLEncoder.encode(search, StandardCharsets.UTF_8.name())}"
+            val url = "$API_URL/search/subject/${URLEncoder.encode(search, StandardCharsets.UTF_8.name())}"
                 .toUri()
                 .buildUpon()
                 .appendQueryParameter("max_results", "20")
@@ -124,7 +124,7 @@ class BangumiApi(
     suspend fun findLibManga(track: Track): Track? {
         return withIOContext {
             with(json) {
-                authClient.newCall(GET("$apiUrl/subject/${track.remote_id}"))
+                authClient.newCall(GET("$API_URL/subject/${track.remote_id}"))
                     .awaitSuccess()
                     .parseAs<JsonObject>()
                     .let { jsonToSearch(it) }
@@ -134,7 +134,7 @@ class BangumiApi(
 
     suspend fun statusLibManga(track: Track): Track? {
         return withIOContext {
-            val urlUserRead = "$apiUrl/collection/${track.remote_id}"
+            val urlUserRead = "$API_URL/collection/${track.remote_id}"
             val requestUserRead = Request.Builder()
                 .url(urlUserRead)
                 .cacheControl(CacheControl.FORCE_NETWORK)
@@ -171,41 +171,41 @@ class BangumiApi(
     }
 
     private fun accessTokenRequest(code: String) = POST(
-        oauthUrl,
+        OAUTH_URL,
         body = FormBody.Builder()
             .add("grant_type", "authorization_code")
-            .add("client_id", clientId)
-            .add("client_secret", clientSecret)
+            .add("client_id", CLIENT_ID)
+            .add("client_secret", CLIENT_SECRET)
             .add("code", code)
-            .add("redirect_uri", redirectUrl)
+            .add("redirect_uri", REDIRECT_URL)
             .build(),
     )
 
     companion object {
-        private const val clientId = "bgm31586666817a5d03b"
-        private const val clientSecret = "3dad27cf8edcac6f55361d7971d219e1"
+        private const val CLIENT_ID = "bgm31586666817a5d03b"
+        private const val CLIENT_SECRET = "3dad27cf8edcac6f55361d7971d219e1"
 
-        private const val apiUrl = "https://api.bgm.tv"
-        private const val oauthUrl = "https://bgm.tv/oauth/access_token"
-        private const val loginUrl = "https://bgm.tv/oauth/authorize"
+        private const val API_URL = "https://api.bgm.tv"
+        private const val OAUTH_URL = "https://bgm.tv/oauth/access_token"
+        private const val LOGIN_URL = "https://bgm.tv/oauth/authorize"
 
-        private const val redirectUrl = "komikku://bangumi-auth"
+        private const val REDIRECT_URL = "komikku://bangumi-auth"
 
         fun authUrl(): Uri =
-            loginUrl.toUri().buildUpon()
-                .appendQueryParameter("client_id", clientId)
+            LOGIN_URL.toUri().buildUpon()
+                .appendQueryParameter("client_id", CLIENT_ID)
                 .appendQueryParameter("response_type", "code")
-                .appendQueryParameter("redirect_uri", redirectUrl)
+                .appendQueryParameter("redirect_uri", REDIRECT_URL)
                 .build()
 
         fun refreshTokenRequest(token: String) = POST(
-            oauthUrl,
+            OAUTH_URL,
             body = FormBody.Builder()
                 .add("grant_type", "refresh_token")
-                .add("client_id", clientId)
-                .add("client_secret", clientSecret)
+                .add("client_id", CLIENT_ID)
+                .add("client_secret", CLIENT_SECRET)
                 .add("refresh_token", token)
-                .add("redirect_uri", redirectUrl)
+                .add("redirect_uri", REDIRECT_URL)
                 .build(),
         )
     }

app/src/main/java/eu/kanade/tachiyomi/data/track/shikimori/ShikimoriApi.kt
@@ -54,7 +54,7 @@ class ShikimoriApi(
                 }
                 authClient.newCall(
                     POST(
-                        "$apiUrl/v2/user_rates",
+                        "$API_URL/v2/user_rates",
                         body = payload.toString().toRequestBody(jsonMime),
                     ),
                 ).awaitSuccess()
@@ -72,14 +72,14 @@ class ShikimoriApi(
     suspend fun deleteLibManga(track: DomainTrack) {
         withIOContext {
             authClient
-                .newCall(DELETE("$apiUrl/v2/user_rates/${track.libraryId}"))
+                .newCall(DELETE("$API_URL/v2/user_rates/${track.libraryId}"))
                 .awaitSuccess()
         }
     }
 
     suspend fun search(search: String): List<TrackSearch> {
         return withIOContext {
-            val url = "$apiUrl/mangas".toUri().buildUpon()
+            val url = "$API_URL/mangas".toUri().buildUpon()
                 .appendQueryParameter("order", "popularity")
                 .appendQueryParameter("search", search)
                 .appendQueryParameter("limit", "20")
@@ -102,10 +102,10 @@ class ShikimoriApi(
             remote_id = obj["id"]!!.jsonPrimitive.long
             title = obj["name"]!!.jsonPrimitive.content
             total_chapters = obj["chapters"]!!.jsonPrimitive.long
-            cover_url = baseUrl + obj["image"]!!.jsonObject["preview"]!!.jsonPrimitive.content
+            cover_url = BASE_URL + obj["image"]!!.jsonObject["preview"]!!.jsonPrimitive.content
             summary = ""
             score = obj["score"]!!.jsonPrimitive.double
-            tracking_url = baseUrl + obj["url"]!!.jsonPrimitive.content
+            tracking_url = BASE_URL + obj["url"]!!.jsonPrimitive.content
             publishing_status = obj["status"]!!.jsonPrimitive.content
             publishing_type = obj["kind"]!!.jsonPrimitive.content
             start_date = obj["aired_on"]!!.jsonPrimitive.contentOrNull ?: ""
@@ -121,13 +121,13 @@ class ShikimoriApi(
             last_chapter_read = obj["chapters"]!!.jsonPrimitive.double
             score = obj["score"]!!.jsonPrimitive.int.toDouble()
             status = toTrackStatus(obj["status"]!!.jsonPrimitive.content)
-            tracking_url = baseUrl + mangas["url"]!!.jsonPrimitive.content
+            tracking_url = BASE_URL + mangas["url"]!!.jsonPrimitive.content
         }
     }
 
     suspend fun findLibManga(track: Track, userId: String): Track? {
         return withIOContext {
-            val urlMangas = "$apiUrl/mangas".toUri().buildUpon()
+            val urlMangas = "$API_URL/mangas".toUri().buildUpon()
                 .appendPath(track.remote_id.toString())
                 .build()
             val mangas = with(json) {
@@ -136,7 +136,7 @@ class ShikimoriApi(
                     .parseAs<JsonObject>()
             }
 
-            val url = "$apiUrl/v2/user_rates".toUri().buildUpon()
+            val url = "$API_URL/v2/user_rates".toUri().buildUpon()
                 .appendQueryParameter("user_id", userId)
                 .appendQueryParameter("target_id", track.remote_id.toString())
                 .appendQueryParameter("target_type", "Manga")
@@ -160,7 +160,7 @@ class ShikimoriApi(
 
     suspend fun getCurrentUser(): Int {
         return with(json) {
-            authClient.newCall(GET("$apiUrl/users/whoami"))
+            authClient.newCall(GET("$API_URL/users/whoami"))
                 .awaitSuccess()
                 .parseAs<JsonObject>()
                 .let {
@@ -180,39 +180,39 @@ class ShikimoriApi(
     }
 
     private fun accessTokenRequest(code: String) = POST(
-        oauthUrl,
+        OAUTH_URL,
         body = FormBody.Builder()
             .add("grant_type", "authorization_code")
-            .add("client_id", clientId)
-            .add("client_secret", clientSecret)
+            .add("client_id", CLIENT_ID)
+            .add("client_secret", CLIENT_SECRET)
             .add("code", code)
-            .add("redirect_uri", redirectUrl)
+            .add("redirect_uri", REDIRECT_URL)
             .build(),
     )
 
     companion object {
-        private const val clientId = "hst3O_VlZ0HrrXcnZAd6m-SCIc1_mv5cQV-3nQCdJ0E"
-        private const val clientSecret = "n1Pn9AIKvUhDzjehfPt_5nk7xbr4wSnas6djggw_AjE"
+        private const val CLIENT_ID = "hst3O_VlZ0HrrXcnZAd6m-SCIc1_mv5cQV-3nQCdJ0E"
+        private const val CLIENT_SECRET = "n1Pn9AIKvUhDzjehfPt_5nk7xbr4wSnas6djggw_AjE"
 
-        private const val baseUrl = "https://shikimori.one"
-        private const val apiUrl = "$baseUrl/api"
-        private const val oauthUrl = "$baseUrl/oauth/token"
-        private const val loginUrl = "$baseUrl/oauth/authorize"
+        private const val BASE_URL = "https://shikimori.one"
+        private const val API_URL = "$BASE_URL/api"
+        private const val OAUTH_URL = "$BASE_URL/oauth/token"
+        private const val LOGIN_URL = "$BASE_URL/oauth/authorize"
 
-        private const val redirectUrl = "komikku://shikimori-auth"
+        private const val REDIRECT_URL = "komikku://shikimori-auth"
 
-        fun authUrl(): Uri = loginUrl.toUri().buildUpon()
-            .appendQueryParameter("client_id", clientId)
-            .appendQueryParameter("redirect_uri", redirectUrl)
+        fun authUrl(): Uri = LOGIN_URL.toUri().buildUpon()
+            .appendQueryParameter("client_id", CLIENT_ID)
+            .appendQueryParameter("redirect_uri", REDIRECT_URL)
             .appendQueryParameter("response_type", "code")
             .build()
 
         fun refreshTokenRequest(token: String) = POST(
-            oauthUrl,
+            OAUTH_URL,
             body = FormBody.Builder()
                 .add("grant_type", "refresh_token")
-                .add("client_id", clientId)
-                .add("client_secret", clientSecret)
+                .add("client_id", CLIENT_ID)
+                .add("client_secret", CLIENT_SECRET)
                 .add("refresh_token", token)
                 .build(),
         )