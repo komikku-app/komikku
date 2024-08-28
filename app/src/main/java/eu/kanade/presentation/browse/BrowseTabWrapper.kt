4c3daecd1c9511cb761fcc8a7c25af63faaac20e
Cuong-Tran <cuongtran.tm@gmail.com>
2024-08-28 17:55:54 +0700

    add back button on smartsearch source

--- app/src/main/java/eu/kanade/presentation/browse/BrowseTabWrapper.kt
+++ app/src/main/java/eu/kanade/presentation/browse/BrowseTabWrapper.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import cafe.adriel.voyager.navigator.LocalNavigator
 import eu.kanade.presentation.components.AppBar
 import eu.kanade.presentation.components.AppBarActions
 import eu.kanade.presentation.components.TabContent
@@ -13,6 +14,7 @@ import tachiyomi.presentation.core.i18n.stringResource
 @Composable
 fun BrowseTabWrapper(tab: TabContent) {
     val snackbarHostState = remember { SnackbarHostState() }
+    val navigator = LocalNavigator.current
     Scaffold(
         topBar = { scrollBehavior ->
             AppBar(
@@ -21,6 +23,7 @@ fun BrowseTabWrapper(tab: TabContent) {
                     AppBarActions(tab.actions)
                 },
                 scrollBehavior = scrollBehavior,
+                navigateUp = { navigator?.pop() },
             )
         },
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
