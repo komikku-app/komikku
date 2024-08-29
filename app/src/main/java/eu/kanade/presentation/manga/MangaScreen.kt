c6b530da300ae142bc9a342681cbc6e6b182ad57
kana-shii <isabella.vasques1806@gmail.com>
2024-08-29 14:16:44 -0300

    everything went wrong

--- app/src/main/java/eu/kanade/presentation/manga/MangaScreen.kt
+++ app/src/main/java/eu/kanade/presentation/manga/MangaScreen.kt
@@ -595,7 +595,7 @@ private fun MangaScreenSmallImpl(
                             // SY <--
                             // KMK -->
                             status = state.manga.status,
-                            fetchInterval = 0,
+                            fetchInterval = -1,
 
                             // KMK <--
                         )
@@ -1002,7 +1002,7 @@ private fun MangaScreenLargeImpl(
                             // SY <--
                             // KMK -->
                             status = state.manga.status,
-                            fetchInterval = 0,
+                            fetchInterval = -1,
                             // KMK <--
                         )
                         // SY -->
