ebd8c040f3daea84d2d2638c760a976f30ac8c8e
Cuong-Tran <cuongtran.tm@gmail.com>
2024-09-05 13:35:00 +0700

    Revert "Auxiliary commit to revert individual files from 84232c7fe229bf557680a787fa02c055ca18dc66"
    This reverts commit 2c9b6277b276f108a25e90e6be9a227440136063.

--- app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
@@ -47,6 +47,8 @@ import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.NavigatorDisposeBehavior
 import cafe.adriel.voyager.navigator.currentOrThrow
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import eu.kanade.domain.base.BasePreferences
 import eu.kanade.domain.sync.SyncPreferences
 import eu.kanade.presentation.components.AppStateBanners
@@ -81,6 +83,7 @@ import eu.kanade.tachiyomi.ui.manga.MangaScreen
 import eu.kanade.tachiyomi.ui.more.NewUpdateScreen
 import eu.kanade.tachiyomi.ui.more.OnboardingScreen
 import eu.kanade.tachiyomi.util.system.dpToPx
+import eu.kanade.tachiyomi.util.system.isDevFlavor
 import eu.kanade.tachiyomi.util.system.isNavigationBarNeedsScrim
 import eu.kanade.tachiyomi.util.system.isPreviewBuildType
 import eu.kanade.tachiyomi.util.system.isReleaseBuildType
@@ -177,6 +180,7 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         val didMigration = if (isLaunch) {
+            addAnalytics()
             Migrator.awaitAndRelease()
         } else {
             false
@@ -590,6 +594,14 @@ class MainActivity : BaseActivity() {
         ready = true
         return true
     }
+
+    // SY -->
+    private fun addAnalytics() {
+        if (!isDevFlavor && (isPreviewBuildType)) {
+            Firebase.analytics.setUserProperty("preview_version", BuildConfig.COMMIT_COUNT)
+        }
+    }
+    // SY <--
 
     companion object {
         const val INTENT_SEARCH = "eu.kanade.tachiyomi.SEARCH"
