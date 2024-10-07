b1bec3a0c16eb01c95860b55f06e673f48a5f6b6
kana-shii <isabella.vasques1806@gmail.com>
2024-10-06 22:53:58 -0300

    Update GetUpcomingManga.kt

--- domain/src/main/java/mihon/domain/upcoming/interactor/GetUpcomingManga.kt
+++ domain/src/main/java/mihon/domain/upcoming/interactor/GetUpcomingManga.kt
@@ -5,8 +5,11 @@ import kotlinx.coroutines.flow.Flow
 import tachiyomi.domain.manga.model.Manga
 import tachiyomi.domain.manga.repository.MangaRepository
 import kotlinx.coroutines.flow.map
+import tachiyomi.domain.library.model.LibraryManga
 import tachiyomi.domain.library.service.LibraryPreferences
+import tachiyomi.domain.library.service.LibraryPreferences.Companion.MANGA_HAS_UNREAD
 import tachiyomi.domain.library.service.LibraryPreferences.Companion.MANGA_NON_COMPLETED
+import tachiyomi.domain.library.service.LibraryPreferences.Companion.MANGA_NON_READ
 import uy.kohesive.injekt.api.get
 import uy.kohesive.injekt.Injekt
 
@@ -25,17 +28,19 @@ class GetUpcomingManga(
     )
 
     suspend fun subscribe(): Flow<List<Manga>> {
+        return mangaRepository.getUpcomingManga(includedStatuses)
+    }
+
+    suspend fun subscribeLibrary(): Flow<List<LibraryManga>> {
         val libraryPreferences: LibraryPreferences = Injekt.get()
         val restrictions = libraryPreferences.autoUpdateMangaRestrictions().get()
 
         return mangaRepository.getUpcomingManga(includedStatuses)
             .map { mangaList ->
-                if (MANGA_NON_COMPLETED in restrictions) {
-                    mangaList.filter { manga ->
-                        manga.status.toInt() != SManga.COMPLETED
-                    }
-                } else {
-                    mangaList
+                mangaList.filter { manga ->
+                    !(MANGA_NON_COMPLETED in restrictions && manga.status.toInt() == SManga.COMPLETED)
+                        (MANGA_HAS_UNREAD !in restrictions || libraryManga.unreadCount == 0L) &&
+                        (MANGA_NON_READ !in restrictions || libraryManga.totalChapters == 0L || libraryManga.hasStarted)
                 }
             }
     }
