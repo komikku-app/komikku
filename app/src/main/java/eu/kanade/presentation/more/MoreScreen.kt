ImaginaryDesignation/TachiyomiX

Added library update errors screen

app/src/main/java/eu/kanade/presentation/more/MoreScreen.kt
@@ -45,6 +45,7 @@ fun MoreScreen(
     onClickDownloadQueue: () -> Unit,
     onClickCategories: () -> Unit,
     onClickStats: () -> Unit,
+    onClickLibraryUpdateErrors: () -> Unit,
     onClickBackupAndRestore: () -> Unit,
     onClickSettings: () -> Unit,
     onClickAbout: () -> Unit,
@@ -137,6 +138,13 @@ fun MoreScreen(
                     title = stringResource(R.string.label_stats),
                     icon = Icons.Outlined.QueryStats,
                     onPreferenceClick = onClickStats,
+                )
+            }
+            item {
+                TextPreferenceWidget(
+                    title = stringResource(R.string.option_label_library_update_errors),
+                    icon = Icons.Outlined.Info,
+                    onPreferenceClick = onClickLibraryUpdateErrors,
                 )
             }
             item {

komikku-app/komikku

(cherry picked from commit 7cf37d52f959ac65f53cf7657563fb4428bd9188)

app/src/main/java/eu/kanade/presentation/more/MoreScreen.kt
@@ -54,13 +54,15 @@ fun MoreScreen(
     onClickDownloadQueue: () -> Unit,
     onClickCategories: () -> Unit,
     onClickStats: () -> Unit,
-    onClickLibraryUpdateErrors: () -> Unit,
     onClickDataAndStorage: () -> Unit,
     onClickSettings: () -> Unit,
     onClickAbout: () -> Unit,
     onClickBatchAdd: () -> Unit,
     onClickUpdates: () -> Unit,
     onClickHistory: () -> Unit,
+    // KMK -->
+    onClickLibraryUpdateErrors: () -> Unit,
+    // KMK <--
 ) {
     val uriHandler = LocalUriHandler.current
 
@@ -176,6 +178,15 @@ fun MoreScreen(
                     onPreferenceClick = onClickStats,
                 )
             }
+            // KMK -->
+            item {
+                TextPreferenceWidget(
+                    title = stringResource(KMR.strings.option_label_library_update_errors),
+                    icon = Icons.Outlined.NewReleases,
+                    onPreferenceClick = onClickLibraryUpdateErrors,
+                )
+            }
+            // KMK <--
             item {
                 TextPreferenceWidget(
                     title = stringResource(KMR.strings.option_label_library_update_errors),