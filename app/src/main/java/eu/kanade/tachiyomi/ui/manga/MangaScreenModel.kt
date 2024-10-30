b0ccbb07f027b9df15f3b1b6b6575197dc0c42ec
MajorTanya <39014446+MajorTanya@users.noreply.github.com>
2024-10-30 14:48:52 +0100

    Fix sporadically recurring spotless CI failure (mihonapp/mihon#1407)
    Somehow this specific issue keeps getting flagged by unrelated PRs'
    CI runs (but only sometimes? Somehow? Other times the CI run would
    succeed with no spotless issues.)
    
    ---------
    
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>
    (cherry picked from commit ed9e13a365ba1b55cec21c26b93b1c62d29485c8)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
@@ -1340,11 +1340,7 @@ class MangaScreenModel(
                 chapters = chapters.toTypedArray(),
             )
 
-            if (
-                successState?.hasLoggedInTrackers == false ||
-                !read ||
-                autoTrackState == AutoTrackState.NEVER
-            ) {
+            if (!read || successState?.hasLoggedInTrackers == false || autoTrackState == AutoTrackState.NEVER) {
                 return@launchIO
             }
 
