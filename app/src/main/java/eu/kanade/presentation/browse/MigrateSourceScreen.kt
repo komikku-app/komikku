2d8739fa2decac70ebdacc7ab0824d21dbb83a63
Cuong-Tran <cuongtran.tm@gmail.com>
2024-11-14 20:34:57 +0700

    remember migrate screen's obsolete option

--- app/src/main/java/eu/kanade/presentation/browse/MigrateSourceScreen.kt
+++ app/src/main/java/eu/kanade/presentation/browse/MigrateSourceScreen.kt
@@ -23,7 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -112,7 +112,7 @@ private fun MigrateSourceList(
 ) {
     // KMK -->
     val lazyListState = rememberLazyListState()
-    var filterObsoleteSource by remember { mutableStateOf(false) }
+    var filterObsoleteSource by rememberSaveable { mutableStateOf(false) }
 
     BackHandler(enabled = !state.searchQuery.isNullOrBlank()) {
         onChangeSearchQuery("")
