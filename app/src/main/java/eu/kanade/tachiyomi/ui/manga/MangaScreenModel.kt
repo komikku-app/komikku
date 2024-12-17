74d2dd9d0e9178307af3f29b9ced93fff4fcdd8a
Cuong-Tran <cuongtran.tm@gmail.com>
2024-12-17 15:53:03 +0700

    ignore category exclusion when manually delete downloaded chapters

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
@@ -1418,6 +1418,9 @@ class MangaScreenModel(
                         chapters,
                         state.manga,
                         state.source,
+                        // KMK -->
+                        true,
+                        // KMK <--
                     )
                 }
             } catch (e: Throwable) {
