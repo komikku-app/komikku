f6776df15142f5fc55e6f4a40db4e3fa8b622743
Cuong-Tran <cuongtran.tm@gmail.com>
2025-01-08 17:02:43 +0700

    Remove custom extension readme/changelog URLs
    This reverts commit 6d255f25ad1e95ca333af2b8ca4d5515055b685f.

--- app/src/main/java/eu/kanade/tachiyomi/extension/util/ExtensionLoader.kt
+++ app/src/main/java/eu/kanade/tachiyomi/extension/util/ExtensionLoader.kt
@@ -59,8 +59,6 @@ internal object ExtensionLoader {
     private const val METADATA_SOURCE_CLASS = "tachiyomi.extension.class"
     private const val METADATA_SOURCE_FACTORY = "tachiyomi.extension.factory"
     private const val METADATA_NSFW = "tachiyomi.extension.nsfw"
-    private const val METADATA_HAS_README = "tachiyomi.extension.hasReadme"
-    private const val METADATA_HAS_CHANGELOG = "tachiyomi.extension.hasChangelog"
     const val LIB_VERSION_MIN = 1.4
     const val LIB_VERSION_MAX = 1.5
 
@@ -311,9 +309,6 @@ internal object ExtensionLoader {
             return LoadResult.Error
         }
 
-        val hasReadme = appInfo.metaData.getInt(METADATA_HAS_README, 0) == 1
-        val hasChangelog = appInfo.metaData.getInt(METADATA_HAS_CHANGELOG, 0) == 1
-
         val classLoader = try {
             ChildFirstPathClassLoader(appInfo.sourceDir, null, context.classLoader)
         } catch (e: Exception) {
@@ -361,8 +356,6 @@ internal object ExtensionLoader {
             libVersion = libVersion,
             lang = lang,
             isNsfw = isNsfw,
-            hasReadme = hasReadme,
-            hasChangelog = hasChangelog,
             sources = sources,
             pkgFactory = appInfo.metaData.getString(METADATA_SOURCE_FACTORY),
             icon = appInfo.loadIcon(pkgManager),
