f2bf5d8664942a14cc4d4bd3209340786a13c288
Cuong-Tran <cuongtran.tm@gmail.com>
2025-02-02 01:15:07 +0700

    chore (MangaRestorer): move sorting Merged source to sort function

--- app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
@@ -9,6 +9,7 @@ import eu.kanade.tachiyomi.data.backup.models.BackupManga
 import eu.kanade.tachiyomi.data.backup.models.BackupMergedMangaReference
 import eu.kanade.tachiyomi.data.backup.models.BackupTracking
 import exh.EXHMigrations
+import exh.source.MERGED_SOURCE_ID
 import tachiyomi.data.DatabaseHandler
 import tachiyomi.data.UpdateStrategyColumnAdapter
 import tachiyomi.data.manga.MangaMapper
@@ -64,7 +65,10 @@ class MangaRestorer(
 
         return backupMangas
             .sortedWith(
-                compareBy<BackupManga> { it.url in urlsBySource[it.source].orEmpty() }
+                // KMK -->
+                compareBy<BackupManga> { it.source == MERGED_SOURCE_ID }
+                    // KMK <--
+                    .then(compareBy { it.url in urlsBySource[it.source].orEmpty() })
                     .then(compareByDescending { it.lastModifiedAt }),
             )
     }
