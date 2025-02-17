44f5cb761f882c75d554dc5914721512f65520b5
Cuong-Tran <cuongtran.tm@gmail.com>
2025-02-17 19:12:30 +0700

    disable in gradle.kts

--- buildSrc/src/main/kotlin/mihon.code.lint.gradle.kts
+++ buildSrc/src/main/kotlin/mihon.code.lint.gradle.kts
@@ -39,18 +39,18 @@ spotless {
 //                "ktlint_standard_function-expression-body" to "disabled",
 //                "ktlint_standard_function-signature" to "disabled",
 //            ))
-            .editorConfigOverride(mapOf(
-                "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
-                "ktlint_standard_class-signature" to "disabled",
-                "ktlint_standard_discouraged-comment-location" to "disabled",
-                "ktlint_standard_function-expression-body" to "disabled",
-                "ktlint_standard_function-signature" to "disabled",
-                "ktlint_standard_max-line-length" to "disabled",
-                // "ktlint_standard_comment-wrapping" to "disabled",
-                "ktlint_standard_type-argument-comment" to "disabled",
-                "ktlint_standard_value-argument-comment" to "disabled",
-                "ktlint_standard_value-parameter-comment" to "disabled",
-            ))
+            // .editorConfigOverride(mapOf(
+            //     "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
+            //     "ktlint_standard_class-signature" to "disabled",
+            //     "ktlint_standard_discouraged-comment-location" to "disabled",
+            //     "ktlint_standard_function-expression-body" to "disabled",
+            //     "ktlint_standard_function-signature" to "disabled",
+            //     "ktlint_standard_max-line-length" to "disabled",
+            //     // "ktlint_standard_comment-wrapping" to "disabled",
+            //     "ktlint_standard_type-argument-comment" to "disabled",
+            //     "ktlint_standard_value-argument-comment" to "disabled",
+            //     "ktlint_standard_value-parameter-comment" to "disabled",
+            // ))
         trimTrailingWhitespace()
         endWithNewline()
     }
