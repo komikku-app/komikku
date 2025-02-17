865817c587afdc056304708dd9ae5de1048df543
Cuong-Tran <cuongtran.tm@gmail.com>
2025-02-17 22:42:09 +0700

    cleanup

--- buildSrc/src/main/kotlin/mihon.code.lint.gradle.kts
+++ buildSrc/src/main/kotlin/mihon.code.lint.gradle.kts
@@ -23,34 +23,6 @@ spotless {
         target("**/*.kt", "**/*.kts")
         targetExclude("**/build/**/*.kt")
         ktlint(libs.ktlint.core.get().version)
-            // Avoid ktlint failed with messy code from SY
-//            .editorConfigOverride(mapOf(
-//                "ktlint_standard_max-line-length" to "disabled",
-//                "ktlint_standard_argument-list-wrapping" to "disabled",
-//                "ktlint_standard_comment-wrapping" to "disabled",
-//                "ktlint_standard_type-argument-comment" to "disabled",
-//                "ktlint_standard_value-argument-comment" to "disabled",
-//                "ktlint_standard_value-parameter-comment" to "disabled",
-//                "ktlint_standard_property-naming" to "disabled",
-//
-//                "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
-//                "ktlint_standard_class-signature" to "disabled",
-//                "ktlint_standard_discouraged-comment-location" to "disabled",
-//                "ktlint_standard_function-expression-body" to "disabled",
-//                "ktlint_standard_function-signature" to "disabled",
-//            ))
-            // .editorConfigOverride(mapOf(
-            //     "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
-            //     "ktlint_standard_class-signature" to "disabled",
-            //     "ktlint_standard_discouraged-comment-location" to "disabled",
-            //     "ktlint_standard_function-expression-body" to "disabled",
-            //     "ktlint_standard_function-signature" to "disabled",
-            //     "ktlint_standard_max-line-length" to "disabled",
-            //     // "ktlint_standard_comment-wrapping" to "disabled",
-            //     "ktlint_standard_type-argument-comment" to "disabled",
-            //     "ktlint_standard_value-argument-comment" to "disabled",
-            //     "ktlint_standard_value-parameter-comment" to "disabled",
-            // ))
         trimTrailingWhitespace()
         endWithNewline()
     }
