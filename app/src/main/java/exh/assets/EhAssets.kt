4068267cc2cca650d97b4aca60c233a5c9ad44ca
Cuong-Tran <cuongtran.tm@gmail.com>
2025-02-18 00:19:45 +0700

    remove unused EhAssets

--- app/src/main/java/exh/assets/EhAssets.kt
+++ app/src/main/java/exh/assets/EhAssets.kt
@@ -1,22 +1,3 @@
 package exh.assets
 
-import androidx.compose.ui.graphics.vector.ImageVector
-import exh.assets.ehassets.AllAssets
-import exh.assets.ehassets.EhLogo
-import exh.assets.ehassets.Exh
-import exh.assets.ehassets.MangadexLogo
-import kotlin.collections.List as ____KtList
-
-public object EhAssets
-
-@Suppress("ObjectPropertyName")
-private var __AllAssets: ____KtList<ImageVector>? = null
-
-public val EhAssets.AllAssets: ____KtList<ImageVector>
-    get() {
-        if (__AllAssets != null) {
-            return __AllAssets!!
-        }
-        __AllAssets = Exh.AllAssets + listOf(EhLogo, MangadexLogo)
-        return __AllAssets!!
-    }
+object EhAssets
