013f44fc3f4cef720e1d89b044a73a8a2c9a839d
MajorTanya <39014446+MajorTanya@users.noreply.github.com>
2025-03-10 03:43:25 +0100

    Add app ID to debug info (mihonapp/mihon#1847)
    This will avoid the need to know which forks has which version numbers
    and avoid confusion in support.
    
    (cherry picked from commit eddf07f9ac31bab57d06515e42df9c854bc50eed)

--- app/src/main/java/eu/kanade/tachiyomi/util/CrashLogUtil.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/CrashLogUtil.kt
@@ -40,6 +40,7 @@ class CrashLogUtil(
 
     fun getDebugInfo(): String {
         return """
+            App ID: ${BuildConfig.APPLICATION_ID}
             App version: ${BuildConfig.VERSION_NAME} (${BuildConfig.COMMIT_SHA}, ${BuildConfig.VERSION_CODE}, ${BuildConfig.BUILD_TIME})
             Build version: ${BuildConfig.COMMIT_COUNT}
             Android version: ${Build.VERSION.RELEASE} (SDK ${Build.VERSION.SDK_INT}; build ${Build.DISPLAY})
