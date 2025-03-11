81209fb0d69a80daa11260dd1d591a2ee75fb56e
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-03-11 12:55:50 +0600

    Move firebase stuff to a standalone module (mihonapp/mihon#1841)
    (cherry picked from commit 046f09c4bdf8fe8bfe5886afefa0e440376184e7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+import mihon.buildlogic.Config
 import mihon.buildlogic.getBuildTime
 import mihon.buildlogic.getCommitCount
 import mihon.buildlogic.getGitSha
@@ -10,16 +11,6 @@ plugins {
     alias(libs.plugins.aboutLibraries)
     id("com.github.ben-manes.versions")
 }
-
-class ConfigClass {
-    val includeAnalytics: Boolean = project.hasProperty("include-analytics")
-    val enableUpdater: Boolean = project.hasProperty("enable-updater")
-    val enableCodeShrink: Boolean = !project.hasProperty("disable-code-shrink")
-    val includeDependencyInfo: Boolean = project.hasProperty("include-dependency-info")
-}
-
-@Suppress("PropertyName")
-val Config = ConfigClass()
 
 if (Config.includeAnalytics) {
     pluginManager.apply {
@@ -102,8 +93,6 @@ android {
     }
 
     sourceSets {
-        val analyticsDir = if (Config.includeAnalytics) "analytics-firebase" else "analytics-firebase-noop"
-        getByName("main").kotlin.srcDirs("src/$analyticsDir/kotlin")
         getByName("preview").res.srcDirs("src/beta/res")
         getByName("benchmark").res.srcDirs("src/debug/res")
     }
@@ -202,6 +191,7 @@ dependencies {
     implementation(projects.domain)
     implementation(projects.presentationCore)
     implementation(projects.presentationWidget)
+    implementation(projects.telemetry)
 
     // Compose
     implementation(compose.activity)
@@ -306,13 +296,6 @@ dependencies {
     // Logging
     implementation(libs.timber)
     implementation(libs.logcat)
-
-    // Crash reports/analytics
-    if (Config.includeAnalytics) {
-        implementation(platform(libs.firebase.bom))
-        implementation(libs.firebase.analytics)
-        implementation(libs.firebase.crashlytics)
-    }
 
     // Shizuku
     implementation(libs.bundles.shizuku)
