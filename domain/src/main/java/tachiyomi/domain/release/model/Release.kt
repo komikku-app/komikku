cde9a342389a88672991a55b044af11437ebdb2c
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-03-18 19:36:16 +0600

    Tweak the app updater logic and add FOSS build support (mihonapp/mihon#1843)
    (cherry picked from commit 28093935d867e5d1a95944e67c6d1eb46cab4a37)

--- domain/src/main/java/tachiyomi/domain/release/model/Release.kt
+++ domain/src/main/java/tachiyomi/domain/release/model/Release.kt
@@ -1,7 +1,5 @@
 package tachiyomi.domain.release.model
 
-import android.os.Build
-
 /**
  * Contains information about the latest release.
  */
@@ -9,31 +7,9 @@ data class Release(
     val version: String,
     val info: String,
     val releaseLink: String,
-    private val assets: List<String>,
+    val downloadLink: String,
     // KMK -->
     val preRelease: Boolean = false,
     val draft: Boolean = false,
     // KMK <--
-) {
-
-    /**
-     * Get download link of latest release from the assets.
-     * @return download link of latest release.
-     */
-    fun getDownloadLink(): String {
-        val apkVariant = when (Build.SUPPORTED_ABIS[0]) {
-            "arm64-v8a" -> "-arm64-v8a"
-            "armeabi-v7a" -> "-armeabi-v7a"
-            "x86" -> "-x86"
-            "x86_64" -> "-x86_64"
-            else -> ""
-        }
-
-        return assets.find { it.contains("Komikku$apkVariant-") } ?: assets[0]
-    }
-
-    /**
-     * Assets class containing download url.
-     */
-    data class Assets(val downloadLink: String)
-}
+)
