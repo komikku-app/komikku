8786d8b43a473617cd9c5ac9a402858d5dfd42c2
Cuong-Tran <cuongtran.tm@gmail.com>
2025-03-25 15:22:18 +0700

    Fix: Reorder feeds (#786)
    * Fix: Reorder feeds
    
    * Addressing @gemini-code-assist comment
    
    * Convert feeds.items to ImmutableList

--- app/src/main/java/eu/kanade/presentation/browse/FeedOrderScreen.kt
+++ app/src/main/java/eu/kanade/presentation/browse/FeedOrderScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import eu.kanade.presentation.browse.components.FeedOrderListItem
 import eu.kanade.tachiyomi.ui.browse.feed.FeedScreenState
@@ -39,7 +40,7 @@ fun FeedOrderScreen(
             val lazyListState = rememberLazyListState()
             val feeds = state.items
 
-            val feedsState = remember { feeds.toMutableList() }
+            val feedsState = remember { feeds.toMutableStateList() }
             val reorderableState = rememberReorderableLazyListState(lazyListState) { from, to ->
                 val item = feedsState.removeAt(from.index)
                 feedsState.add(to.index, item)
