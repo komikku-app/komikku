537d2115971e77f15fb684351a0c27b58a72eee3
kana-shii <deiimos.mars@gmail.com>
2025-03-28 03:10:54 -0300

    Reapply "Update LibraryScreenModel.kt"
    This reverts commit 2ecd7c3a20aea4a97da0bb10a1a051d88e9b6e3d.

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryScreenModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryScreenModel.kt
@@ -247,6 +247,7 @@ class LibraryScreenModel(
                     prefs.filterUnread,
                     prefs.filterStarted,
                     prefs.filterBookmarked,
+                    prefs.filterFillermarked,
                     prefs.filterCompleted,
                     prefs.filterIntervalCustom,
                     // SY -->
@@ -310,6 +311,7 @@ class LibraryScreenModel(
         val filterUnread = prefs.filterUnread
         val filterStarted = prefs.filterStarted
         val filterBookmarked = prefs.filterBookmarked
+        val filterFillermarked = prefs.filterFillermarked
         val filterCompleted = prefs.filterCompleted
         val filterIntervalCustom = prefs.filterIntervalCustom
 
@@ -341,6 +343,10 @@ class LibraryScreenModel(
 
         val filterFnBookmarked: (LibraryItem) -> Boolean = {
             applyFilter(filterBookmarked) { it.libraryManga.hasBookmarks }
+        }
+
+        val filterFnFillermarked: (LibraryItem) -> Boolean = {
+            applyFilter(filterFillermarked) { it.libraryManga.hasFillermarks }
         }
 
         val filterFnCompleted: (LibraryItem) -> Boolean = {
@@ -379,6 +385,7 @@ class LibraryScreenModel(
                 filterFnUnread(it) &&
                 filterFnStarted(it) &&
                 filterFnBookmarked(it) &&
+                filterFnFillermarked(it) &&
                 filterFnCompleted(it) &&
                 filterFnIntervalCustom(it) &&
                 filterFnTracking(it) &&
@@ -516,6 +523,7 @@ class LibraryScreenModel(
             libraryPreferences.filterUnread().changes(),
             libraryPreferences.filterStarted().changes(),
             libraryPreferences.filterBookmarked().changes(),
+            libraryPreferences.filterFillermarked().changes(),
             libraryPreferences.filterCompleted().changes(),
             libraryPreferences.filterIntervalCustom().changes(),
             // SY -->
@@ -545,6 +553,7 @@ class LibraryScreenModel(
                 // KMK -->
                 sourceBadge = it[13] as Boolean,
                 useLangIcon = it[14] as Boolean,
+                filterFillermarked = it[15] as TriState,
                 // KMK <--
             )
         }
@@ -776,7 +785,7 @@ class LibraryScreenModel(
                                 // SY <--
                                 manga.source,
 
-                            )
+                                )
                     }
                     .let { if (amount != null) it.take(amount) else it }
 
@@ -1430,6 +1439,7 @@ class LibraryScreenModel(
         val filterUnread: TriState,
         val filterStarted: TriState,
         val filterBookmarked: TriState,
+        val filterFillermarked: TriState,
         val filterCompleted: TriState,
         val filterIntervalCustom: TriState,
         // SY -->
