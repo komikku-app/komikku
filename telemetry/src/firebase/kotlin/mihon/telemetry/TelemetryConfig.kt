62beeb41928a70576c0d6b8b51336be5c98b7795
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-04-08 02:07:15 +0600

    Only enable telemetry in Mihon production apps (mihonapp/mihon#1976)
    (cherry picked from commit 5e029b1fe6838da90fc95009a0f675b59e6f04fd)

--- telemetry/src/firebase/kotlin/mihon/telemetry/TelemetryConfig.kt
+++ telemetry/src/firebase/kotlin/mihon/telemetry/TelemetryConfig.kt
@@ -6,25 +6,40 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 object TelemetryConfig {
-    private lateinit var analytics: FirebaseAnalytics
-    private lateinit var crashlytics: FirebaseCrashlytics
+    private var analytics: FirebaseAnalytics? = null
+    private var crashlytics: FirebaseCrashlytics? = null
 
     fun init(context: Context, isPreviewBuildType: Boolean, commitCount: String) {
+        // To stop forks/test builds from polluting our data
+        if (!context.isMihonProductionApp()) return
+
         analytics = FirebaseAnalytics.getInstance(context)
         FirebaseApp.initializeApp(context)
         crashlytics = FirebaseCrashlytics.getInstance()
         // KMK -->
         if (isPreviewBuildType) {
-            analytics.setUserProperty("preview_version", commitCount)
+            analytics?.setUserProperty("preview_version", commitCount)
         }
         // KMK <--
     }
 
     fun setAnalyticsEnabled(enabled: Boolean) {
-        analytics.setAnalyticsCollectionEnabled(enabled)
+        analytics?.setAnalyticsCollectionEnabled(enabled)
     }
 
     fun setCrashlyticsEnabled(enabled: Boolean) {
-        crashlytics.isCrashlyticsCollectionEnabled = enabled
+        crashlytics?.isCrashlyticsCollectionEnabled = enabled
+    }
+
+    private fun Context.isMihonProductionApp(): Boolean {
+        if (packageName !in MIHON_PACKAGES) return false
+
+        return packageManager.getPackageInfo(packageName, SignatureFlags)
+            .getCertificateFingerprints()
+            .any { it == MIHON_CERTIFICATE_FINGERPRINT }
     }
 }
+
+private val MIHON_PACKAGES = hashSetOf("app.komikku", "app.komikku.beta")
+private const val MIHON_CERTIFICATE_FINGERPRINT =
+    "CB:EC:12:1A:A8:2E:BB:02:AA:A7:38:06:99:2E:03:68:A9:7D:47:B5:45:1E:D6:52:48:16:D0:30:84:C4:59:05"
