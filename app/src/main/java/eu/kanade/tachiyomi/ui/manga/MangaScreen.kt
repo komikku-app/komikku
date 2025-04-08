c7befa350de102bad3028ce6b42ee234eeb1f472
Cuong-Tran <cuongtran.tm@gmail.com>
2025-04-08 23:17:37 +0700

    Don't open new Manga screen after migration of duplicated entries

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreen.kt
@@ -470,7 +470,7 @@ class MangaScreen(
                     screenModel = MigrateDialogScreenModel(),
                     onDismissRequest = onDismissRequest,
                     onClickTitle = { navigator.push(MangaScreen(dialog.oldManga.id)) },
-                    onPopScreen = { navigator.replace(MangaScreen(dialog.newManga.id)) },
+                    onPopScreen = { onDismissRequest() },
                 )
             }
             MangaScreenModel.Dialog.SettingsSheet -> ChapterSettingsDialog(
