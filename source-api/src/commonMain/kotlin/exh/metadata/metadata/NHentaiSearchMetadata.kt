b2f90d19c364addae8633759281ab25448b5a969
BrutuZ <BrutuZ@users.noreply.github.com>
2025-05-11 15:16:43 -0300

    Populate Author field and clear Description on a couple of delegated (jobobby04/tachiyomiSY#1432)
    (cherry picked from commit c2d0308ac0319485d443d93ee5bd6ae657001922)

--- source-api/src/commonMain/kotlin/exh/metadata/metadata/NHentaiSearchMetadata.kt
+++ source-api/src/commonMain/kotlin/exh/metadata/metadata/NHentaiSearchMetadata.kt
@@ -71,6 +71,11 @@ class NHentaiSearchMetadata : RaisedSearchMetadata() {
             if (tags.isNotEmpty()) tags.joinToString(transform = { it.name }) else null
         }
 
+        // Set group (if we can find one)
+        val group = tags.ofNamespace(NHENTAI_GROUP_NAMESPACE).let { tags ->
+            if (tags.isNotEmpty()) tags.joinToString(transform = { it.name }) else null
+        }
+
         // Copy tags -> genres
         val genres = tagsToGenreString()
 
@@ -85,16 +90,15 @@ class NHentaiSearchMetadata : RaisedSearchMetadata() {
             }
         }
 
-        val description = null
-
         return manga.copy(
             url = key ?: manga.url,
             thumbnail_url = cover ?: manga.thumbnail_url,
             title = title,
-            artist = artist ?: manga.artist,
+            artist = group ?: manga.artist,
+            author = artist ?: manga.artist,
             genre = genres,
             status = status,
-            description = description,
+            description = null,
         )
     }
 
@@ -134,6 +138,7 @@ class NHentaiSearchMetadata : RaisedSearchMetadata() {
         const val BASE_URL = "https://nhentai.net"
 
         private const val NHENTAI_ARTIST_NAMESPACE = "artist"
+        private const val NHENTAI_GROUP_NAMESPACE = "group"
         const val NHENTAI_CATEGORIES_NAMESPACE = "category"
 
         fun typeToExtension(t: String?) =
