ac0e004e83605bd2d97fd1a67fe8fc87acebed43
DND <tetome@163.com>
2025-05-18 12:38:50 +0800

    Fix E-Hentai Jump/Seek match for detailed date

--- app/src/main/java/eu/kanade/tachiyomi/source/online/all/EHentai.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/online/all/EHentai.kt
@@ -1391,7 +1391,7 @@ class EHentai(
         private const val BLANK_PREVIEW_THUMB = "https://$THUMB_DOMAIN/g/$BLANK_THUMB"
 
         private val MATCH_YEAR_REGEX = "^\\d{4}\$".toRegex()
-        private val MATCH_SEEK_REGEX = "^\\d{2,4}-\\d{1,2}".toRegex()
+        private val MATCH_SEEK_REGEX = "^\\d{2,4}-\\d{1,2}(-\\d{1,2})?".toRegex()
         private val MATCH_JUMP_REGEX = "^\\d+(\$|d\$|w\$|m\$|y\$|-\$)".toRegex()
 
         private const val EH_API_BASE = "https://api.e-hentai.org/api.php"
