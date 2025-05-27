4023e958b8cba5a78fcfff9fd1c72ff1cd7223cb
Cuong-Tran <cuongtran.tm@gmail.com>
2025-05-27 14:10:01 +0700

    fix(release-notes): Fix Github Release Notes incorrectly displayed with Markdown

--- app/src/main/java/eu/kanade/presentation/more/WhatsNewScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/WhatsNewScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import eu.kanade.presentation.manga.components.MarkdownRender
 import eu.kanade.presentation.theme.TachiyomiPreviewTheme
+import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
 import tachiyomi.i18n.MR
 import tachiyomi.i18n.kmk.KMR
 import tachiyomi.i18n.sy.SYMR
@@ -45,7 +46,10 @@ fun WhatsNewScreen(
                 .fillMaxWidth()
                 .padding(vertical = MaterialTheme.padding.large),
         ) {
-            MarkdownRender(content = changelogInfo)
+            MarkdownRender(
+                content = changelogInfo.trimIndent(),
+                flavour = GFMFlavourDescriptor(),
+            )
 
             TextButton(
                 onClick = onOpenInBrowser,
@@ -67,14 +71,43 @@ private fun WhatsNewScreenPreview() {
             currentVersion = "v0.99.9",
             versionName = "v1.00.0",
             changelogInfo = """
-                ## Yay
-                Foobar
+                ## v1.13.1
 
-                ### More info
-                [komikku-app/komikku@23d862d17...48fb4a2e6](https://github.com/komikku-app/komikku/compare/23d862d17...48fb4a2e6)
-                - Hello ([@cuong-tran](@https://github.com/cuong-tran))
-                - World
-            """.trimIndent(),
+
+                #### What's Changed
+                ##### Fix
+
+                - Fix mark existing duplicate read chapters as read option not working in some cases ([@AntsyLich](https://github.com/AntsyLich))
+                - Fix: NaN when dragging `Start/Resume` reading button in MangaScreen ([@cuong-tran](https://github.com/cuong-tran))
+
+
+                **Full Changelog**: [komikku-app/komikku@v1.13.0...v1.13.1](https://github.com/komikku-app/komikku/compare/v1.13.0...v1.13.1)
+
+
+                ---
+                ## v1.12.6
+
+
+                #### What's Changed
+                ##### Fix
+                - bump version ([@cuong-tran](https://github.com/cuong-tran))
+                - rename repo ([@cuong-tran](https://github.com/cuong-tran))
+
+                **Full Changelog**: [komikku-app/komikku@v1.12.5...v1.12.6](https://github.com/komikku-app/komikku/compare/v1.12.5...v1.12.6)
+
+
+                ---
+                ## v1.12.5
+
+
+
+                #### What's Changed
+                ##### Fix
+
+                - Fix (MangasPage): crash when extensions trying to destructuring MangasPage ([@cuong-tran](https://github.com/cuong-tran))
+
+                **Full Changelog**: [komikku-app/komikku@v1.12.4...v1.12.5](https://github.com/komikku-app/komikku/compare/v1.12.4...v1.12.5)
+            """,
             onOpenInBrowser = {},
             onAcceptUpdate = {},
         )
