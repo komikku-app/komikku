54ddfb0e015af325d0a07881d2f868dd28b9a998
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2025-07-04 12:00:00 +0700

    (chore): Don't wrap an intent-chooser inside another intent-chooser (#1505)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreen.kt
@@ -2,7 +2,6 @@ package eu.kanade.tachiyomi.ui.manga
 
 import android.Manifest
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.activity.compose.BackHandler
@@ -693,12 +692,7 @@ class MangaScreen(
         try {
             getMangaUrl(manga_, source_)?.let { url ->
                 val intent = url.toUri().toShareIntent(context, type = "text/plain")
-                context.startActivity(
-                    Intent.createChooser(
-                        intent,
-                        context.stringResource(MR.strings.action_share),
-                    ),
-                )
+                context.startActivity(intent)
             }
         } catch (e: Exception) {
             context.toast(e.message)
