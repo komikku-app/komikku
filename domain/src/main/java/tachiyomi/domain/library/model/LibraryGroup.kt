5cf9ce3edee09b5f79dc46023de6667f25bf7e16
Cuong-Tran <cuongtran.tm@gmail.com>
2025-07-22 18:12:05 +0700

    fix(library): Fix Ungrouped UI

--- domain/src/main/java/tachiyomi/domain/library/model/LibraryGroup.kt
+++ domain/src/main/java/tachiyomi/domain/library/model/LibraryGroup.kt
@@ -12,13 +12,13 @@ object LibraryGroup {
     const val BY_TRACK_STATUS = 3
     const val UNGROUPED = 4
 
-    fun groupTypeStringRes(type: Int, hasCategories: Boolean = true): StringResource {
+    fun groupTypeStringRes(type: Int): StringResource {
         return when (type) {
             BY_STATUS -> MR.strings.status
             BY_SOURCE -> MR.strings.label_sources
             BY_TRACK_STATUS -> SYMR.strings.tracking_status
             UNGROUPED -> SYMR.strings.ungrouped
-            else -> if (hasCategories) MR.strings.categories else SYMR.strings.ungrouped
+            else -> MR.strings.categories
         }
     }
 }
