a6c8b50510dcb8b83f12b94a89db5902d04f6241
Fermin Cirella <fermincirella.ar@gmail.com>
2025-08-07 15:02:43 -0300

    feat(library): Add library preference to show empty categories during search/filtering

--- domain/src/main/java/tachiyomi/domain/library/service/LibraryPreferences.kt
+++ domain/src/main/java/tachiyomi/domain/library/service/LibraryPreferences.kt
@@ -224,6 +224,10 @@ class LibraryPreferences(
     fun autoClearChapterCache() = preferenceStore.getBoolean("auto_clear_chapter_cache", false)
 
     fun hideMissingChapters() = preferenceStore.getBoolean("pref_hide_missing_chapter_indicators", false)
+
+    // KMK -->
+    fun showEmptyCategoriesSearch() = preferenceStore.getBoolean("show_empty_categories_search", false)
+    // KMK <--
     // endregion
 
     // region Swipe Actions
