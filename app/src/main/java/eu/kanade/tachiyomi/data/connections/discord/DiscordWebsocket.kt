825d6b72a4a497f1bc1976c5effd758a29ad0dbe
Cuong-Tran <cuongtran.tm@gmail.com>
2025-08-19 14:45:11 +0700

    Using IO coroutine instead

--- app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/DiscordWebsocket.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/DiscordWebsocket.kt
@@ -56,7 +56,7 @@ open class DiscordWebSocketImpl(
     private var connected = false
 
     override val coroutineContext: CoroutineContext
-        get() = SupervisorJob() + Dispatchers.Default
+        get() = SupervisorJob() + Dispatchers.IO
 
     private fun sendIdentify() {
         val response = Identity.Response(
