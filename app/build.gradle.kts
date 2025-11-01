117af304a96d028b59c3b050e27905e833321dd3
NGB-Was-Taken <76197326+NGB-Was-Taken@users.noreply.github.com>
2025-11-01 13:53:10 +0545

    Update shizuku.version to v13.1.5 (mihonapp/mihon#2566)
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>
    (cherry picked from commit e9469451acf6c7ad39ff08e344ad5013ddc39337)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -147,9 +147,9 @@ android {
     buildFeatures {
         viewBinding = true
         buildConfig = true
+        aidl = true
 
         // Disable some unused things
-        aidl = false
         renderScript = false
         shaders = false
     }
