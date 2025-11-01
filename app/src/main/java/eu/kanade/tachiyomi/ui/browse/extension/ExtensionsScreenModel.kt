dfacf681fae73dd45a86d2ef3fb2ff1290aee24a
c2y5 <97918490+c2y5@users.noreply.github.com>
2025-11-02 03:09:22 +1300

    Fix extension download stuck at pending state in some cases (mihonapp/mihon#2483)
    Also auto update extension list whenever a repository is added or removed
    
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>
    (cherry picked from commit 87c6f34a558b009be4d316e396ec3eeb4cfbbbf8)

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/extension/ExtensionsScreenModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/extension/ExtensionsScreenModel.kt
@@ -198,6 +198,7 @@ class ExtensionsScreenModel(
 
     fun cancelInstallUpdateExtension(extension: Extension) {
         extensionManager.cancelInstallUpdateExtension(extension)
+        removeDownloadState(extension)
     }
 
     private fun addDownloadState(extension: Extension, installStep: InstallStep) {
