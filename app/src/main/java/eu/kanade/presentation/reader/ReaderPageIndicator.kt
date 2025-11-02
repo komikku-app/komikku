1ea7bc5ddbb7b34d33e1b532053304c29fe35fc8
AntsyLich <59261191+antsylich@users.noreply.github.com>
2025-11-02 19:41:33 +0600

    Make reader edge-to-edge (mihonapp/mihon#1908)
    (cherry picked from commit 5f0c4606681cd59b38ae0855c7827e149fa5488c)

--- app/src/main/java/eu/kanade/presentation/reader/ReaderPageIndicator.kt
+++ app/src/main/java/eu/kanade/presentation/reader/ReaderPageIndicator.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.TextStyle
@@ -15,11 +16,12 @@ import androidx.compose.ui.unit.sp
 import eu.kanade.presentation.theme.TachiyomiPreviewTheme
 
 @Composable
-fun PageIndicatorText(
+fun ReaderPageIndicator(
     // SY -->
     currentPage: String,
     // SY <--
     totalPages: Int,
+    modifier: Modifier = Modifier,
 ) {
     if (currentPage.isEmpty() || totalPages <= 0) return
 
@@ -40,6 +42,7 @@ fun PageIndicatorText(
 
     Box(
         contentAlignment = Alignment.Center,
+        modifier = modifier,
     ) {
         Text(
             text = text,
@@ -54,10 +57,10 @@ fun PageIndicatorText(
 
 @PreviewLightDark
 @Composable
-private fun PageIndicatorTextPreview() {
+private fun ReaderPageIndicatorPreview() {
     TachiyomiPreviewTheme {
         Surface {
-            PageIndicatorText(currentPage = "10", totalPages = 69)
+            ReaderPageIndicator(currentPage = "10", totalPages = 69)
         }
     }
 }
--- app/src/main/java/eu/kanade/presentation/reader/appbars/ReaderAppBars.kt
+++ app/src/main/java/eu/kanade/presentation/reader/appbars/ReaderAppBars.kt
@@ -2,10 +2,13 @@ package eu.kanade.presentation.reader.appbars
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
@@ -13,13 +16,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Bookmark
-import androidx.compose.material.icons.outlined.BookmarkBorder
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
@@ -30,20 +32,16 @@ import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import eu.kanade.presentation.components.AppBar
-import eu.kanade.presentation.components.AppBarActions
 import eu.kanade.presentation.reader.components.ChapterNavigator
 import eu.kanade.tachiyomi.ui.reader.setting.ReaderOrientation
 import eu.kanade.tachiyomi.ui.reader.setting.ReadingMode
 import eu.kanade.tachiyomi.ui.reader.viewer.Viewer
 import eu.kanade.tachiyomi.ui.reader.viewer.pager.R2LPagerViewer
 import kotlinx.collections.immutable.ImmutableSet
-import kotlinx.collections.immutable.persistentListOf
-import tachiyomi.i18n.MR
 import tachiyomi.presentation.core.components.material.padding
-import tachiyomi.presentation.core.i18n.stringResource
 
-private val animationSpec = tween<IntOffset>(200)
+private val readerBarsSlideAnimationSpec = tween<IntOffset>(200)
+private val readerBarsFadeAnimationSpec = tween<Float>(150)
 
 // SY -->
 enum class NavBarType {
@@ -72,7 +70,6 @@ fun BoxIgnoreLayoutDirection(modifier: Modifier, content: @Composable BoxScope.(
 @Composable
 fun ReaderAppBars(
     visible: Boolean,
-    fullscreen: Boolean,
 
     mangaTitle: String?,
     chapterTitle: String?,
@@ -129,28 +126,18 @@ fun ReaderAppBars(
         .surfaceColorAtElevation(3.dp)
         .copy(alpha = if (isSystemInDarkTheme()) 0.9f else 0.95f)
 
-    val modifierWithInsetsPadding = if (fullscreen) {
-        Modifier.systemBarsPadding()
-    } else {
-        Modifier
-    }
-
     // SY -->
     BoxIgnoreLayoutDirection(
         Modifier.fillMaxWidth(),
     ) {
         AnimatedVisibility(
             visible = visible && navBarType == NavBarType.VerticalLeft,
-            enter = slideInHorizontally(
-                initialOffsetX = { -it },
-                animationSpec = animationSpec,
-            ),
-            exit = slideOutHorizontally(
-                targetOffsetX = { -it },
-                animationSpec = animationSpec,
-            ),
-            modifier = modifierWithInsetsPadding
-                .padding(bottom = 64.dp, top = 112.dp)
+            enter = slideInHorizontally(initialOffsetX = { -it }, animationSpec = readerBarsSlideAnimationSpec) +
+                fadeIn(animationSpec = readerBarsFadeAnimationSpec),
+            exit = slideOutHorizontally(targetOffsetX = { -it }, animationSpec = readerBarsSlideAnimationSpec) +
+                fadeOut(animationSpec = readerBarsFadeAnimationSpec),
+            modifier = Modifier
+                .padding(top = 164.dp, bottom = 70.dp)
                 .align(Alignment.TopStart),
         ) {
             ChapterNavigator(
@@ -162,23 +149,21 @@ fun ReaderAppBars(
                 currentPage = currentPage,
                 totalPages = totalPages,
                 onPageIndexChange = onPageIndexChange,
+                // SY -->
                 isVerticalSlider = true,
                 currentPageText = currentPageText,
+                // SY <--
             )
         }
 
         AnimatedVisibility(
             visible = visible && navBarType == NavBarType.VerticalRight,
-            enter = slideInHorizontally(
-                initialOffsetX = { it },
-                animationSpec = animationSpec,
-            ),
-            exit = slideOutHorizontally(
-                targetOffsetX = { it },
-                animationSpec = animationSpec,
-            ),
-            modifier = modifierWithInsetsPadding
-                .padding(bottom = 64.dp, top = 112.dp)
+            enter = slideInHorizontally(initialOffsetX = { it }, animationSpec = readerBarsSlideAnimationSpec) +
+                fadeIn(animationSpec = readerBarsFadeAnimationSpec),
+            exit = slideOutHorizontally(targetOffsetX = { it }, animationSpec = readerBarsSlideAnimationSpec) +
+                fadeOut(animationSpec = readerBarsFadeAnimationSpec),
+            modifier = Modifier
+                .padding(top = 164.dp, bottom = 70.dp)
                 .align(Alignment.TopEnd),
         ) {
             ChapterNavigator(
@@ -190,87 +175,38 @@ fun ReaderAppBars(
                 currentPage = currentPage,
                 totalPages = totalPages,
                 onPageIndexChange = onPageIndexChange,
+                // SY -->
                 isVerticalSlider = true,
                 currentPageText = currentPageText,
+                // SY <--
             )
         }
         // SY <--
-        Column(
-            modifier = Modifier.fillMaxHeight(),
-            verticalArrangement = Arrangement.SpaceBetween,
-        ) {
+        Column(modifier = Modifier.fillMaxHeight()) {
             AnimatedVisibility(
                 visible = visible,
-                enter = slideInVertically(
-                    initialOffsetY = { -it },
-                    animationSpec = animationSpec,
-                ),
-                exit = slideOutVertically(
-                    targetOffsetY = { -it },
-                    animationSpec = animationSpec,
-                ),
+                enter = slideInVertically(initialOffsetY = { -it }, animationSpec = readerBarsSlideAnimationSpec) +
+                    fadeIn(animationSpec = readerBarsFadeAnimationSpec),
+                exit = slideOutVertically(targetOffsetY = { -it }, animationSpec = readerBarsSlideAnimationSpec) +
+                    fadeOut(animationSpec = readerBarsFadeAnimationSpec),
             ) {
                 // SY -->
-                Column(modifierWithInsetsPadding) {
+                Column {
                     // SY <--
-                    AppBar(
-                        modifier = /*SY --> */ Modifier /*SY <-- */
+                    ReaderTopBar(
+                        modifier = Modifier
+                            .background(backgroundColor)
                             .clickable(onClick = onClickTopAppBar),
-                        backgroundColor = backgroundColor,
-                        title = mangaTitle,
-                        subtitle = chapterTitle,
+                        mangaTitle = mangaTitle,
+                        chapterTitle = chapterTitle,
                         navigateUp = navigateUp,
-                        actions = {
-                            AppBarActions(
-                                actions = persistentListOf<AppBar.AppBarAction>().builder()
-                                    .apply {
-                                        add(
-                                            AppBar.Action(
-                                                title = stringResource(
-                                                    if (bookmarked) {
-                                                        MR.strings.action_remove_bookmark
-                                                    } else {
-                                                        MR.strings.action_bookmark
-                                                    },
-                                                ),
-                                                icon = if (bookmarked) {
-                                                    Icons.Outlined.Bookmark
-                                                } else {
-                                                    Icons.Outlined.BookmarkBorder
-                                                },
-                                                onClick = onToggleBookmarked,
-                                            ),
-                                        )
-                                        /* SY -->
-                                        onOpenInWebView?.let {
-                                            add(
-                                                AppBar.OverflowAction(
-                                                    title = stringResource(MR.strings.action_open_in_web_view),
-                                                    onClick = it,
-                                                ),
-                                            )
-                                        }
-                                        onOpenInBrowser?.let {
-                                            add(
-                                                AppBar.OverflowAction(
-                                                    title = stringResource(MR.strings.action_open_in_browser),
-                                                    onClick = it,
-                                                ),
-                                            )
-                                        }
-                                        onShare?.let {
-                                            add(
-                                                AppBar.OverflowAction(
-                                                    title = stringResource(MR.strings.action_share),
-                                                    onClick = it,
-                                                ),
-                                            )
-                                        }
-                                        SY <-- */
-                                    }
-                                    .build(),
-                            )
-                        },
+                        bookmarked = bookmarked,
+                        onToggleBookmarked = onToggleBookmarked,
+                        // SY -->
+                        onOpenInWebView = null, // onOpenInWebView,
+                        onOpenInBrowser = null, // onOpenInBrowser,
+                        onShare = null, // onShare,
+                        // SY <--
                     )
                     // SY -->
                     ExhUtils(
@@ -288,28 +224,23 @@ fun ReaderAppBars(
                         onClickBoostPage = onClickBoostPage,
                         onClickBoostPageHelp = onClickBoostPageHelp,
                     )
-                    // SY <--
                 }
+                // SY <--
             }
 
             Spacer(modifier = Modifier.weight(1f))
 
             AnimatedVisibility(
                 visible = visible,
-                enter = slideInVertically(
-                    initialOffsetY = { it },
-                    animationSpec = animationSpec,
-                ),
-                exit = slideOutVertically(
-                    targetOffsetY = { it },
-                    animationSpec = animationSpec,
-                ),
+                enter = slideInVertically(initialOffsetY = { it }, animationSpec = readerBarsSlideAnimationSpec) +
+                    fadeIn(animationSpec = readerBarsFadeAnimationSpec),
+                exit = slideOutVertically(targetOffsetY = { it }, animationSpec = readerBarsSlideAnimationSpec) +
+                    fadeOut(animationSpec = readerBarsFadeAnimationSpec),
             ) {
-                Column(
-                    modifier = modifierWithInsetsPadding,
-                    verticalArrangement = Arrangement.spacedBy(MaterialTheme.padding.small),
-                ) {
+                Column(verticalArrangement = Arrangement.spacedBy(MaterialTheme.padding.small)) {
+                    // SY -->
                     if (navBarType == NavBarType.Bottom) {
+                        // SY <--
                         ChapterNavigator(
                             isRtl = isRtl,
                             onNextChapter = onNextChapter,
@@ -319,15 +250,18 @@ fun ReaderAppBars(
                             currentPage = currentPage,
                             totalPages = totalPages,
                             onPageIndexChange = onPageIndexChange,
+                            // SY -->
                             isVerticalSlider = false,
                             currentPageText = currentPageText,
+                            // SY <--
                         )
                     }
-                    BottomReaderBar(
-                        // SY -->
-                        enabledButtons = enabledButtons,
-                        // SY <--
-                        backgroundColor = backgroundColor,
+                    ReaderBottomBar(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(backgroundColor)
+                            .padding(horizontal = MaterialTheme.padding.small)
+                            .windowInsetsPadding(WindowInsets.navigationBars),
                         readingMode = readingMode,
                         onClickReadingMode = onClickReadingMode,
                         orientation = orientation,
@@ -336,6 +270,7 @@ fun ReaderAppBars(
                         onClickCropBorder = onClickCropBorder,
                         onClickSettings = onClickSettings,
                         // SY -->
+                        enabledButtons = enabledButtons,
                         currentReadingMode = currentReadingMode,
                         dualPageSplitEnabled = dualPageSplitEnabled,
                         doublePages = doublePages,
