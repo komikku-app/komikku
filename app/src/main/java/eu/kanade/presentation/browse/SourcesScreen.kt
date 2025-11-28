b24b14c73d5ae992534d1350ebd8373d5826f3a6
dhanuarf <10323845+dhanuarf@users.noreply.github.com>
2025-11-28 14:21:41 +0700

    fix(sources-search): search box visibility glitch when scrolling (#1278)
    * fix(sources-screen): search box visibility glitch when scrolling
    
    * Refactor to a reusable function
    
    * Apply to MigrateSourceScreen
    
    * Fix warning
    
    * suggestions
    
    * Revert "Fix warning"
    
    This reverts commit b1c97bcf04b9e34b3eb314fd54c45be953d2f451.
    
    * Add comment
    
    ---------
    
    Co-authored-by: Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>

--- app/src/main/java/eu/kanade/presentation/browse/SourcesScreen.kt
+++ app/src/main/java/eu/kanade/presentation/browse/SourcesScreen.kt
@@ -1,7 +1,9 @@
 package eu.kanade.presentation.browse
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,6 +25,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -82,24 +85,12 @@ fun SourcesScreen(
             modifier = Modifier.padding(contentPadding),
         )
         // KMK -->
-        else -> Column(
-            // Wrap around so we can use stickyHeader
+        else -> Box(
             modifier = Modifier.padding(contentPadding),
         ) {
-            AnimatedFloatingSearchBox(
-                listState = lazyListState,
-                searchQuery = state.searchQuery,
-                onChangeSearchQuery = onChangeSearchQuery,
-                placeholderText = stringResource(KMR.strings.action_search_for_source),
-                modifier = Modifier
-                    .padding(
-                        horizontal = MaterialTheme.padding.medium,
-                        vertical = MaterialTheme.padding.small,
-                    ),
-            )
-
             FastScrollLazyColumn(
                 state = lazyListState,
+                contentPadding = PaddingValues(top = 65.dp),
                 // KMK <--
             ) {
                 items(
@@ -141,6 +132,22 @@ fun SourcesScreen(
                     }
                 }
             }
+
+            // KMK -->
+            AnimatedFloatingSearchBox(
+                listState = lazyListState,
+                searchQuery = state.searchQuery,
+                onChangeSearchQuery = onChangeSearchQuery,
+                placeholderText = stringResource(KMR.strings.action_search_for_source),
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(
+                        horizontal = MaterialTheme.padding.medium,
+                        vertical = MaterialTheme.padding.small,
+                    )
+                    .align(Alignment.TopCenter),
+            )
+            // KMK <--
         }
     }
 }
