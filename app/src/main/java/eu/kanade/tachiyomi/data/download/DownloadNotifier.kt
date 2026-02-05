73d21ed591e0dc540f1adb8c652c80052adb2fae
MuhamadSyabitHidayattulloh <tebepc@gmail.com>
2026-02-05 14:40:28 +0700

    fix(notifications): paused notifications not showing

--- app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadNotifier.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadNotifier.kt
@@ -66,6 +66,14 @@ internal class DownloadNotifier(private val context: Context) {
      */
     fun dismissProgress() {
         context.cancelNotification(Notifications.ID_DOWNLOAD_CHAPTER_PROGRESS)
+        context.cancelNotification(Notifications.ID_DOWNLOAD_CHAPTER_PAUSED)
+    }
+
+    /**
+     * Dismiss the pause notification. Called when resuming downloads.
+     */
+    fun dismissPaused() {
+        context.cancelNotification(Notifications.ID_DOWNLOAD_CHAPTER_PAUSED)
     }
 
     /**
@@ -147,7 +155,7 @@ internal class DownloadNotifier(private val context: Context) {
                 NotificationReceiver.clearDownloadsPendingBroadcast(context),
             )
 
-            show(Notifications.ID_DOWNLOAD_CHAPTER_PROGRESS)
+            show(Notifications.ID_DOWNLOAD_CHAPTER_PAUSED)
         }
 
         // Reset initial values
