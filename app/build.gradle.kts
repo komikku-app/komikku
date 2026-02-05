a510de445171570f0b37743fec13877fe8f587d6
MajorTanya <39014446+majortanya@users.noreply.github.com>
2026-02-05 06:29:21 +0100

    Clean up some build warnings (mihonapp/mihon#2929)
    * Replace deprecated rememberPlainTooltipPositionProvider
    
    * Remove superfluous when branch
    
    This when is marked as exhaustive.
    
    * Replace deprecated LibrariesContainer call
    
    AboutLibraries now wants us to produce the libraries ourselves.
    
    * Replace deprecated ClipboardManager with Clipboard
    
    Clipboard uses suspend functions, hence the coroutine scope addition.
    
    * Use multi-dollar strs to simplify GraphQL queries
    
    These have been available since Kotlin 2.1.
    
    * Remove various redundant casts & conversions
    
    - WebViewScreenContent: loadingState is in the LoadingState.Loading
      branch, no need to cast at all
    - Bangumi: username is not modified, make val
    - Kavita: token is already a String
    - PagerViewerAdapter: insertPageLastPage is already null-checked
    - PagerViewerAdapter: use reified filterIsInstance
    - ReaderViewModel: chapter IDs are already Longs
    - CloudflareInterceptor: webview is smart-cast to non-null here
    
    * Replace deprecated MenuAnchorType
    
    Literally just a typealias for ExposedDropdownMenuAnchorType anyway.
    
    * OptimizeNonSkippingGroups is enabled by default
    
    * Suppress shadowing warning
    
    This is explicitly intentional according to the KDocs.
    
    * Migrate Context Receivers to Context Parameters
    
    Requires changing the compiler arg, but that is part of the migration:
    
    https://blog.jetbrains.com/kotlin/2025/04/update-on-context-parameters
    
    Apparently, the only visible change is that names are required now.
    "_" can be used for anonymous context parameters.
    
    * Fix expression bodies with explicit return
    
    Naming conflict resolved by aliasing.
    
    From 2.4/2.5 onward, these will only be allowed with explicit return
    types, or have to be turned into a block body. I opted for the latter
    since the function is reasonably dense already.
    
    see: https://youtrack.jetbrains.com/issue/KTLC-288
    
    * Suppress deprecation of non-AutoMirrored icons
    
    We use these arrows for navigation in the Upcoming screen.
    I strongly doubt the AutoMirrored versions would make sense for our
    use-case.
    
    * Explicitly opt-in to new annotation default rules
    
    affects the following annotated value-parameters:
    - Preference.SliderPreference.steps (`@IntRange`)
    - ReaderViewModel.State.brightnessOverlayValue (`@IntRange`)
    - ReadingMode.iconRes (`@DrawableRes`)
    - MigrationListScreenModel.Dialog.Progress.progress (`@FloatRange`)
    
    see: https://youtrack.jetbrains.com/issue/KT-73255
    see: https://github.com/Kotlin/KEEP/blob/change-defaulting-rule/proposals/annotation-target-in-properties.md
    
    Warning message was the following:
    
        This annotation is currently applied to the value parameter only, but in the future it will also be applied to field.
        - To opt in to applying to both value parameter and field, add '-Xannotation-default-target=param-property' to your compiler arguments.
        - To keep applying to the value parameter only, use the '@param:' annotation target.
    
    (cherry picked from commit b543bc089a442c5e93b0fb6c83bc4037740b1eb5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -175,6 +175,7 @@ kotlin {
             "-opt-in=kotlinx.coroutines.FlowPreview",
             "-opt-in=kotlinx.coroutines.InternalCoroutinesApi",
             "-opt-in=kotlinx.serialization.ExperimentalSerializationApi",
+            "-Xannotation-default-target=param-property",
         )
     }
 }
