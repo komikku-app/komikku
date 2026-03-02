e16f9ddfdd10a252ac26df81f60f25f33d22cc4b
KaiserBh <kaiserbh@protonmail.com>
2026-03-02 15:13:56 +1100

    chore: remove logging from AndroidCookieJar
    (cherry picked from commit 78efe02e726056bab67fc64f25da965ec111b0c1)

--- core/common/src/main/kotlin/eu/kanade/tachiyomi/network/AndroidCookieJar.kt
+++ core/common/src/main/kotlin/eu/kanade/tachiyomi/network/AndroidCookieJar.kt
@@ -1,6 +1,5 @@
 package eu.kanade.tachiyomi.network
 
-import android.util.Log
 import android.webkit.CookieManager
 import okhttp3.Cookie
 import okhttp3.CookieJar
@@ -55,12 +54,6 @@ class AndroidCookieJar : CookieJar {
 
     fun addAll(url: HttpUrl, cookies: List<Cookie>) {
         val urlString = url.toString()
-        Log.d("AndroidCookieJar", "Adding cookies to URL: $urlString")
-
-        // Log incoming cookies to add
-        cookies.forEach { newCookie ->
-            Log.d("AndroidCookieJar", "Incoming cookie: ${newCookie.name}=${newCookie.value}")
-        }
 
         // Get existing cookies for the URL
         val existingCookies = manager.getCookie(urlString)?.split("; ")?.associate {
@@ -68,23 +61,13 @@ class AndroidCookieJar : CookieJar {
             name to value
         }?.toMutableMap() ?: mutableMapOf()
 
-        Log.d("AndroidCookieJar", "Existing cookies: $existingCookies")
-
         // Add or update the cookies
         cookies.forEach { newCookie ->
-            Log.d("AndroidCookieJar", "Adding/updating cookie: ${newCookie.name}=${newCookie.value}")
             existingCookies[newCookie.name] = newCookie.value
         }
 
         // Convert the map back to a string and set it in the cookie manager
         val finalCookiesString = existingCookies.entries.joinToString("; ") { "${it.key}=${it.value}" }
-        Log.d("AndroidCookieJar", "Final cookies string: $finalCookiesString")
         manager.setCookie(urlString, finalCookiesString)
-
-        // Verify if cookies are set correctly
-        val setCookies = manager.getCookie(urlString)
-        Log.d("AndroidCookieJar", "Set cookies in manager: $setCookies")
-
-        Log.d("AndroidCookieJar", "All cookies added for URL: $urlString")
     }
 }
