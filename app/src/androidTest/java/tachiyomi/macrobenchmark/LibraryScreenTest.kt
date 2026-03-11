39bf7e73815c4bcd5880b35978f1bc4b66c5422d
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2026-03-11 17:43:35 +0700

    Perform various app actions

--- app/src/androidTest/java/tachiyomi/macrobenchmark/LibraryScreenTest.kt
+++ app/src/androidTest/java/tachiyomi/macrobenchmark/LibraryScreenTest.kt
@@ -108,4 +108,92 @@ class LibraryScreenTest {
             device.findObject(By.pkg(APP_PACKAGE).depth(0)),
         )
     }
+
+    @Test
+    fun performVariousActions_doesNotCrash() {
+        Thread.sleep(10000)
+        device.waitForIdle()
+
+        device.findObject(By.descContains("Filter"))?.click()
+        device.waitForIdle()
+        Thread.sleep(1000)
+        device.findObject(By.text("Downloaded"))?.let {
+            it.click()
+            device.waitForIdle()
+            it.click()
+            device.waitForIdle()
+            it.click()
+            device.waitForIdle()
+        }
+        device.pressBack()
+        device.waitForIdle()
+
+        device.findObject(By.desc("More options"))?.click()
+        device.waitForIdle()
+        device.findObject(By.text("Update category"))?.click()
+        device.waitForIdle()
+
+        // Open library manga
+        device.click(200, 600)
+        Thread.sleep(5000)
+        device.waitForIdle()
+        device.pressBack()
+        device.waitForIdle()
+
+        device.findObject(By.text("Updates"))?.click()
+        Thread.sleep(1000)
+        device.waitForIdle()
+        device.click(500, 600)
+        Thread.sleep(10000)
+        device.waitForIdle()
+        device.pressBack()
+        device.waitForIdle()
+
+        device.findObject(By.text("History"))?.click()
+        Thread.sleep(1000)
+        device.waitForIdle()
+        device.click(500, 500)
+        Thread.sleep(10000)
+        device.waitForIdle()
+        device.pressBack()
+        device.waitForIdle()
+
+        device.findObject(By.text("Browse"))?.click()
+        Thread.sleep(1000)
+        device.waitForIdle()
+        device.click(500, 900)
+        Thread.sleep(10000)
+        device.waitForIdle()
+        device.pressBack()
+        device.waitForIdle()
+
+        device.findObject(By.text("Feed"))?.click()
+        Thread.sleep(5000)
+        device.waitForIdle()
+        device.click(200, 700)
+        Thread.sleep(5000)
+        device.waitForIdle()
+        device.pressBack()
+        device.waitForIdle()
+
+        device.findObject(By.text("Extensions"))?.click()
+        Thread.sleep(500)
+        device.waitForIdle()
+        device.findObject(By.text("Migrate"))?.click()
+        Thread.sleep(500)
+        device.waitForIdle()
+        device.findObject(By.text("More"))?.click()
+        Thread.sleep(500)
+        device.waitForIdle()
+        device.findObject(By.text("Settings"))?.click()
+        Thread.sleep(500)
+        device.waitForIdle()
+
+        Thread.sleep(5000)
+        device.waitForIdle()
+        assertNotNull(
+            "App window should still be present",
+            device.findObject(By.pkg(APP_PACKAGE).depth(0)),
+        )
+    }
 }
