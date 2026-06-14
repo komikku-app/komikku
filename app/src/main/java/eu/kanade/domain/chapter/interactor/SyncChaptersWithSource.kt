b007a26016f0d2f92147d6e7afd558d5d3422b14
MajorTanya <39014446+MajorTanya@users.noreply.github.com>
2026-06-14 16:47:11 +0200

    Suppress more tachiyomix-related deprecations (mihonapp/mihon#3409)
    (cherry picked from commit aa6742bf3ee450223cc666384a779e224316bfc1)

--- app/src/main/java/eu/kanade/domain/chapter/interactor/SyncChaptersWithSource.kt
+++ app/src/main/java/eu/kanade/domain/chapter/interactor/SyncChaptersWithSource.kt
@@ -90,6 +90,7 @@ class SyncChaptersWithSource(
             // Update metadata from source if necessary.
             if (source is HttpSource) {
                 val sChapter = chapter.toSChapter()
+                @Suppress("DEPRECATION")
                 source.prepareNewChapter(sChapter, manga.toSManga())
                 chapter = chapter.copyFromSChapter(sChapter)
             }
