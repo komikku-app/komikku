b6e855eaa9a5c348206ccf6d35292ad48fb5a29d
rianza <rianyanuar53@gmail.com>
2026-06-17 21:39:53 +0700

    chore(firebase): enable telemetry for app.komikkurnz

--- telemetry/src/firebase/kotlin/mihon/telemetry/TelemetryConfig.kt
+++ telemetry/src/firebase/kotlin/mihon/telemetry/TelemetryConfig.kt
@@ -40,6 +40,6 @@ object TelemetryConfig {
     }
 }
 
-private val MIHON_PACKAGES = hashSetOf("app.komikku", "app.komikku.beta")
+private val MIHON_PACKAGES = hashSetOf("app.komikkurnz", "app.komikkurnz.beta")
 private const val MIHON_CERTIFICATE_FINGERPRINT =
-    "CB:EC:12:1A:A8:2E:BB:02:AA:A7:38:06:99:2E:03:68:A9:7D:47:B5:45:1E:D6:52:48:16:D0:30:84:C4:59:05"
+    "91:2F:BF:F3:15:66:10:34:6D:90:10:A6:FD:C2:DF:6B:CA:0F:AD:19:60:1B:C6:83:A0:67:BE:EA:DA:5C:BC:FC"
