a1708a0ec7032a2998f5c0ff3b8e1969c0006a17
Rexx-Gaming-PC <Bunnykapoor49@gmail.com>
2026-08-06 13:36:49 +0545

    fix(ci): re-enable -Xcontext-parameters for Kotlin 2.3.0

--- buildSrc/src/main/kotlin/mihon/buildlogic/ProjectExtensions.kt
+++ buildSrc/src/main/kotlin/mihon/buildlogic/ProjectExtensions.kt
@@ -41,6 +41,7 @@ internal fun Project.configureAndroid(commonExtension: CommonExtension<*, *, *, 
         compilerOptions {
             jvmTarget.set(AndroidConfig.JvmTarget)
             freeCompilerArgs.addAll(
+                "-Xcontext-parameters",
                 "-opt-in=kotlin.RequiresOptIn",
             )
 
